1084d551d1f0f8d6728e540c915ef779ca8e1132
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-26 12:27:01 +0000

    refactoring design to use new Panel component

--- webapp/src/routes/app/AccountsAndGoals.tsx
+++ webapp/src/routes/app/AccountsAndGoals.tsx
@@ -1,6 +1,25 @@
 import Panel from "../../components/Panel"
 import SummaryCard from "../../components/SummaryCard"
 
+function PanelTitle({ title }: { title: string }) {
+    return <h2 className="flex-grow flex items-center px-4">{title}</h2>
+}
+
+function AddButton() {
+    return (
+        <p
+            className="
+                flex items-center
+                px-4
+                cursor-pointer
+                hover:bg-neutral-100 dark:hover:bg-neutral-800
+            "
+        >
+            Add
+        </p>
+    )
+}
+
 function AccountsAndGoals() {
     return (
         <div
@@ -21,13 +40,63 @@ function AccountsAndGoals() {
                 name="Total"
                 summaries={[{ amount: 133742, currency: "EUR" }]}
             />
-            <Panel title="Goals" className="row-span-3" />
-            <Panel title="Bank Accounts" />
-            <Panel title="Loans" />
-            <Panel title="Income" />
-            <Panel title="Savings" />
-            <Panel title="Credit Lines" />
-            <Panel title="Expenses" />
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Goals" />
+                        <AddButton />
+                    </>
+                }
+                className="row-span-3"
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Bank Accounts" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Loans" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Income" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Savings" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Credit Lines" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
+            <Panel
+                header={
+                    <>
+                        <PanelTitle title="Expenses" />
+                        <AddButton />
+                    </>
+                }
+            ></Panel>
         </div>
     )
 }
