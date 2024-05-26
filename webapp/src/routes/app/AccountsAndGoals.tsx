6fea781084eca628a51b5ce804a58ce5bbe0b195
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-26 11:53:27 +0000

    prototyping application design

--- webapp/src/routes/app/AccountsAndGoals.tsx
+++ webapp/src/routes/app/AccountsAndGoals.tsx
@@ -1,15 +1,106 @@
+type SummaryAmount = {
+    amount: number
+    currency: string
+}
+
+type SummaryCardProps = {
+    name: string,
+    amounts: SummaryAmount[]
+}
+
+function SummaryCard({ name, amounts }: SummaryCardProps) {
+    return (
+        <div
+            className="
+                flex items-center justify-between gap-1
+                px-4 py-1.5 h-14
+                border rounded-lg dark:border-neutral-800
+                bg-neutral-50 dark:bg-neutral-900
+                shadow
+            "
+        >
+            <p className="flex-grow">{name}</p>
+            {amounts.map(({ amount, currency }) => (
+                <p
+                    className={
+                        amount > 0
+                            ? "text-green-500"
+                            : amount < 0
+                                ? "text-red-500"
+                                : ""
+                    }
+                >
+                    {amount} {currency}
+                </p>
+            ))}
+        </div>
+    )
+}
+
+type ListCardProps = {
+    title: string,
+    className?: string
+    children?: JSX.Element[]
+}
+
+function ListCard({ title, className, children }: ListCardProps) {
+    return (
+        <div
+            className={`
+                flex flex-col
+                h-full
+                bg-neutral-50 dark:bg-neutral-900
+                divide-y dark:divide-neutral-800
+                border dark:border-neutral-800 rounded-lg
+                shadow
+                ${className}
+            `}
+        >
+            <div className="h-14 flex justify-between items-stretch">
+                <h2 className="text-lg px-4 py-1.5 flex items-center">{title}</h2>
+            </div>
+            <div className="flex-grow overflow-y-auto divide-y">
+                {
+                    children
+                        ? children
+                        : (
+                            <div className="h-full flex flex-col justify-center items-center">
+                                <span>No children</span>
+                            </div>
+                        )
+                }
+            </div>
+        </div>
+    )
+}
+
 function AccountsAndGoals() {
     return (
         <div
-            className="p-1.5
-            flex flex-col justify-center items-center gap-1
-            h-full
-            border rounded-lg dark:border-neutral-800
-            bg-neutral-50 dark:bg-neutral-900
-            shadow"
+            className="
+                grid grid-cols-4 grid-rows-[min-content_minmax(0,_1fr)_minmax(0,_1fr)] gap-1
+                h-full
+            "
         >
-            <h1 className="text-4xl">Welcome to the Accounts and Goals section</h1>
-            <p>This is a placeholder</p>
+            <SummaryCard
+                name="capital"
+                amounts={[{ amount: 133742, currency: "EUR" }]}
+            />
+            <SummaryCard
+                name="debts"
+                amounts={[{ amount: -133742, currency: "EUR" }]}
+            />
+            <SummaryCard
+                name="total"
+                amounts={[{ amount: 133742, currency: "EUR" }]}
+            />
+            <ListCard title="goals" className="row-span-3" />
+            <ListCard title="bank accounts" />
+            <ListCard title="loans" />
+            <ListCard title="income" />
+            <ListCard title="savings" />
+            <ListCard title="credit line" />
+            <ListCard title="expenses" />
         </div>
     )
 }
