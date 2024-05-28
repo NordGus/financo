e299809229846bfad26cb2a6a6a2a4b18e27409c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-28 20:50:28 +0000

    changing currency type to dinero.js Currency

--- webapp/src/types/Account.ts
+++ webapp/src/types/Account.ts
@@ -1,9 +1,11 @@
+import { Currency } from "dinero.js"
+
 export default interface Account {
     id: number
     kind: string
     name: string
     description: string
-    currency: string
+    currency: Currency
     balance: number
     capital: number
     history: {
