94a35ac223fb3489abe04abacffd16ef0ab192a1
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-28 21:03:11 +0000

    adding a basic design for external account

--- webapp/src/components/account/preview/external/Account.tsx
+++ webapp/src/components/account/preview/external/Account.tsx
@@ -4,8 +4,21 @@ type Props = {
     account: AccountType
 }
 
-export default function Account({ account: { name } }: Props) {
+export default function Account({ account: { name, description, currency } }: Props) {
     return (
-        <p className="px-4 py-1.5">{name}</p>
+        <div
+            className="
+                px-4 py-1.5 h-24
+                flex flex-col justify-center
+                hover:bg-neutral-100 dark:hover:bg-neutral-800
+                cursor-pointer
+            "
+        >
+            <div className="flex justify-between items-start gap-1">
+                <p className="text-lg flex-grow">{name}</p>
+                <p className="text-sm text-neutral-400">{currency}</p>
+            </div>
+            <p className="text-sm text-neutral-400">{description}</p>
+        </div>
     )
 }
