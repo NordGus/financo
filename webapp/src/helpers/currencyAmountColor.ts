cc4d08df5939d7cf4442f071df73a8c523ff2150
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-28 20:49:04 +0000

    adding helper function to set money color

---
+++ webapp/src/helpers/currencyAmountColor.ts
@@ -0,0 +1,6 @@
+export default function currencyAmountColor(amount: number): string {
+    if (amount > 0) return 'text-green-500'
+    if (amount < 0) return 'text-red-500'
+
+    return 'text-neutral-400'
+}
