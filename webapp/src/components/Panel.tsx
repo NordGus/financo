1614eecf4f3fbdbc3838d2fe87a96149c8c5eec7
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-31 22:17:05 +0000

    updating Panel component styling

--- webapp/src/components/Panel.tsx
+++ webapp/src/components/Panel.tsx
@@ -11,7 +11,7 @@ interface Props {
 function Panel({ header, className, loading = false, children }: PropsWithChildren<Props>) {
     return (
         <div
-            className={`flex flex-col bg-neutral-50 dark:bg-neutral-900 divide-y dark:divide-neutral-800 border dark:border-neutral-800 rounded shadow overflow-clip ${className}`}
+            className={`flex flex-col bg-neutral-50 dark:bg-neutral-900 divide-y dark:divide-neutral-800 border dark:border-neutral-800 rounded shadow overflow-clip text-neutral-950 dark:text-neutral-50 ${className}`}
         >
             <div
                 className="flex justify-between items-stretch min-h-10 h-10 max-h-10 divide-x dark:divide-neutral-800"
