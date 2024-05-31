651c3dfc52154c1179c8b83582f65b35f504694b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-31 18:44:18 +0000

    redisigning Account view

--- webapp/src/routes/app/accounts/Account.tsx
+++ webapp/src/routes/app/accounts/Account.tsx
@@ -1,37 +1,43 @@
-import { Link, LoaderFunctionArgs, useLoaderData, useParams } from "react-router-dom";
+import { Link, LoaderFunctionArgs, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import { queryClient, staleTimeDefault } from "../../../queyClient";
 import { getAccount } from "../../../api/accounts";
 
-export async function loader({ params }: LoaderFunctionArgs) {
+import Throbber from "../../../components/Throbber";
+
+export async function loader({ params: { id } }: LoaderFunctionArgs) {
     return queryClient.fetchQuery({
-        queryKey: ['accounts', 'details', params.id!],
-        queryFn: getAccount(params.id!),
+        queryKey: ['accounts', 'details', id!],
+        queryFn: getAccount(id!),
         staleTime: staleTimeDefault
     })
 }
 
 export default function Account() {
-    const initialData = useLoaderData() as Awaited<ReturnType<typeof loader>>
-    const params = useParams()
-    const { data: account } = useQuery({
-        queryKey: ['accounts', 'details', params.id!],
-        queryFn: getAccount(params.id!),
-        staleTime: staleTimeDefault,
-        initialData
+    const { id: paramsID } = useParams()
+    const accountQuery = useQuery({
+        queryKey: ['accounts', 'details', paramsID!],
+        queryFn: getAccount(paramsID!),
+        staleTime: staleTimeDefault
     })
+
+    if (accountQuery.isFetching) {
+        return (
+            <div className="h-[30dvh] flex justify-center items-center">
+                <Throbber variant="big" />
+            </div>
+        )
+    }
+
+    const { name } = accountQuery.data!
 
     return (
         <>
             <div
-                className="
-                    min-h-12 h-12 max-h-12
-                    flex justify-between items-stretch
-                    divide-x dark:divide-neutral-800
-                "
+                className="min-h-12 h-12 max-h-12 flex justify-between items-stretch divide-x dark:divide-neutral-800"
             >
-                <h1 className="text-lg flex items-center flex-grow">{account.name}</h1>
+                <h1 className="text-lg flex items-center flex-grow">{name}</h1>
                 <Link
                     to="/accounts"
                     className="flex items-center px-4 py-1.5"
