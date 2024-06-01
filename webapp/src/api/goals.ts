177fafcec2bc309f8c8736269ff7a1245dbbda81
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-01 01:31:14 +0000

    adding api actions to retrieve archived and reached goals

--- webapp/src/api/goals.ts
+++ webapp/src/api/goals.ts
@@ -10,3 +10,25 @@ export async function getGoals(): Promise<Goal[]> {
 
     return response.json()
 }
+
+export async function getArchivedGoals(): Promise<Goal[]> {
+    const response = await fetch("/api/goals/archived")
+
+    if (!response.ok) {
+        console.error(response)
+        throw new Error('Network response was not ok')
+    }
+
+    return response.json()
+}
+
+export async function getReachedGoals(): Promise<Goal[]> {
+    const response = await fetch("/api/goals/reached")
+
+    if (!response.ok) {
+        console.error(response)
+        throw new Error('Network response was not ok')
+    }
+
+    return response.json()
+}
