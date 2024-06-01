70715c425ea6716484d089d2f9139d93f135bfcf
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-01 12:27:03 +0000

    refactoring GoalsPanels to new standard

--- webapp/src/routes/app/AccountsAndGoals/GoalsPanel.tsx
+++ webapp/src/routes/app/AccountsAndGoals/GoalsPanel.tsx
@@ -6,8 +6,9 @@ import { staleTimeDefault } from "../../../queyClient"
 
 import Panel, { ActionButton, ActionLink, Title } from "@components/Panel"
 import WithNavigation from "@components/goal/preview/WithNavigation"
+import { Link } from "react-router-dom"
 
-type Querie = "active" | "archived" | "reached"
+type Queries = "active" | "archived" | "reached"
 
 interface GoalsPanelProps {
     className?: string
@@ -36,10 +37,18 @@ export default function GoalsPanel({ className }: GoalsPanelProps) {
     const archivedQuery = useQuery(archivedQueryOptions)
     const reachedQuery = useQuery(reachedQueryOptions)
 
-    const [currentQuery, setCurrentQuery] = useState<Querie>("active")
+    const [currentQuery, setCurrentQuery] = useState<Queries>("active")
 
     return (
         <Panel
+            className={className}
+            loading={
+                {
+                    active: activeQuery.isFetching,
+                    archived: archivedQuery.isFetching,
+                    reached: reachedQuery.isFetching
+                }[currentQuery] || false
+            }
             header={
                 <>
                     <Title text="Goals" grow={true} />
@@ -48,41 +57,48 @@ export default function GoalsPanel({ className }: GoalsPanelProps) {
             }
             tabs={
                 <>
-                    <ActionButton
-                        text="Active"
-                        onClick={() => setCurrentQuery("active")}
-                        active={currentQuery === "active"}
-                        grow={true}
-                    />
-                    <ActionButton
-                        text="Archived"
-                        onClick={() => setCurrentQuery("archived")}
-                        active={currentQuery === "archived"}
-                        grow={true}
-                    />
-                    <ActionButton
-                        text="Reached"
-                        onClick={() => setCurrentQuery("reached")}
-                        active={currentQuery === "reached"}
-                        grow={true}
-                    />
+                    {
+                        ([
+                            { text: "Active", query: "active" },
+                            { text: "Archived", query: "archived" },
+                            { text: "Active", query: "reached" },
+                        ] as { text: string, query: Queries }[]).map(({ text, query }) => (
+                            <ActionButton
+                                text={text}
+                                onClick={() => setCurrentQuery(query)}
+                                active={currentQuery === query}
+                                grow={true}
+                            />
+                        ))
+                    }
                 </>
             }
-            className={className}
-            loading={
-                currentQuery === "active"
-                    ? activeQuery.isFetching
-                    : currentQuery === "archived"
-                        ? archivedQuery.isFetching
-                        : reachedQuery.isFetching
+            contents={
+                {
+                    active: activeQuery.data?.
+                        map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />),
+                    archived: archivedQuery.data?.
+                        map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />),
+                    reached: reachedQuery.data?.
+                        map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />)
+                }[currentQuery] || null
             }
-        >
-            {currentQuery === "active" && activeQuery.data?.
-                map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />)}
-            {currentQuery === "archived" && archivedQuery.data?.
-                map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />)}
-            {currentQuery === "reached" && reachedQuery.data?.
-                map((goal) => <WithNavigation key={`goal:${goal.id}`} goal={goal} />)}
-        </Panel>
+            noContentsMessage={
+                {
+                    active: <div className="flex flex-col justify-center items-center gap-2">
+                        <p>No <span className="font-bold">Savings Goals</span> active in the system</p>
+                        <Link to={`/accounts`} className="text-sm underline">
+                            Please add a new one
+                        </Link>
+                    </div>,
+                    archived: <div className="flex flex-col justify-center items-center gap-2">
+                        <p>No <span className="font-bold">Savings Goals</span> archived in the system</p>
+                    </div>,
+                    reached: <div className="flex flex-col justify-center items-center gap-2">
+                        <p>No <span className="font-bold">Savings Goals</span> reached in the system</p>
+                    </div>
+                }[currentQuery] || undefined
+            }
+        />
     )
 }
