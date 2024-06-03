0a23a39d0b10a06015656057ea59cfca66ab0f78
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-03 21:19:14 +0000

    mocking account details views

--- webapp/src/routes/App/Account/New/index.tsx
+++ webapp/src/routes/App/Account/New/index.tsx
@@ -1,3 +1,42 @@
+import { useParams } from "react-router-dom";
+
+import { Kind } from "@/types/Account";
+
+import kindToHuman from "@helpers/account/kindToHuman";
+import Panel from "@components/Panel";
+import Throbber from "@components/Throbber";
+
 export default function New() {
-    return <span>New Account</span>
+    const { kind } = useParams()
+
+    return (
+        <Panel.Base
+            className="h-full"
+            header={<>
+                {
+                    false && <div
+                        className="flex justify-center items-center py-1 px-2 gap-2"
+                    >
+                        <Throbber variant="small" />
+                        <p>Fetching</p>
+                    </div>
+                }
+                <Panel.Components.Title text={`New ${kindToHuman(kind! as Kind)}`} />
+                <span className="flex-grow content-['']"></span>
+                <Panel.Components.ActionLink to="/accounts" text="Close" />
+            </>}
+        >
+            <div className="flex-grow grid grid-cols-2 grid-rows-3">
+                <div className="row-span-3 overflow-y-auto border-r dark:border-neutral-800">
+                    Account Form goes here
+                </div>
+                <div>
+                    a graphic goes here
+                </div>
+                <div className="row-span-2 border-t dark:border-neutral-800">
+                    transactions go here
+                </div>
+            </div>
+        </Panel.Base>
+    )
 }
--- webapp/src/routes/App/Account/Show/index.tsx
+++ webapp/src/routes/App/Account/Show/index.tsx
@@ -4,8 +4,9 @@ import { useParams } from "react-router-dom"
 
 import { accountQuery } from "@queries/accounts"
 
-import Panel from "@components/Panel"
 import kindToHuman from "@helpers/account/kindToHuman"
+import Throbber from "@components/Throbber"
+import Panel from "@components/Panel"
 
 export default function Show() {
     const { id } = useParams()
@@ -13,18 +14,33 @@ export default function Show() {
     const query = useQuery(queryOptions)
 
     return (
-        <Panel.WithLoadingIndicator
-            className="max-h-[95dvh] min-h-[30dvh]"
-            loading={query.isFetching}
-            grow={true}
+        <Panel.Base
+            className="h-full"
             header={<>
+                {
+                    query.isFetching && <div
+                        className="flex justify-center items-center py-1 px-2 gap-2"
+                    >
+                        <Throbber variant="small" />
+                        <p>Fetching</p>
+                    </div>
+                }
                 {query.isFetched && <Panel.Components.Title text={kindToHuman(query.data!.kind)} />}
                 <span className="flex-grow content-['']"></span>
                 <Panel.Components.ActionLink to="/accounts" text="Close" />
             </>}
-            contents={<>
-                <span>{query.data?.name}</span>
-            </>}
-        />
+        >
+            <div className="flex-grow grid grid-cols-2 grid-rows-3">
+                <div className="row-span-3 overflow-y-auto border-r dark:border-neutral-800">
+                    Account Form goes here
+                </div>
+                <div>
+                    a graphic goes here
+                </div>
+                <div className="row-span-2 border-t dark:border-neutral-800">
+                    transactions go here
+                </div>
+            </div>
+        </Panel.Base>
     )
 }
