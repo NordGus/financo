44632e37bfe4e547b23f094be26f7d02e68f8345
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-07 20:12:52 +0000

    setting up base books design

--- webapp/src/routes/App/Books/index.tsx
+++ webapp/src/routes/App/Books/index.tsx
@@ -1,15 +1,29 @@
+import Panel from "@components/Panel";
+
 export default function Books() {
     return (
-        <div
-            className="p-1.5
-            flex flex-col justify-center items-center gap-1
-            h-full
-            border rounded dark:border-neutral-800
-            bg-neutral-50 dark:bg-neutral-900
-            shadow"
-        >
-            <h1 className="text-4xl">Welcome to the Books section</h1>
-            <p>This is a placeholder</p>
+        <div className="grid grid-rows-4 h-full grid-cols-4 gap-1">
+            <Panel.Base
+                className="col-span-2 row-span-4"
+                header={<>
+                    <Panel.Components.Title grow={true} text="Transactions" />
+                </>}
+            >
+                <div className="flex items-center px-2">Filters</div>
+                <div className="divide-y dark:divide-neutral-800 flex justify-center items-center flex-grow overflow-y-auto">
+                    <span>Transactions here</span>
+                </div>
+            </Panel.Base>
+            <Panel.Clean className="col-span-2 row-span-2">
+                <span className="flex-grow flex justify-center items-center">
+                    Expenses and Income Chart
+                </span>
+            </Panel.Clean>
+            <Panel.WithLoadingIndicator
+                className="col-span-2 row-span-2"
+                header={<Panel.Components.Title grow={true} text="Pending Transactions" />}
+                contents={<></>}
+            />
         </div>
     )
 }
