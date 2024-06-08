3bcfdfe0cdb266eff113fbb0d4735f741933708e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-08 01:02:23 +0000

    making placeholder for transactions

--- webapp/src/routes/App/Books/index.tsx
+++ webapp/src/routes/App/Books/index.tsx
@@ -3,17 +3,16 @@ import Panel from "@components/Panel";
 export default function Books() {
     return (
         <div className="grid grid-rows-4 h-full grid-cols-4 gap-1">
-            <Panel.Base
+            <Panel.WithFilters
+                grow={true}
                 className="col-span-2 row-span-4"
                 header={<>
                     <Panel.Components.Title grow={true} text="Transactions" />
                 </>}
-            >
-                <div className="flex items-center px-2">Filters</div>
-                <div className="divide-y dark:divide-neutral-800 flex justify-center items-center flex-grow overflow-y-auto">
-                    <span>Transactions here</span>
-                </div>
-            </Panel.Base>
+                loading={false}
+                contents={<span>Transactions here</span>}
+                filters={<div className="flex items-center px-2">Filters</div>}
+            />
             <Panel.Clean className="col-span-2 row-span-2">
                 <span className="flex-grow flex justify-center items-center">
                     Expenses and Income Chart
