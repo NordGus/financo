184e730d043df7eabe9d7d852351c623bb6fdb58
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-08 14:45:44 +0000

    adding styling to prevent text selection on Panel ActionButton component

--- webapp/src/components/Panel/components/ActionButton.tsx
+++ webapp/src/components/Panel/components/ActionButton.tsx
@@ -7,7 +7,7 @@ interface ActionButtonProps {
 
 export function ActionButton({ text, onClick, active, grow = false }: ActionButtonProps) {
     return <p
-        className={`flex items-center justify-center px-4 cursor-pointer ${active ? "bg-neutral-100 dark:bg-neutral-800" : "hover:bg-neutral-100 dark:hover:bg-neutral-800"} ${grow ? "flex-grow" : ""}`}
+        className={`flex items-center justify-center px-4 cursor-pointer select-none ${active ? "bg-neutral-100 dark:bg-neutral-800" : "hover:bg-neutral-100 dark:hover:bg-neutral-800"} ${grow ? "flex-grow" : ""}`}
         onClick={onClick}
     >
         {text}
