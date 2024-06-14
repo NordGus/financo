4f13aeaa2506994a700ba945f0296646e3165e8d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-14 17:00:12 +0000

    synchronizing animation duration on modals

--- webapp/src/components/Modal.tsx
+++ webapp/src/components/Modal.tsx
@@ -49,7 +49,7 @@ export default function Modal(props: ModalProps) {
     return (
         <dialog ref={ref} className={`group ${className}`} {...rest}>
             <div
-                className="fixed inset-0 flex justify-center items-end bg-neutral-500/20 opacity-0 group-data-[open]:opacity-100 backdrop-blur-sm transition-all duration-200 sm:p-2"
+                className="fixed inset-0 flex justify-center items-end bg-neutral-500/20 opacity-0 group-data-[open]:opacity-100 backdrop-blur-sm transition-all duration-500 sm:p-2"
             >
                 <div
                     className={`w-full max-w-[400px] sm:min-w-[400px] ${variants[variant]} translate-y-full group-data-[open]:translate-y-0 transition-transform duration-500 ${bodyClassName ? bodyClassName : ""}`}
