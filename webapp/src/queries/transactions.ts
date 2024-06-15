0462f965bd89a46cc7a6230f22bf2f776a144b0a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-15 20:16:34 +0000

    adding transaction query builder

--- webapp/src/queries/transactions.ts
+++ webapp/src/queries/transactions.ts
@@ -1,10 +1,18 @@
 import { staleTimeDefault } from "./Client";
-import { getPendingTransactions } from "@api/transactions";
+import { getPendingTransactions, getTransaction } from "@api/transactions";
 
 export const transactionsQueries = {
     pending: {
         queryKey: ['transactions', 'pending'],
         queryFn: getPendingTransactions,
+        staleTime: staleTimeDefault
+    }
+}
+
+export function transactionQuery(id: string) {
+    return {
+        queryKey: ['transaction', 'details', id],
+        queryFn: getTransaction(id),
         staleTime: staleTimeDefault
     }
 }
