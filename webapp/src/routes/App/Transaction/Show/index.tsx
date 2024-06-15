b9bda5bba626f5d0a957d1942b4dadc1ef88fb2a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-15 20:18:38 +0000

    adding transaction query to Transaction Show

--- webapp/src/routes/App/Transaction/Show/index.tsx
+++ webapp/src/routes/App/Transaction/Show/index.tsx
@@ -1,26 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useParams } from "react-router-dom";
+
+import { transactionQuery } from "@queries/transactions";
+
 import Panel from "@components/Panel";
 import Throbber from "@components/Throbber";
-import { useParams } from "react-router-dom";
 
 export default function Show() {
     const { id } = useParams()
+    const queryOptions = useMemo(() => transactionQuery(id!), [id!])
+    const { data: transaction, isFetching } = useQuery(queryOptions)
 
     return (
         <Panel.Base
             className="h-full"
             header={<>
                 {
-                    <div
+                    isFetching && <div
                         className="flex justify-center items-center py-1 px-2 gap-2"
                     >
                         <Throbber variant="small" />
+                        <p>Fetching</p>
                     </div>
                 }
                 <span className="flex-grow content-['']"></span>
                 <Panel.Components.ActionLink to="/books" text="Close" />
             </>}
         >
-            <span className="flex-grow flex justify-center items-center">{id}</span>
+            <span className="flex-grow flex justify-center items-center">{transaction?.id}</span>
         </Panel.Base>
     )
 }
