2e022bd3335f8c10d28b476c1b0b55491e8fd74c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-06-16 12:55:30 +0000

    redesigning Account Show page

--- webapp/src/routes/App/Account/Show/index.tsx
+++ webapp/src/routes/App/Account/Show/index.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query"
 import { useMemo } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 import { accountQuery } from "@queries/accounts"
 
-import kindToHuman from "@helpers/account/kindToHuman"
 import Throbber from "@components/Throbber"
 import Panel from "@components/Panel"
+import Transactions from "./Transactions"
 
 export default function Show() {
     const { id } = useParams()
@@ -14,33 +14,40 @@ export default function Show() {
     const query = useQuery(queryOptions)
 
     return (
-        <Panel.Base
-            className="h-full"
-            header={<>
-                {
-                    query.isFetching && <div
-                        className="flex justify-center items-center py-1 px-2 gap-2"
-                    >
-                        <Throbber variant="small" />
-                        <p>Fetching</p>
-                    </div>
-                }
-                {query.isFetched && <Panel.Components.Title text={kindToHuman(query.data!.kind)} />}
-                <span className="flex-grow content-['']"></span>
-                <Panel.Components.ActionLink to="/accounts" text="Close" />
-            </>}
+        <div
+            className="h-full grid grid-rows-[minmax(0,_min-content)_minmax(0,_1fr)_minmax(0,_1fr)_minmax(0,_1fr)] grid-cols-2 gap-2"
         >
-            <div className="flex-grow grid grid-cols-2 grid-rows-3">
-                <div className="row-span-3 overflow-y-auto border-r dark:border-neutral-800">
+            <div className="col-span-2 flex items-stretch min-h-10 h-10 max-h-10">
+                <Link
+                    to="/accounts"
+                    className="flex items-center justify-center px-4 bg-neutral-50 dark:bg-neutral-900 border dark:border-neutral-800 rounded shadow overflow-clip text-neutral-950 dark:text-neutral-50  hover:bg-neutral-100 dark:hover:bg-neutral-800"
+                >
+                    Close
+                </Link>
+            </div>
+            <Panel.Base
+                header={<>
+                    {
+                        query.isFetching && <div
+                            className="flex justify-center items-center py-1 px-2 gap-2"
+                        >
+                            <Throbber variant="small" />
+                            <p>Fetching</p>
+                        </div>
+                    }
+                    <Panel.Components.Title grow={true} text="Details" />
+                </>}
+                className="row-span-3"
+            >
+                <div className="flex-grow overflow-y-auto">
                     Account Form goes here
                 </div>
-                <div>
-                    a graphic goes here
-                </div>
-                <div className="row-span-2 border-t dark:border-neutral-800">
-                    transactions go here
-                </div>
-            </div>
-        </Panel.Base>
+            </Panel.Base>
+            <Panel.Clean className="">a graphic goes here</Panel.Clean>
+            <Transactions
+                className="row-span-2"
+                accountId={id!}
+            />
+        </div >
     )
 }
