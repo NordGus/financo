3df40e09166024e0ddee8af6e263bd75bda077ff
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-07 10:03:28 +0000

    setting up id to auto increment

--- server/database/migrations/20240622184012_create_accounts.sql
+++ server/database/migrations/20240622184012_create_accounts.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS accounts (
-    id BIGINT PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     parent_id BIGINT,
     kind VARCHAR NOT NULL,
     currency VARCHAR NOT NULL,
@@ -17,20 +17,30 @@ CREATE TABLE IF NOT EXISTS accounts (
 );
 
 CREATE INDEX account_parent_id_on_accounts_index ON accounts (parent_id);
+
 CREATE INDEX account_kind_on_accounts_index ON accounts (kind);
+
 CREATE INDEX account_currency_on_accounts_index ON accounts (currency);
+
 CREATE INDEX account_kind_currency_on_accounts_index ON accounts (kind, currency);
+
 CREATE INDEX account_archived_at_on_accounts_index ON accounts (archived_at);
+
 CREATE INDEX account_deleted_at_on_accounts_index ON accounts (deleted_at);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP INDEX account_parent_id_on_accounts_index;
+
 DROP INDEX account_kind_on_accounts_index;
+
 DROP INDEX account_currency_on_accounts_index;
+
 DROP INDEX account_kind_currency_on_accounts_index;
+
 DROP INDEX account_archived_at_on_accounts_index;
+
 DROP INDEX account_deleted_at_on_accounts_index;
 
 DROP TABLE accounts;
