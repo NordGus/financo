22983602a9f0dae2fded04ef267a1571b15b3b94
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-08 18:31:24 +0000

    adding transactions migration

---
+++ server/database/migrations/20240707200956_create_transactions.sql
@@ -0,0 +1,40 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS transactions (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    source_id BIGINT CONSTRAINT transaction_source_reference REFERENCES accounts (id),
+    target_id BIGINT CONSTRAINT transaction_target_reference REFERENCES accounts (id),
+    source_amount BIGINT NOT NULL,
+    target_amount BIGINT NOT NULL,
+    notes TEXT,
+    issued_at DATE NOT NULL,
+    executed_at DATE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE INDEX transaction_source_reference_index ON transactions (source_id);
+
+CREATE INDEX transaction_target_reference_index ON transactions (target_id);
+
+CREATE INDEX transaction_issued_at_index ON transactions (issued_at);
+
+CREATE INDEX transaction_executed_at_index ON transactions (executed_at);
+
+CREATE INDEX transaction_deleted_at_on_transactions_index ON transactions (deleted_at);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX transaction_source_reference_index;
+
+DROP INDEX transaction_target_reference_index;
+
+DROP INDEX transaction_issued_at_index;
+
+DROP INDEX transaction_executed_at_index;
+
+DROP INDEX transaction_deleted_at_on_transactions_index;
+
+DROP TABLE transactions;
+-- +goose StatementEnd
