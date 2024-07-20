d5036ce6a144167716e899c6cd1666f2e5f017c6
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-20 20:31:12 +0000

    mocking new Books component

--- webapp/src/routes/App/Books/index.tsx
+++ webapp/src/routes/App/Books/index.tsx
@@ -1,36 +1,85 @@
 import { Outlet, useOutlet } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import moment from "moment";
 
+import { getTransactions, ListFilters } from "@api/transactions";
+
+import Modal from "@components/Modal";
 import Transactions from "./Transactions";
 import Chart from "./Chart";
 
-import Modal from "@components/Modal";
+type ModalStyles = "outlet" | "history" | "upcoming" | "none";
+
+function defaultHistoryFilters(): ListFilters {
+    return {
+        executedFrom: moment().subtract({ months: 1 }).format('YYYY-MM-DD'),
+        executedUntil: moment().format('YYYY-MM-DD')
+    }
+}
 
 export default function Books() {
     const outlet = useOutlet()
     const [outletCache, setOutletCache] = useState(outlet)
+    const [modalFor, setModalFor] = useState<ModalStyles>("none")
+    const [showHistoryFilters, setShowHistoryFilters] = useState(false)
+    const [showUpcomingFilters, setShowUpcomingFilters] = useState(false)
+    const [historyFilters, setHistoryFilters] = useState<ListFilters>(defaultHistoryFilters())
+
+    const historyMutation = useMutation({
+        mutationFn: (filters: ListFilters) => getTransactions(filters)()
+    })
 
     useEffect(() => {
         if (outlet && !outletCache) setOutletCache(outlet)
     }, [outlet, outletCache])
 
+    useEffect(() => {
+        if (outlet && modalFor !== "outlet")
+            setModalFor("outlet")
+        else if (showHistoryFilters && modalFor !== "history")
+            setModalFor("history")
+        else if (showUpcomingFilters && modalFor !== "upcoming")
+            setModalFor("upcoming")
+        else if (!outlet && !showHistoryFilters && !showUpcomingFilters && modalFor !== "none")
+            setModalFor("none")
+    }, [outlet, showHistoryFilters, showUpcomingFilters])
+
+    useEffect(() => historyMutation.mutate(historyFilters), [historyFilters])
+
     return (
         <>
             <div className="grid grid-rows-[60dvh,_minmax(0,_1fr)] h-full grid-cols-3 gap-1">
-                <Transactions.History className="row-span-2" />
+                <Transactions.History
+                    showFilters={showHistoryFilters}
+                    setShowFilters={setShowHistoryFilters}
+                    filters={historyMutation}
+                    className="row-span-2"
+                />
                 <Transactions.Upcoming className="" />
                 <Transactions.Pending className="" />
                 <Chart className="col-span-2" />
             </div>
             <Modal
-                open={!!outlet}
-                onClose={() => setOutletCache(null)} variant="small"
+                open={modalFor !== "none"}
+                onClose={() => {
+                    setOutletCache(null)
+                }}
+                variant="small"
                 bodyClassName="h-[75dvh]"
             >
                 {
-                    outlet
-                        ? <Outlet />
-                        : (outletCache)
+                    modalFor === "outlet" && (outlet ? <Outlet /> : (outletCache))
+                }
+                {
+                    modalFor === "history" && (
+                        <h1 className="text-2xl text-neutral-50">History Modal Filters</h1>
+                    )
+                }
+                {
+                    modalFor === "upcoming" && (
+                        <h1 className="text-2xl text-neutral-50">Upcoming Modal Filters</h1>
+                    )
                 }
             </Modal>
         </>
