c2ed490f653738a457cedde5eafd85fa7ffd77c1
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-21 16:56:23 +0000

    bugfix: boolean query params considered empty

--- webapp/src/helpers/isEmptyParam.ts
+++ webapp/src/helpers/isEmptyParam.ts
@@ -1,6 +1,7 @@
 import { isEmpty, isNil, isNaN } from "lodash"
 
 export default function isEmptyParam(param: any): boolean {
+    if (typeof param === 'boolean') return false
     if (isEmpty(param)) return true
     if (isNil(param)) return true
     if (isNaN(param)) return true
