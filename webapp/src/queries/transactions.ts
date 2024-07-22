306a4517284675c0c6a641ac19d4bcbcfd156f59
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-22 23:46:06 +0000

    adding a transaction query to retrieve the current moth's transactions for a given account

--- webapp/src/queries/transactions.ts
+++ webapp/src/queries/transactions.ts
@@ -1,5 +1,6 @@
+import moment from "moment";
 import { staleTimeDefault } from "./Client";
-import { getPendingTransactions, getTransaction } from "@api/transactions";
+import { getPendingTransactions, getTransaction, getTransactions } from "@api/transactions";
 
 export const transactionsQueries = {
     pending: {
@@ -13,6 +14,18 @@ export function transactionQuery(id: string) {
     return {
         queryKey: ['transaction', 'details', id],
         queryFn: getTransaction(id),
+        staleTime: staleTimeDefault
+    }
+}
+
+export function currentMonthsTransactionsForAccountQuery(accountID: number) {
+    return {
+        queryKey: ['account', accountID, 'transactions'],
+        queryFn: getTransactions({
+            executedFrom: moment().startOf('month').format('YYYY-MM-DD'),
+            executedUntil: moment().endOf('month').format('YYYY-MM-DD'),
+            account: [accountID]
+        }),
         staleTime: staleTimeDefault
     }
 }
