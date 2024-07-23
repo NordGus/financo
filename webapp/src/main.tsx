aef12d183522ef826d659ec5fe2bcebeb967bf6f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-23 16:37:45 +0000

    adding material symbols

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 
+import './index.css'
+import 'material-symbols/rounded.css'
+
 import QueryClient from "@queries/Client"
 import router from './router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
