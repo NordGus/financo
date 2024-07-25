30b93aa731fe184b92bd7d89ee471786b4963e75
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-25 22:29:59 +0000

    cleaning Dashboard from old styling

--- webapp/src/routes/App/Dashboard/index.tsx
+++ webapp/src/routes/App/Dashboard/index.tsx
@@ -1,15 +1,10 @@
 export default function Dashboard() {
     return (
-        <div
-            className="p-1.5
-            flex flex-col justify-center items-center gap-1
-            h-full
-            border rounded dark:border-zinc-800
-            bg-zinc-50 dark:bg-zinc-950
-            shadow"
-        >
-            <h1 className="text-4xl">Welcome to financo</h1>
+        <>
+            <h1 className="text-4xl">
+                Welcome to financo
+            </h1>
             <p>Your personal finances helper</p>
-        </div>
+        </>
     )
 }
