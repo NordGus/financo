e2e5fce719a6787674f003c6eb66d83619c1fa46
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-25 18:52:04 +0000

    cleaning accounts layout

--- webapp/src/routes/App/Account/_index.tsx
+++ webapp/src/routes/App/Account/_index.tsx
@@ -12,7 +12,9 @@ export function loader(_queryClient: QueryClient): LoaderFunction {
 export default function Layout() {
     return (
         <>
-            <Breadcrumbs />
+            <div className="h-10 md:h-9 flex items-center mb-2">
+                <Breadcrumbs />
+            </div>
             <Outlet />
         </>
     )
