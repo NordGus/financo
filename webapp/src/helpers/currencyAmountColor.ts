c6a3145100af41a2690569a2fc63d4218fb14701
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 17:36:38 +0000

    adding some color differentiation to currencyAmountColor helper

--- webapp/src/helpers/currencyAmountColor.ts
+++ webapp/src/helpers/currencyAmountColor.ts
@@ -1,6 +1,7 @@
-export default function currencyAmountColor(amount: number): string {
-    if (amount > 0) return 'text-green-500'
+export default function currencyAmountColor(amount: number, transaction: boolean = true): string {
+    if (amount > 0 && transaction) return 'text-green-500'
+    if (amount > 0) return 'text-zinc-950 dark:text-zinc-50'
     if (amount < 0) return 'text-red-500'
 
-    return 'text-zinc-400'
+    return 'text-zinc-500'
 }
