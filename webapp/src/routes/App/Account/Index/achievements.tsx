250d8acfbdeaa9146dc99fcdaf84ac4e0de22534
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 19:13:10 +0000

    developing AchievementsTracker

--- webapp/src/routes/App/Account/Index/achievements.tsx
+++ webapp/src/routes/App/Account/Index/achievements.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { isEmpty, isNil } from "lodash";
+import { CircleHelpIcon, TrophyIcon } from "lucide-react";
 import moment from "moment";
 
 import Goal from "@/types/Goal";
@@ -12,7 +13,7 @@ import { Throbber } from "@components/Throbber";
 import { Button } from "@components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@components/ui/card";
 import { Table, TableBody, TableCell, TableRow } from "@components/ui/table";
-import { TrophyIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@components/ui/tooltip";
 
 export function AchievementsTracker({ className }: { className?: string }) {
     // [ ] TODO: Implement goals/achievements endpoints
@@ -40,12 +41,77 @@ export function AchievementsTracker({ className }: { className?: string }) {
         },
         {
             id: 2,
-            name: "The emergency fund is getting serious",
-            description: "Leveling the emergency fund to 3 months payment",
+            name: "This is not even my final form",
+            description: "Take the emergency fund to 3 month's income",
             goal: 600000,
             currency: "EUR",
-            balance: 133700,
+            balance: 600000,
             position: 2,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 3,
+            name: "Inner Peace",
+            description: "Your emergency fund gives you 3 month's runway",
+            goal: 1200000,
+            currency: "EUR",
+            balance: 1200000,
+            position: 3,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 4,
+            name: "Harmony within, Hurricane without",
+            description: "Now your emergency fund has 12 month's income",
+            goal: 2400000,
+            currency: "EUR",
+            balance: 1200000,
+            position: 4,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 5,
+            name: "Upgrades for my Desktop",
+            description: null,
+            goal: 100000,
+            currency: "EUR",
+            balance: 0,
+            position: 5,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 6,
+            name: "Investment for the Studio",
+            description: "Buying some hardware to create games better",
+            goal: 600000,
+            currency: "EUR",
+            balance: 0,
+            position: 6,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 7,
+            name: "Honeymoon",
+            description: "A little treat for my spouse",
+            goal: 2000000,
+            currency: "EUR",
+            balance: 0,
+            position: 7,
             achievedAt: null,
             archivedAt: null,
             createdAt: moment().add({ month: -3 }).toDate().toISOString(),
@@ -74,27 +140,35 @@ export function AchievementsTracker({ className }: { className?: string }) {
                         : <Table>
                             <TableBody>
                                 {
-                                    isNil(goals) || isEmpty(goals)
-                                        ? <TableRow>
-                                            <TableCell className="text-center">
-                                                You have no active savings goals
+                                    goals.sort((a, b) => a.position - b.position).map((goal) => {
+                                        const progress = goal.balance / goal.goal
+                                        const color = "#22c55e"
+
+                                        return <TableRow>
+                                            <TableCell>{goal.name}</TableCell>
+                                            <TableCell>
+                                                {!isNil(goal.description) && (
+                                                    <Tooltip delayDuration={0}>
+                                                        <TooltipTrigger
+                                                            className="text-zinc-500 hover:text-zinc-950 dark:hover:text-zinc-50"
+                                                        >
+                                                            <CircleHelpIcon size={18} />
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            {goal.description}
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                )}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Progress
+                                                    progress={progress}
+                                                    icon={<TrophyIcon size={20} />}
+                                                    color={color}
+                                                />
                                             </TableCell>
                                         </TableRow>
-                                        : goals.map((goal) => {
-                                            const progress = goal.balance / goal.goal
-                                            const color = "#22c55e"
-
-                                            return <TableRow>
-                                                <TableCell>{goal.name}</TableCell>
-                                                <TableCell>
-                                                    <Progress
-                                                        progress={progress}
-                                                        icon={<TrophyIcon size={20} />}
-                                                        color={color}
-                                                    />
-                                                </TableCell>
-                                            </TableRow>
-                                        })
+                                    })
                                 }
                             </TableBody>
                         </Table>
