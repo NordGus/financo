daa51ebb75c8cb6b85967cbfea16cf73fe132fd1
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 09:37:38 +0000

    adding SystemHistoric Kind

--- webapp/src/helpers/account/kindToHuman.ts
+++ webapp/src/helpers/account/kindToHuman.ts
@@ -2,12 +2,13 @@ import { Kind } from "@/types/Account";
 
 export default function kindToHuman(kind: Kind): string {
     return {
-        [Kind.CapitalNormal]: "Bank Account",
+        [Kind.CapitalNormal]: "Bank",
         [Kind.CapitalSavings]: "Savings",
         [Kind.DebtCredit]: "Credit",
         [Kind.DebtLoan]: "Loan",
-        [Kind.DebtPersonal]: "Personal Loan",
+        [Kind.DebtPersonal]: "Personal",
         [Kind.ExternalIncome]: "Income",
-        [Kind.ExternalExpense]: "Expense"
+        [Kind.ExternalExpense]: "Expense",
+        [Kind.SystemHistoric]: "Historic"
     }[kind]
 }
