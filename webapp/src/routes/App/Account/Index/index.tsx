ed3d99d542fb7c3a131b80e4ea4ab7ef9d5b355e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 19:22:06 +0000

    cleaning unused code

--- webapp/src/routes/App/Account/Index/index.tsx
+++ webapp/src/routes/App/Account/Index/index.tsx
@@ -3,8 +3,6 @@ import { QueryClient } from "@tanstack/react-query";
 
 import { Button } from "@components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
-import { SummaryAvailableCredit, SummaryCapital, SummaryDebt, SummaryNetWorth } from "./summaries";
-import { AchievementsTracker } from "./achievements";
 import {
     CapitalAccountsTable,
     DebtAccountsTable,
