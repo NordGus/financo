8a689bb4f51072eb18265154726787c590f5d8e7
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 10:32:15 +0000

    initial implementation of ExpensesAccountsTable

--- webapp/src/routes/App/Account/Index/accounts.tsx
+++ webapp/src/routes/App/Account/Index/accounts.tsx
@@ -283,43 +283,22 @@ export function IncomeAccountsTable({ }) {
                                     <TableRow>
                                         <TableHead>Name</TableHead>
                                         <TableHead>Description</TableHead>
-                                        <TableHead>Kind</TableHead>
-                                        <TableHead>Balance</TableHead>
+                                        <TableHead>Currency</TableHead>
                                     </TableRow>
                                 </TableHeader>
                                 <TableBody>
                                     {
                                         active.map(({
-                                            id, kind, name, description, balance, currency, color
+                                            id, name, description, currency
                                         }) => {
-                                            const clr = new Color(color)
-                                                .to("HSL")
-                                                .set({ l: (l) => l >= 50 ? 1 : 100 })
-                                                .toString({ precision: 3, format: "rgb" })
-
                                             return <TableRow
                                                 key={`account:${id}`}
                                                 className="cursor-pointer"
                                                 onClick={() => navigate(`/accounts/${id}`)}
                                             >
-                                                <TableCell>
-                                                    {name}
-                                                </TableCell>
+                                                <TableCell>{name}</TableCell>
                                                 <TableCell>{description}</TableCell>
-                                                <TableCell>
-                                                    <span
-                                                        className="px-2 py-1 rounded-md text-xs"
-                                                        style={{
-                                                            backgroundColor: color,
-                                                            color: clr
-                                                        }}
-                                                    >
-                                                        {kindToHuman(kind)}
-                                                    </span>
-                                                </TableCell>
-                                                <TableCell className={currencyAmountColor(balance)}>
-                                                    {currencyAmountToHuman(balance, currency)}
-                                                </TableCell>
+                                                <TableCell>{currency}</TableCell>
                                             </TableRow>
                                         })
                                     }
@@ -334,65 +313,34 @@ export function IncomeAccountsTable({ }) {
                             <AccordionItem value="archived">
                                 <AccordionTrigger>Archive</AccordionTrigger>
                                 <AccordionContent>
-                                    {
-                                        isNil(archived) || isEmpty(archived)
-                                            ? <div></div>
-                                            : <div className="rounded-md border dark:border-zinc-800">
-                                                <Table>
-                                                    <TableHeader>
-                                                        <TableRow>
-                                                            <TableHead>Name</TableHead>
-                                                            <TableHead>Description</TableHead>
-                                                            <TableHead>Kind</TableHead>
-                                                            <TableHead>Balance</TableHead>
+                                    <div className="rounded-md border dark:border-zinc-800">
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead>Name</TableHead>
+                                                    <TableHead>Description</TableHead>
+                                                    <TableHead>Currency</TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            <TableBody>
+                                                {
+                                                    archived.map(({
+                                                        id, name, description, currency
+                                                    }) => {
+                                                        return <TableRow
+                                                            key={`account:${id}`}
+                                                            className="cursor-pointer"
+                                                            onClick={() => navigate(`/accounts/${id}`)}
+                                                        >
+                                                            <TableCell>{name}</TableCell>
+                                                            <TableCell>{description}</TableCell>
+                                                            <TableCell>{currency}</TableCell>
                                                         </TableRow>
-                                                    </TableHeader>
-                                                    <TableBody>
-                                                        {
-                                                            archived.map(({
-                                                                id, kind, name, description, balance, currency, color
-                                                            }) => {
-                                                                const clr = new Color(color)
-                                                                    .to("HSL")
-                                                                    .set({ l: (l) => l >= 50 ? 1 : 100 })
-                                                                    .toString({ precision: 3, format: "rgb" })
-
-                                                                return <TableRow
-                                                                    className="cursor-pointer"
-                                                                    onClick={() => navigate(`/accounts/${id}`)}
-                                                                >
-                                                                    <TableCell>
-                                                                        {name}
-                                                                    </TableCell>
-                                                                    <TableCell>{description}</TableCell>
-                                                                    <TableCell>
-                                                                        <span
-                                                                            className="px-2 py-1 rounded-md text-xs"
-                                                                            style={{
-                                                                                backgroundColor: color,
-                                                                                color: clr
-                                                                            }}
-                                                                        >
-                                                                            {kindToHuman(kind)}
-                                                                        </span>
-                                                                    </TableCell>
-                                                                    <TableCell
-                                                                        className={currencyAmountColor(balance)}
-                                                                    >
-                                                                        {
-                                                                            currencyAmountToHuman(
-                                                                                balance,
-                                                                                currency
-                                                                            )
-                                                                        }
-                                                                    </TableCell>
-                                                                </TableRow>
-                                                            })
-                                                        }
-                                                    </TableBody>
-                                                </Table>
-                                            </div>
-                                    }
+                                                    })
+                                                }
+                                            </TableBody>
+                                        </Table>
+                                    </div>
                                 </AccordionContent>
                             </AccordionItem>
                         </Accordion>
@@ -403,13 +351,32 @@ export function IncomeAccountsTable({ }) {
 }
 
 export function ExpenseAccountsTable({ }) {
-    const { data: accounts, isFetching, isError, error } = useQuery({
-        queryKey: ["accounts", "expenses", "list"],
+    // TODO: Implement children account usage
+
+    const navigate = useNavigate()
+    const {
+        data: active,
+        isFetching: activeIsFetching,
+        isError: activeIsError,
+        error: activeError
+    } = useQuery({
+        queryKey: ["accounts", "expense", "list"],
         queryFn: getAccounts({ kind: [Kind.ExternalExpense] }),
         staleTime: staleTimeDefault
     })
+    const {
+        data: archived,
+        isFetching: archivedIsFetching,
+        isError: archivedIsError,
+        error: archivedError
+    } = useQuery({
+        queryKey: ["accounts", "expense", "list", "archived"],
+        queryFn: getAccounts({ kind: [Kind.ExternalExpense], archived: true }),
+        staleTime: staleTimeDefault
+    })
 
-    if (isError) throw error
+    if (activeIsError) throw activeError
+    if (archivedIsError) throw archivedError
 
     return (
         <Card>
@@ -420,15 +387,79 @@ export function ExpenseAccountsTable({ }) {
                         Recent orders from your store.
                     </CardDescription>
                 </div>
-                {isFetching && <Throbber variant="small" />}
+                {activeIsFetching || archivedIsFetching && <Throbber variant="small" />}
             </CardHeader>
-            <CardContent>
+            <CardContent className="mt-4 space-y-4">
                 {
-                    isNil(accounts) || isEmpty(accounts)
+                    isNil(active) || isEmpty(active)
                         ? <div></div>
-                        : <Table>
-
-                        </Table>
+                        : <div className="rounded-md border dark:border-zinc-800">
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>Name</TableHead>
+                                        <TableHead>Description</TableHead>
+                                        <TableHead>Currency</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {
+                                        active.map(({
+                                            id, name, description, currency
+                                        }) => {
+                                            return <TableRow
+                                                key={`account:${id}`}
+                                                className="cursor-pointer"
+                                                onClick={() => navigate(`/accounts/${id}`)}
+                                            >
+                                                <TableCell>{name}</TableCell>
+                                                <TableCell>{description}</TableCell>
+                                                <TableCell>{currency}</TableCell>
+                                            </TableRow>
+                                        })
+                                    }
+                                </TableBody>
+                            </Table>
+                        </div>
+                }
+                {
+                    isEmpty(archived) || isNil(archived)
+                        ? null
+                        : <Accordion type="single" collapsible={true}>
+                            <AccordionItem value="archived">
+                                <AccordionTrigger>Archive</AccordionTrigger>
+                                <AccordionContent>
+                                    <div className="rounded-md border dark:border-zinc-800">
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead>Name</TableHead>
+                                                    <TableHead>Description</TableHead>
+                                                    <TableHead>Currency</TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            <TableBody>
+                                                {
+                                                    archived.map(({
+                                                        id, name, description, currency
+                                                    }) => {
+                                                        return <TableRow
+                                                            key={`account:${id}`}
+                                                            className="cursor-pointer"
+                                                            onClick={() => navigate(`/accounts/${id}`)}
+                                                        >
+                                                            <TableCell>{name}</TableCell>
+                                                            <TableCell>{description}</TableCell>
+                                                            <TableCell>{currency}</TableCell>
+                                                        </TableRow>
+                                                    })
+                                                }
+                                            </TableBody>
+                                        </Table>
+                                    </div>
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
                 }
             </CardContent>
         </Card>
