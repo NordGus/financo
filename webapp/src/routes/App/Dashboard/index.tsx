81200af98f6012a2c9bf7cf8c1feec76560db852
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 20:22:15 +0000

    adding some widgets to the dashboard component

--- webapp/src/routes/App/Dashboard/index.tsx
+++ webapp/src/routes/App/Dashboard/index.tsx
@@ -1,10 +1,27 @@
+import { AchievementsTracker } from "@components/widgets/achievements";
+import {
+    SummaryAvailableCredit,
+    SummaryCapital,
+    SummaryDebt,
+    SummaryNetWorth
+} from "@components/widgets/summaries";
+
 export default function Dashboard() {
     return (
-        <>
-            <h1 className="text-4xl">
-                Welcome to financo
-            </h1>
-            <p>Your personal finances helper</p>
-        </>
+        <div className="space-y-4">
+            <div>
+                <h1 className="text-4xl">
+                    Welcome to financo
+                </h1>
+                <p>Your personal finances helper</p>
+            </div>
+            <div className="grid grid-cols-12 row-auto gap-4 items-start">
+                <SummaryCapital className="self-stretch col-span-3" />
+                <SummaryDebt className="self-stretch col-span-3" />
+                <SummaryNetWorth className="self-stretch col-span-3" />
+                <SummaryAvailableCredit className="self-stretch col-span-3" />
+                <AchievementsTracker className="col-span-3 row-span-3" />
+            </div>
+        </div>
     )
 }
