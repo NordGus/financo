cc6747b280baaed2d385791ee8427a94886a9a23
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-26 07:34:17 +0000

    implementing achievement tracking component

---
+++ webapp/src/routes/App/Account/Index/achievements.tsx
@@ -0,0 +1,102 @@
+import Goal from "@/types/Goal";
+import { getGoals } from "@api/goals";
+import Progress from "@components/Progress";
+import Throbber from "@components/Throbber";
+import { Button } from "@components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@components/ui/card";
+import { Table, TableBody, TableCell, TableRow } from "@components/ui/table";
+import { staleTimeDefault } from "@queries/Client";
+import { TabsContent } from "@radix-ui/react-tabs";
+import { useQuery } from "@tanstack/react-query";
+import { isEmpty, isNil } from "lodash";
+import { TrophyIcon } from "lucide-react";
+import moment from "moment";
+
+export function AchievementsTracker() {
+    // TODO: Implement goals/achievements endpoints
+    // const query = useQuery({
+    //     queryKey: ['goals', 'tracker'],
+    //     queryFn: getGoals,
+    //     staleTime: staleTimeDefault
+    // })
+
+    const isFetching = false
+    const goals: Goal[] = [
+        {
+            id: 1,
+            name: "My first emergency fund",
+            description: "A journey of a thousand kilometers start with a single step",
+            goal: 100000,
+            currency: "EUR",
+            balance: 100000,
+            position: 1,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        },
+        {
+            id: 2,
+            name: "The emergency fund is getting serious",
+            description: "Leveling the emergency fund to 3 months payment",
+            goal: 600000,
+            currency: "EUR",
+            balance: 133700,
+            position: 2,
+            achievedAt: null,
+            archivedAt: null,
+            createdAt: moment().add({ month: -3 }).toDate().toISOString(),
+            updatedAt: moment().add({ month: -3 }).toDate().toISOString()
+        }
+    ]
+
+    return (
+        <Card>
+            <CardHeader className="flex flex-row items-start justify-between space-y-0 pb-2">
+                <div>
+                    <CardTitle>Achievements</CardTitle>
+                    <CardDescription>
+                        Progression for your active savings goals
+                    </CardDescription>
+                </div>
+                {isFetching && <Throbber variant="small" />}
+            </CardHeader>
+            <CardContent>
+                {
+                    isNil(goals) || isEmpty(goals)
+                        ? <div className="mt-4 space-y-2">
+                            <p>You have no active savings goals</p>
+                            <Button variant="outline" className="">Create New Goal</Button>
+                        </div>
+                        : <Table>
+                            <TableBody>
+                                {
+                                    isNil(goals) || isEmpty(goals)
+                                        ? <TableRow>
+                                            <TableCell className="text-center">
+                                                You have no active savings goals
+                                            </TableCell>
+                                        </TableRow>
+                                        : goals.map((goal) => {
+                                            const progress = goal.balance / goal.goal
+                                            const color = progress < 1 ? "#14b8a6" : "#22c55e"
+
+                                            return <TableRow>
+                                                <TableCell>{goal.name}</TableCell>
+                                                <TableCell>
+                                                    <Progress
+                                                        progress={progress}
+                                                        icon={<TrophyIcon size={20} />}
+                                                        color={color}
+                                                    />
+                                                </TableCell>
+                                            </TableRow>
+                                        })
+                                }
+                            </TableBody>
+                        </Table>
+                }
+            </CardContent>
+        </Card>
+    )
+}
--- webapp/src/routes/App/Account/Index/index.tsx
+++ webapp/src/routes/App/Account/Index/index.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@comp
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
 import { Button } from "@components/ui/button";
 import { Plus } from "lucide-react";
+import { AchievementsTracker } from "./achievements";
 
 export function loader(_queryClient: QueryClient): LoaderFunction {
     return async (_props: LoaderFunctionArgs) => {
@@ -14,72 +15,75 @@ export function loader(_queryClient: QueryClient): LoaderFunction {
 
 export default function Index() {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="space-y-2">
             <div className="grid gap-2 grid-cols-4">
                 <SummaryCapital />
                 <SummaryDebt />
                 <SummaryNetWorth />
                 <SummaryAvailableCredit />
             </div>
-            <Tabs defaultValue={"capital"} className="flex flex-col gap-2">
-                <div className="flex">
-                    <TabsList>
-                        <TabsTrigger value="capital">Capital</TabsTrigger>
-                        <TabsTrigger value="debt">Debt</TabsTrigger>
-                        <TabsTrigger value="income">Income</TabsTrigger>
-                        <TabsTrigger value="expenses">Expenses</TabsTrigger>
-                    </TabsList>
-                    <span className="grow contents-['']"></span>
-                    <Button className="gap-1" asChild={true}>
-                        <Link to="/accounts/new">
-                            <Plus className="h-4 w-4" />
-                            <span className="sr-only sm:not-sr-only">New</span>
-                        </Link>
-                    </Button>
-                </div>
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <TabsContent value="capital">
-                            <CardTitle>Capital</CardTitle>
-                            <CardDescription>
-                                Recent orders from your store.
-                            </CardDescription>
-                        </TabsContent>
-                        <TabsContent value="debt">
-                            <CardTitle>Debts and Credit Lines</CardTitle>
-                            <CardDescription>
-                                Recent orders from your store.
-                            </CardDescription>
-                        </TabsContent>
-                        <TabsContent value="income">
-                            <CardTitle>Income Sources</CardTitle>
-                            <CardDescription>
-                                Recent orders from your store.
-                            </CardDescription>
-                        </TabsContent>
-                        <TabsContent value="expenses">
-                            <CardTitle>Expenses</CardTitle>
-                            <CardDescription>
-                                Recent orders from your store.
-                            </CardDescription>
-                        </TabsContent>
-                    </CardHeader>
-                    <CardContent>
-                        <TabsContent value="capital">
-                            <h1>capital</h1>
-                        </TabsContent>
-                        <TabsContent value="debt">
-                            <h1>debt</h1>
-                        </TabsContent>
-                        <TabsContent value="income">
-                            <h1>income</h1>
-                        </TabsContent>
-                        <TabsContent value="expenses">
-                            <h1>expenses</h1>
-                        </TabsContent>
-                    </CardContent>
-                </Card>
-            </Tabs>
+            <div className="grid grid-cols-3 gap-2 items-start">
+                <Tabs defaultValue={"capital"} className="flex flex-col gap-2 col-span-2">
+                    <div className="flex">
+                        <TabsList>
+                            <TabsTrigger value="capital">Capital</TabsTrigger>
+                            <TabsTrigger value="debt">Debt</TabsTrigger>
+                            <TabsTrigger value="income">Income</TabsTrigger>
+                            <TabsTrigger value="expenses">Expenses</TabsTrigger>
+                        </TabsList>
+                        <span className="grow contents-['']"></span>
+                        <Button className="gap-1" asChild={true}>
+                            <Link to="/accounts/new">
+                                <Plus className="h-4 w-4" />
+                                <span className="sr-only sm:not-sr-only">New</span>
+                            </Link>
+                        </Button>
+                    </div>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <TabsContent value="capital">
+                                <CardTitle>Capital</CardTitle>
+                                <CardDescription>
+                                    Recent orders from your store.
+                                </CardDescription>
+                            </TabsContent>
+                            <TabsContent value="debt">
+                                <CardTitle>Debts and Credit Lines</CardTitle>
+                                <CardDescription>
+                                    Recent orders from your store.
+                                </CardDescription>
+                            </TabsContent>
+                            <TabsContent value="income">
+                                <CardTitle>Income Sources</CardTitle>
+                                <CardDescription>
+                                    Recent orders from your store.
+                                </CardDescription>
+                            </TabsContent>
+                            <TabsContent value="expenses">
+                                <CardTitle>Expenses</CardTitle>
+                                <CardDescription>
+                                    Recent orders from your store.
+                                </CardDescription>
+                            </TabsContent>
+                        </CardHeader>
+                        <CardContent>
+                            <TabsContent value="capital">
+                                <h1>capital</h1>
+                            </TabsContent>
+                            <TabsContent value="debt">
+                                <h1>debt</h1>
+                            </TabsContent>
+                            <TabsContent value="income">
+                                <h1>income</h1>
+                            </TabsContent>
+                            <TabsContent value="expenses">
+                                <h1>expenses</h1>
+                            </TabsContent>
+                        </CardContent>
+                    </Card>
+                </Tabs>
+                <AchievementsTracker />
+            </div>
         </div>
     )
 }
