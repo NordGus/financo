87083bd0b6730443c27b90259229c7986997848f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-27 22:45:21 +0000

    fixing lint

--- webapp/src/helpers/isEmptyParam.ts
+++ webapp/src/helpers/isEmptyParam.ts
@@ -1,6 +1,6 @@
 import { isEmpty, isNil, isNaN } from "lodash"
 
-export default function isEmptyParam(param: any): boolean {
+export default function isEmptyParam(param: unknown): boolean {
     if (typeof param === 'boolean') return false
     if (isEmpty(param)) return true
     if (isNil(param)) return true
