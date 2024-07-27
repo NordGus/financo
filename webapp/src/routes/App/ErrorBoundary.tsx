9f95ae003b1f359abaa225f344b434a771b2917c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-27 22:33:51 +0000

    preventing nightly development flashbang when the error boundary in hit

--- webapp/src/routes/App/ErrorBoundary.tsx
+++ webapp/src/routes/App/ErrorBoundary.tsx
@@ -7,7 +7,7 @@ export default function ErrorBoundary() {
 
     return (
         <div
-            className="w-full h-[100dvh] flex flex-col justify-center items-center gap-5"
+            className="w-full h-[100dvh] flex flex-col justify-center items-center gap-5 bg-zinc-50 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50"
         >
             <h1 className="text-4xl mb-5 font-bold">Oops!</h1>
             <p className="opacity-75">Sorry, an unexpected error has occurred.</p>
