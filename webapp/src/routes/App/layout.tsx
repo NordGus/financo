14b76845d94630c8ee0a0df93e514ff680602a77
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-28 17:12:51 +0000

    cleaning code style

--- webapp/src/routes/App/layout.tsx
+++ webapp/src/routes/App/layout.tsx
@@ -17,15 +17,47 @@ export default function Layout() {
         <div className='w-full h-[100dvh] grid grid-cols-[min-content_minmax(0,_1fr)] dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50'
         >
             <nav className='flex flex-col gap-2 p-2 h-full m-0 border-r border-zinc-200 dark:border-zinc-800 bg-transparent shadow-sm'>
-                <NavLink name="Dashboard" path="" icon={<ChartCandlestickIcon />} />
-                <NavLink name="Accounts" path="accounts" icon={<VaultIcon />} />
-                <NavLink name="Ledger" path="books" icon={<BookMarkedIcon />} />
-                <NavLink name="Budgets" path="budgets" icon={<NotebookTabsIcon />} />
-                <NavLink name="Payment Plans" path="plans" icon={<RouteIcon />} />
-                <NavLink name="Savings Goals" path="goals" icon={<GoalIcon />} />
+                <NavLink
+                    name="Dashboard"
+                    path=""
+                    icon={<ChartCandlestickIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Accounts"
+                    path="accounts"
+                    icon={<VaultIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Ledger"
+                    path="books"
+                    icon={<BookMarkedIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Budgets"
+                    path="budgets"
+                    icon={<NotebookTabsIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Payment Plans"
+                    path="plans"
+                    icon={<RouteIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Savings Goals"
+                    path="goals"
+                    icon={<GoalIcon className="w-6 h-6" />}
+                />
                 <span className="grow contents-['']"></span>
-                <NavLink name="Achievements" path="achievements" icon={<TrophyIcon />} />
-                <NavLink name="Configuration" path="settings" icon={<SettingsIcon />} />
+                <NavLink
+                    name="Achievements"
+                    path="achievements"
+                    icon={<TrophyIcon className="w-6 h-6" />}
+                />
+                <NavLink
+                    name="Configuration"
+                    path="settings"
+                    icon={<SettingsIcon className="w-6 h-6" />}
+                />
             </nav>
             <main className='block min-h-full overflow-y-auto p-2'>
                 <Outlet />
