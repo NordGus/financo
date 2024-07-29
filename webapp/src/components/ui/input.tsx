49dc582552e2fb8ee096646952eb3d9fca4b74c8
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-29 18:56:32 +0000

    adding styles for color input type

--- webapp/src/components/ui/input.tsx
+++ webapp/src/components/ui/input.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -12,6 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+          type === 'color' && "m-0 p-0 [&::-moz-color-swatch]:border-0 [&::-moz-color-swatch]:border-transparent [&::-webkit-color-swatch-wrapper]:border-0 [&::-webkit-color-swatch-wrapper]:border-transparent",
           className
         )}
         ref={ref}
