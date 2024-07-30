4e428d5f57d60ac69e9b5054355e4a675d58074a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-07-30 22:08:19 +0000

    changing design

--- webapp/src/routes/App/Account/Index/index.tsx
+++ webapp/src/routes/App/Account/Index/index.tsx
@@ -9,6 +9,13 @@ import {
     ExpenseAccountsTable,
     IncomeAccountsTable
 } from "./accounts";
+import { GoalsTracker } from "@components/widgets/goals";
+import {
+    SummaryCapital,
+    SummaryDebt,
+    SummaryNetWorth,
+    SummaryAvailableCredit
+} from "@components/widgets/summaries";
 
 export function loader(_queryClient: QueryClient): LoaderFunction {
     return async (_props: LoaderFunctionArgs) => {
@@ -18,31 +25,42 @@ export function loader(_queryClient: QueryClient): LoaderFunction {
 
 export default function Index() {
     return (
-        <Tabs defaultValue={"capital"} className="flex flex-col gap-4 m-0">
-            <div className="flex">
-                <TabsList>
-                    <TabsTrigger value="capital">Capital</TabsTrigger>
-                    <TabsTrigger value="debt">Debt</TabsTrigger>
-                    <TabsTrigger value="income">Income</TabsTrigger>
-                    <TabsTrigger value="expenses">Expenses</TabsTrigger>
-                </TabsList>
-                <span className="grow contents-['']"></span>
-                <Button className="gap-1" asChild={true}>
-                    <Link to="/accounts/new">New</Link>
-                </Button>
+        <div className="grid grid-cols-3 gap-4 items-start">
+            <div className="grid grid-cols-2 gap-4">
+                <SummaryCapital />
+                <SummaryDebt />
+                <SummaryNetWorth />
+                <SummaryAvailableCredit />
+                <GoalsTracker className="col-span-2" />
             </div>
-            <TabsContent value="capital" className="m-0">
-                <CapitalAccountsTable />
-            </TabsContent>
-            <TabsContent value="debt" className="m-0">
-                <DebtAccountsTable />
-            </TabsContent>
-            <TabsContent value="income" className="m-0">
-                <IncomeAccountsTable />
-            </TabsContent>
-            <TabsContent value="expenses" className="m-0">
-                <ExpenseAccountsTable />
-            </TabsContent>
-        </Tabs>
+            <div className="col-span-2">
+                <Tabs defaultValue={"capital"} className="flex flex-col gap-4 m-0">
+                    <div className="flex">
+                        <TabsList>
+                            <TabsTrigger value="capital">Capital</TabsTrigger>
+                            <TabsTrigger value="debt">Debt</TabsTrigger>
+                            <TabsTrigger value="income">Income</TabsTrigger>
+                            <TabsTrigger value="expenses">Expenses</TabsTrigger>
+                        </TabsList>
+                        <span className="grow contents-['']"></span>
+                        <Button className="gap-1" asChild={true}>
+                            <Link to="/accounts/new">New</Link>
+                        </Button>
+                    </div>
+                    <TabsContent value="capital" className="m-0">
+                        <CapitalAccountsTable />
+                    </TabsContent>
+                    <TabsContent value="debt" className="m-0">
+                        <DebtAccountsTable />
+                    </TabsContent>
+                    <TabsContent value="income" className="m-0">
+                        <IncomeAccountsTable />
+                    </TabsContent>
+                    <TabsContent value="expenses" className="m-0">
+                        <ExpenseAccountsTable />
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </div>
     )
 }
