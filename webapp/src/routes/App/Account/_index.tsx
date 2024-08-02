38cde89207c688b6e024673ddc3e6b532c51c2e5
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-02 21:09:12 +0000

    making new account button invisible when the user is on the new account view

--- webapp/src/routes/App/Account/_index.tsx
+++ webapp/src/routes/App/Account/_index.tsx
@@ -1,8 +1,9 @@
 import { QueryClient } from "@tanstack/react-query";
-import { Link, LoaderFunction, LoaderFunctionArgs, Outlet } from "react-router-dom";
+import { Link, LoaderFunction, LoaderFunctionArgs, Outlet, useMatch } from "react-router-dom";
 
 import Breadcrumbs from "@components/breadcrumbs";
 import { Button } from "@components/ui/button";
+import { cn } from "@/lib/utils";
 
 export function loader(_queryClient: QueryClient): LoaderFunction {
     return async (_props: LoaderFunctionArgs) => {
@@ -11,13 +12,20 @@ export function loader(_queryClient: QueryClient): LoaderFunction {
 }
 
 export default function Layout() {
+    const match = useMatch("/accounts/new")
+
     return (
         <div className="gap-4 flex flex-col">
             <div className="flex items-center">
                 <Breadcrumbs />
                 <span className="grow contents-['']"></span>
                 <Button asChild={true}>
-                    <Link to="/accounts/new">New Account</Link>
+                    <Link
+                        to="/accounts/new"
+                        className={cn(match && "pointer-events-none opacity-0")}
+                    >
+                        New Account
+                    </Link>
                 </Button>
             </div>
             <Outlet />
