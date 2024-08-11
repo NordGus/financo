99c8125a62cf479e1ba595e991cce8bddc53875a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-11 13:57:24 +0000

    implementing select handler and query

--- server/accounts/http/handlers/select.go
+++ server/accounts/http/handlers/select.go
@@ -2,12 +2,9 @@ package handlers
 
 import (
 	"encoding/json"
+	"financo/server/accounts/queries/select_query"
 	"financo/server/types/generic/context_key"
 	"financo/server/types/records/account"
-	"financo/server/types/shared/color"
-	"financo/server/types/shared/currency"
-	"financo/server/types/shared/icon"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -15,131 +12,13 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const (
-	selectQuery = `
-WITH
-    active_transactions (
-        id,
-        source_id,
-        target_id,
-        issued_at,
-        executed_at,
-        source_amount,
-        target_amount
-    ) AS (
-        SELECT
-            id,
-            source_id,
-            target_id,
-            issued_at,
-            executed_at,
-            source_amount,
-            target_amount
-        FROM transactions
-        WHERE
-            deleted_at IS NULL
-            AND executed_at IS NOT NULL
-            AND executed_at <= NOW()
-    ),
-    children (
-        id,
-        parent_id,
-        kind,
-        currency,
-        name,
-        description,
-        balance,
-        capital,
-        color,
-        icon,
-        archived_at,
-        created_at,
-        updated_at
-    ) AS (
-        SELECT
-            acc.id,
-            acc.parent_id,
-            acc.kind,
-            acc.currency,
-            acc.name,
-            acc.description,
-            SUM(
-                CASE
-                    WHEN tr.source_id = acc.id THEN - tr.source_amount
-					WHEN tr.target_id = acc.id THEN tr.target_amount
-                    ELSE 0
-                END
-            ) AS balance,
-            acc.capital,
-            acc.color,
-            acc.icon,
-            acc.archived_at,
-            acc.created_at,
-            acc.updated_at
-        FROM
-            accounts acc
-            LEFT JOIN active_transactions tr ON tr.source_id = acc.id
-            OR tr.target_id = acc.id
-        WHERE
-            acc.parent_id IS NOT NULL
-			AND acc.deleted_at IS NULL
-            AND acc.archived_at IS NULL
-            AND acc.kind != $1
-        GROUP BY
-            acc.id
-    )
-SELECT
-    acc.id,
-    acc.kind,
-    acc.currency,
-    acc.name,
-    acc.description,
-    SUM(
-        CASE
-			WHEN blc.source_id = acc.id THEN - blc.source_amount
-			WHEN blc.target_id = acc.id THEN blc.target_amount
-			ELSE 0
-		END
-    ) AS balance,
-    acc.capital,
-    acc.color,
-    acc.icon,
-    acc.archived_at,
-    acc.created_at,
-    acc.updated_at,
-    child.id,
-    MAX(child.kind) AS child_kind,
-    MAX(child.currency) AS child_currency,
-    MAX(child.name) AS child_name,
-    MAX(child.description) AS child_description,
-    MAX(child.balance)::bigint AS child_balance,
-    MAX(child.capital) AS child_capital,
-    MAX(child.color) AS child_color,
-    MAX(child.icon) AS child_icon,
-    MAX(child.archived_at) AS child_archived_at,
-    MAX(child.created_at) AS child_created_at,
-    MAX(child.updated_at) AS child_updated_at
-FROM
-    accounts acc
-    LEFT JOIN children child ON child.parent_id = acc.id
-    LEFT JOIN active_transactions blc ON blc.source_id = acc.id
-    OR blc.target_id = acc.id
-WHERE
-    acc.kind = ANY ($2)
-    AND acc.parent_id IS NULL
-    AND acc.deleted_at IS NULL
-	`
-)
-
 func Select(w http.ResponseWriter, r *http.Request) {
 	var (
-		ctx     = r.Context()
-		kinds   = make([]account.Kind, 0, 7)
-		results = make([]Preview, 0, 10)
-		query   = selectQuery
-		idx     = 0
+		kinds    = make([]account.Kind, 0, 7)
+		archived = r.URL.Query().Get("archived") == "true"
 	)
-	db, ok := ctx.Value(context_key.DB).(*pgxpool.Conn)
+
+	conn, ok := r.Context().Value(context_key.DB).(*pgxpool.Conn)
 	if !ok {
 		log.Println("failed to retrieved database connection")
 		http.Error(
@@ -150,42 +29,13 @@ func Select(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.URL.Query().Has(kindKey) {
-		for _, k := range strings.Split(r.URL.Query().Get(kindKey), ",") {
-			kind := account.Kind(k)
-
-			if err := kindsSelectValidation(kind); err != nil {
-				log.Println(err)
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-
-			kinds = append(kinds, kind)
-		}
-	} else {
-		kinds = append(
-			kinds,
-			account.CapitalNormal,
-			account.CapitalSavings,
-			account.DebtCredit,
-			account.DebtLoan,
-			account.DebtPersonal,
-			account.ExternalExpense,
-			account.ExternalIncome,
-		)
+	for _, k := range strings.Split(r.URL.Query().Get("kind"), ",") {
+		kinds = append(kinds, account.Kind(k))
 	}
 
-	if r.URL.Query().Has(archivedKey) && r.URL.Query().Get(archivedKey) == "true" {
-		query += " AND acc.archived_at IS NOT NULL"
-	} else {
-		query += " AND acc.archived_at IS NULL"
-	}
-
-	query += " GROUP BY child.id, acc.id"
-
-	rows, err := db.Query(ctx, query, account.SystemHistoric, kinds)
+	res, err := select_query.New(kinds, archived, conn).Find(r.Context())
 	if err != nil {
-		log.Println("failed query", err)
+		log.Println("query failed", err)
 		http.Error(
 			w,
 			http.StatusText(http.StatusInternalServerError),
@@ -193,65 +43,8 @@ func Select(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		var (
-			acc   Preview
-			child Child
-		)
-
-		err = rows.Scan(
-			&acc.ID,
-			&acc.Kind,
-			&acc.Currency,
-			&acc.Name,
-			&acc.Description,
-			&acc.Balance,
-			&acc.Capital,
-			&acc.Color,
-			&acc.Icon,
-			&acc.ArchivedAt,
-			&acc.CreatedAt,
-			&acc.UpdatedAt,
-			&child.ID,
-			&child.Kind,
-			&child.Currency,
-			&child.Name,
-			&child.Description,
-			&child.Balance,
-			&child.Capital,
-			&child.Color,
-			&child.Icon,
-			&child.ArchivedAt,
-			&child.CreatedAt,
-			&child.UpdatedAt,
-		)
-		if err != nil {
-			log.Println("failed scan", err)
-			http.Error(
-				w,
-				http.StatusText(http.StatusInternalServerError),
-				http.StatusInternalServerError,
-			)
-			return
-		}
-
-		if len(results) == 0 {
-			results = append(results, acc)
-			results[idx].Children = make([]Preview, 0, 5)
-		} else if results[idx].ID != acc.ID {
-			idx++
-			results = append(results, acc)
-			results[idx].Children = make([]Preview, 0, 5)
-		}
-
-		if child.ID.Valid {
-			results[idx].Children = append(results[idx].Children, buildSelectChild(child))
-		}
-	}
-
-	response, err := json.Marshal(results)
+	response, err := json.Marshal(&res)
 	if err != nil {
 		log.Println("failed json Marshal", err)
 		http.Error(
@@ -264,6 +57,7 @@ func Select(w http.ResponseWriter, r *http.Request) {
 
 	_, err = w.Write(response)
 	if err != nil {
+		log.Println("failed to write response", err)
 		http.Error(
 			w,
 			http.StatusText(http.StatusInternalServerError),
@@ -272,41 +66,5 @@ func Select(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
 	w.Header().Add("Content-Type", "application/json")
-}
-
-func kindsSelectValidation(k account.Kind) error {
-	kinds := map[account.Kind]bool{
-		account.CapitalNormal:   true,
-		account.CapitalSavings:  true,
-		account.DebtPersonal:    true,
-		account.DebtLoan:        true,
-		account.DebtCredit:      true,
-		account.ExternalIncome:  true,
-		account.ExternalExpense: true,
-	}
-
-	if _, ok := kinds[k]; !ok {
-		return fmt.Errorf("invalid account kind: %s", k)
-	}
-
-	return nil
-}
-
-func buildSelectChild(child Child) Preview {
-	return Preview{
-		ID:          child.ID.Val,
-		Kind:        account.Kind(child.Kind.Val),
-		Currency:    currency.Type(child.Currency.Val),
-		Name:        child.Name.Val,
-		Description: child.Description,
-		Balance:     child.Balance.Val,
-		Capital:     child.Capital.Val,
-		Color:       color.Type(child.Color.Val),
-		Icon:        icon.Type(child.Icon.Val),
-		ArchivedAt:  child.ArchivedAt,
-		CreatedAt:   child.CreatedAt.Val,
-		UpdatedAt:   child.CreatedAt.Val,
-	}
 }
