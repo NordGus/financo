a2fd816e990a91945c29e31d1ed772effed70057
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-15 01:35:33 +0000

    cleaning code

--- webapp/src/routes/App/Ledger/Transactions/index.ts
+++ webapp/src/routes/App/Ledger/Transactions/index.ts
@@ -1,11 +1,5 @@
 import Form from "./form";
-import History from "./history";
-import Pending from "./pending";
-import Upcoming from "./upcoming";
 
 export default {
-    Form,
-    History,
-    Pending,
-    Upcoming
+    Form
 }
