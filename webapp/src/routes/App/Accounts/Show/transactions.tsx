09c9e35b8393c437a5ce04a48e246be8a87e59b2
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-15 12:55:14 +0000

    redesigning transaction history

--- webapp/src/routes/App/Accounts/Show/transactions.tsx
+++ webapp/src/routes/App/Accounts/Show/transactions.tsx
@@ -12,10 +12,15 @@ import Transaction from "@/types/Transaction";
 
 import currencyAmountToHuman from "@helpers/currencyAmountToHuman";
 import currencyAmountColor from "@helpers/currencyAmountColor";
+import kindToHuman from "@helpers/account/kindToHuman";
 import { cn } from "@/lib/utils";
 
-import { getTransactions, ListFilters } from "@api/transactions";
-import { pendingTransactionsQueryOptions, upcomingTransactionsQueryOptions } from "@queries/accounts";
+import {
+    getPendingTransactions,
+    getTransactions,
+    ListFilters,
+    PendingFilters
+} from "@api/transactions";
 
 import { Throbber } from "@components/Throbber";
 import {
@@ -26,43 +31,146 @@ import {
     CardHeader,
     CardTitle
 } from "@components/ui/card";
-import { Table, TableBody, TableCell, TableRow } from "@components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@components/ui/table";
 import { Button } from "@components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@components/ui/popover";
 import { Calendar } from "@components/ui/calendar";
+import { Accordion } from "@components/ui/accordion";
+import { accountContrastColor } from "@helpers/account/accountContrastColor";
 
 interface Props {
     account: Detailed
     className?: string
 }
 
-function defaultHistoryFilters(account: Detailed): ListFilters {
+function historyFilters(account: Detailed, filters: ListFilters): ListFilters {
     return {
-        executedFrom: moment().startOf('month').startOf('day').toISOString(),
-        executedUntil: moment().endOf('day').toISOString(),
-        account: [account.id]
+        executedFrom: filters.executedFrom || moment().startOf('month').startOf('day').toISOString(),
+        executedUntil: filters.executedUntil || moment().endOf('day').toISOString(),
+        account: [account.id, ...(filters.account || [])]
+    }
+}
+
+function upcomingFilters(account: Detailed, filters: ListFilters): ListFilters {
+    return {
+        executedFrom: moment().toISOString(),
+        executedUntil: moment().add({ month: 1 }).toISOString(),
+        account: [account.id, ...(filters.account || [])]
+    }
+}
+
+function pendingFilters(account: Detailed, filters: ListFilters): PendingFilters {
+    return {
+        account: [account.id, ...(filters.account || [])]
     }
 }
 
 // [ ] make endpoints specialized to retrieved transactions for a given account
 export function Transactions({ account, className }: Props) {
-    const [filters, setFilters] = useState<ListFilters>(defaultHistoryFilters(account))
+    const [filters, setFilters] = useState<ListFilters>(historyFilters(account, {}))
+    const [date, setDate] = useState<DateRange>({
+        from: moment(filters.executedFrom).toDate(),
+        to: moment(filters.executedUntil).toDate()
+    })
+    const [clearable, setClearable] = useState(
+        !isEqual(
+            { from: date.from?.toISOString(), to: date.to?.toISOString() },
+            { from: historyFilters(account, {}).executedFrom, to: historyFilters(account, {}).executedUntil }
+        )
+    )
 
-    const pendingQuery = useSuspenseQuery(pendingTransactionsQueryOptions(account.id))
-    const upcomingQuery = useSuspenseQuery(upcomingTransactionsQueryOptions(account.id))
+    const pendingMutation = useMutation({
+        mutationKey: ["transactions", "pending", "account", account.id],
+        mutationFn: (filters: ListFilters) => getPendingTransactions(pendingFilters(account, filters))()
+    })
+    const upcomingMutation = useMutation({
+        mutationKey: ["transactions", "upcoming", "account", account.id],
+        mutationFn: (filters: ListFilters) => getTransactions(upcomingFilters(account, filters))()
+    })
     const historyMutation = useMutation({
         mutationKey: ["transactions", "account", account.id],
         mutationFn: (filters: ListFilters) => getTransactions(filters)()
     })
 
-    useEffect(() => historyMutation.mutate(filters), [filters, account.updatedAt])
+    useEffect(() => {
+        historyMutation.mutate(historyFilters(account, filters))
+        pendingMutation.mutate(pendingFilters(account, filters))
+        upcomingMutation.mutate(upcomingFilters(account, filters))
+    }, [filters, account.updatedAt])
 
     return (
-        <div className={cn("", className)}>
+        <div className={cn("flex flex-col gap-4", className)}>
+            <div className={cn("flex justify-end gap-4")}>
+                {
+                    clearable && <Button
+                        variant="link"
+                        onClick={() => {
+                            const reset = historyFilters(account, {})
+
+                            setDate({
+                                from: moment(reset.executedFrom).toDate(),
+                                to: moment(reset.executedUntil).toDate()
+                            })
+                            setFilters(reset)
+                            setClearable(false)
+                        }}
+                    >
+                        Clear
+                    </Button>
+                }
+                <Popover>
+                    <PopoverTrigger asChild={true}>
+                        <Button
+                            id="date"
+                            variant="outline"
+                            className={cn(
+                                "min-w-[10rem] justify-start text-left font-normal",
+                                !date && "text-zinc-500"
+                            )}
+                        >
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {date?.from ? (
+                                date.to ? (
+                                    <>
+                                        {format(date.from, "LLL dd, y")} -{" "}
+                                        {format(date.to, "LLL dd, y")}
+                                    </>
+                                ) : (
+                                    format(date.from, "LLL dd, y")
+                                )
+                            ) : (
+                                <span>Pick a date</span>
+                            )}
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                            initialFocus={true}
+                            mode="range"
+                            defaultMonth={date?.from}
+                            selected={date}
+                            onSelect={(value) => {
+                                setDate(value!)
+                                setFilters(historyFilters(account, {
+                                    ...filters,
+                                    executedFrom: value!.from!.toISOString(),
+                                    executedUntil: value!.to!.toISOString(),
+                                }))
+                                setClearable(true)
+                            }}
+                            numberOfMonths={2}
+                        />
+                    </PopoverContent>
+                </Popover>
+            </div>
             <Card>
                 <CardHeader>
                     <CardTitle>Transactions</CardTitle>
                 </CardHeader>
+                <Accordion type="multiple">
+
+                </Accordion>
+                <History account={account} mutation={historyMutation} />
             </Card>
         </div>
     )
@@ -159,132 +267,46 @@ export function UpcomingTransactions({
     )
 }
 
-export function TransactionHistory({
-    account: { id: accountID },
+function History({
+    account,
     mutation: { data: transactions, isPending, isError, error },
-    defaultFilters,
-    filtersState: [filters, setFilters],
-    className
 }: {
-    account: { id: number },
+    account: Detailed,
     mutation: UseMutationResult<Transaction[], Error, ListFilters, unknown>,
-    defaultFilters: () => ListFilters,
-    filtersState: [ListFilters, Dispatch<SetStateAction<ListFilters>>]
-    className?: string,
 }) {
-    const [date, setDate] = useState<DateRange | undefined>({
-        from: moment(filters.executedFrom).toDate(),
-        to: moment(filters.executedUntil).toDate()
-    })
-    const [clearable, setClearable] = useState(
-        !isEqual(
-            { from: date?.from?.toISOString(), to: date?.to?.toISOString() },
-            { from: defaultFilters().executedFrom, to: defaultFilters().executedUntil }
-        )
-    )
 
     if (isError) throw error
+    if (isPending) return (
+        <CardContent>
+            <div className="flex flex-row justify-center items-center">
+                <Throbber /> <span>Fetching</span>
+            </div>
+        </CardContent>
+    )
+    if (isEmpty(transactions) || isNil(transactions)) return (
+        <CardContent>
+            <p>This account doesn't have any transactions for the period</p>
+            <Button variant="outline" asChild={true}>
+                <Link to="/accounts/new">Create New Transaction</Link>
+            </Button>
+        </CardContent>
+    )
 
-    return (<>
-        <div className={cn("flex justify-between")}>
-            <Popover>
-                <PopoverTrigger asChild={true}>
-                    <Button
-                        id="date"
-                        variant="outline"
-                        className={cn(
-                            "min-w-[10rem] justify-start text-left font-normal",
-                            !date && "text-zinc-500"
-                        )}
-                    >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date?.from ? (
-                            date.to ? (
-                                <>
-                                    {format(date.from, "LLL dd, y")} -{" "}
-                                    {format(date.to, "LLL dd, y")}
-                                </>
-                            ) : (
-                                format(date.from, "LLL dd, y")
-                            )
-                        ) : (
-                            <span>Pick a date</span>
-                        )}
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                        initialFocus={true}
-                        mode="range"
-                        defaultMonth={date?.from}
-                        selected={date}
-                        onSelect={(value) => {
-                            setDate(value)
-                            setFilters({
-                                account: [accountID],
-                                executedFrom: value?.from?.toISOString(),
-                                executedUntil: value?.to?.toISOString(),
-                            })
-                            setClearable(true)
-                        }}
-                        numberOfMonths={2}
-                    />
-                </PopoverContent>
-            </Popover>
-            {
-                clearable && <Button
-                    variant="link"
-                    onClick={() => {
-                        setDate({
-                            from: moment(defaultFilters().executedFrom).toDate(),
-                            to: moment(defaultFilters().executedUntil).toDate()
-                        })
-                        setFilters(defaultFilters())
-                        setClearable(false)
-                    }}
-                >
-                    Clear
-                </Button>
-            }
-        </div>
-        <Card className={className}>
-            <CardHeader className="flex flex-row items-start justify-between space-y-0">
-                <div>
-                    <CardTitle>Transactions</CardTitle>
-                    <CardDescription>
-                        Account's transaction history
-                    </CardDescription>
-                </div>
-                {isPending && <Throbber variant="small" />}
-            </CardHeader>
-            <CardContent className="space-y-4">
-                {
-                    (isEmpty(transactions) || isNil(transactions))
-                        ? <>
-                            <p>This account doesn't have any transactions for the period</p>
-                            <Button variant="outline" asChild={true}>
-                                <Link to="/accounts/new">Create New Transaction</Link>
-                            </Button>
-                        </>
-                        : <TransactionsTable
-                            accountID={accountID}
-                            transactions={transactions}
-                            sortByFn={
-                                (a, b) => Date.parse(b.executedAt!) - Date.parse(a.executedAt!)
-                            }
-                            groupByFn={({ executedAt }) => executedAt!}
-                            withUpcoming={true}
-                        />
-                }
-            </CardContent>
-        </Card>
-    </>)
+    return (
+        <TransactionsTable
+            account={account}
+            transactions={transactions}
+            sortByFn={(a, b) => Date.parse(b.executedAt!) - Date.parse(a.executedAt!)}
+            groupByFn={({ executedAt }) => executedAt!}
+            withUpcoming={true}
+        />
+    )
 }
 
 function TransactionsTable({
-    accountID, transactions, sortByFn, groupByFn, withUpcoming
+    account, transactions, sortByFn, groupByFn, withUpcoming
 }: {
-    accountID: number
+    account: Detailed
     transactions: Transaction[]
     sortByFn: (a: Transaction, b: Transaction) => number
     groupByFn: (tr: Transaction) => string,
@@ -292,55 +314,91 @@ function TransactionsTable({
 }) {
     const upcomingDate = moment().endOf('day').toDate()
 
-    return Object.entries(groupBy(transactions.sort(sortByFn), groupByFn))
-        .map(([date, tr]) => {
-            return {
-                date: moment(date).toDate(),
-                transactions: tr.sort((a, b) => Date.parse(a.updatedAt) - Date.parse(b.updatedAt))
-            }
-        })
-        .map(({ date, transactions }) => (
-            <div
-                key={`transactions:pending:${date.toISOString()}`}
-                className={cn("", withUpcoming && date > upcomingDate && "opacity-50")}
-            >
-                <h4 className="text-xl">
+    return (
+        <Table>
+            <TableBody>
+                {Object.entries(groupBy(transactions.sort(sortByFn), groupByFn))
+                    .map(([date, tr]) => {
+                        return {
+                            date: moment(date).toDate(),
+                            transactions: tr.sort((a, b) => Date.parse(a.updatedAt) - Date.parse(b.updatedAt))
+                        }
+                    })
+                    .map(({ date, transactions }) => (
+                        <SectionRow
+                            key={`transactions:${date.toISOString()}:${account.id}`}
+                            date={date}
+                            transactions={transactions}
+                            account={account}
+                            withUpcoming={withUpcoming}
+                            upcomingDate={upcomingDate}
+                        />
+                    ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+interface SectionProps {
+    account: Detailed
+    date: Date
+    transactions: Transaction[]
+    withUpcoming: boolean
+    upcomingDate: Date
+}
+
+function SectionRow({ account: { id: accountID }, date, transactions, withUpcoming, upcomingDate }: SectionProps) {
+    const cellClassName = withUpcoming && date > upcomingDate ? "opacity-50" : ""
+
+    return (
+        <>
+            <TableRow className={cn(date)}>
+                <TableHead colSpan={3}>
                     {date.toLocaleDateString(undefined, {
                         weekday: "long",
                         year: "numeric",
                         month: "long",
                         day: "numeric"
                     })}
-                </h4>
-                <Table>
-                    <TableBody>
-                        {transactions.map(({ id, source, target, sourceAmount, targetAmount }) => {
-                            const external = source.id === accountID ? target : source
-                            const account = source.id === accountID ? source : target
-                            const amount = source.id === accountID ? sourceAmount : targetAmount
+                </TableHead>
+            </TableRow>
+            {
+                transactions.map(({ id, source, target, sourceAmount, targetAmount }) => {
+                    const external = source.id === accountID ? target : source
+                    const account = source.id === accountID ? source : target
+                    const amount = source.id === accountID ? sourceAmount : targetAmount
 
-                            return <TableRow key={`transaction:${id}`}>
-                                <TableCell>
-                                    {
-                                        isNil(external.parent)
-                                            ? external.name
-                                            : `${external.parent.name} (${external.name})`
-                                    }
-                                </TableCell>
-                                <TableCell
-                                    className={
-                                        cn(
-                                            "text-right",
-                                            currencyAmountColor(account.id === source.id ? -1 : 1)
-                                        )
-                                    }
-                                >
-                                    {currencyAmountToHuman(amount, account.currency)}
-                                </TableCell>
-                            </TableRow>
-                        })}
-                    </TableBody>
-                </Table>
-            </div>
-        ))
+                    return (<TableRow key={`transaction:&{accountID}:${id}:${withUpcoming}`}>
+                        <TableCell
+                            className={cn("text-center w-[5dvw]", cellClassName)}
+                            style={{
+                                backgroundColor: external.color,
+                                color: accountContrastColor(external.color)
+                            }}
+                        >
+                            {kindToHuman(external.kind)}
+                        </TableCell>
+                        <TableCell className={cellClassName}>
+                            {
+                                isNil(external.parent)
+                                    ? `${external.name}`
+                                    : `${external.parent.name} (${external.name})`
+                            }
+                        </TableCell>
+                        <TableCell
+                            className={
+                                cn(
+                                    "text-right w-[7.5dvw] font-semibold",
+                                    currencyAmountColor(account.id === source.id ? -1 : 1),
+                                    cellClassName
+                                )
+                            }
+                        >
+                            {currencyAmountToHuman(amount, account.currency)}
+                        </TableCell>
+                    </TableRow>)
+                })
+            }
+        </>
+    )
 }
