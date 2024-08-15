fd5d4b69c6b1148d0a615ceae6c466b3689da240
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-15 01:15:54 +0000

    finishing redesign for the ledger transaction list

--- webapp/src/routes/App/Ledger/Index/index.tsx
+++ webapp/src/routes/App/Ledger/Index/index.tsx
@@ -1,14 +1,25 @@
 import { LoaderFunction, LoaderFunctionArgs, useOutletContext } from "react-router-dom";
-import { Dispatch, SetStateAction } from "react";
-import { QueryClient } from "@tanstack/react-query";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { QueryClient, useMutation } from "@tanstack/react-query";
+import { isEmpty, isNil } from "lodash";
+import moment from "moment";
 
 import Transaction from "@/types/Transaction";
 
-import { ListFilters } from "@api/transactions";
+import {
+    getPendingTransactions,
+    getUpcomingTransactions,
+    ListFilters,
+    PendingFilters,
+    UpcomingFilters
+} from "@api/transactions";
 
-import Transactions from "@routes/App/Ledger/Transactions";
-import { Card, CardHeader, CardTitle } from "@components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@components/ui/card";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@components/ui/accordion";
+import { Throbber } from "@components/Throbber";
 import { TransactionsHistory } from "./history";
+import { TransactionsPending } from "./pending"
+import { TransactionsUpcoming } from "./upcoming";
 
 interface OutletContext {
     filters: ListFilters
@@ -25,22 +36,77 @@ export function loader(_queryClient: QueryClient): LoaderFunction {
 export default function Index() {
     const { filters, setOpen, setTransaction } = useOutletContext<OutletContext>()
 
+    const pendingTransactions = useMutation({
+        mutationKey: ['transactions', 'pending'],
+        mutationFn: (filters: PendingFilters) => getPendingTransactions(filters)()
+    })
+
+    const upcomingTransactions = useMutation({
+        mutationKey: ['transactions', 'upcoming'],
+        mutationFn: (filters: UpcomingFilters) => getUpcomingTransactions(filters)()
+    })
+
+    useEffect(() => pendingTransactions.mutate({ account: filters.account }), [filters])
+    useEffect(() => {
+        upcomingTransactions.mutate({
+            executedUntil: moment().add({ month: 1 }).toISOString(), account: filters.account
+        })
+    }, [filters])
+
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Transactions</CardTitle>
-                </CardHeader>
-                <TransactionsHistory filters={filters} setOpen={setOpen} setTransaction={setTransaction} />
-            </Card>
-            <div className="grid grid-rows-[60dvh,_minmax(0,_1fr)] h-full max-h-[100dvh] grid-cols-3 gap-2">
-                <Transactions.History
-                    filters={filters} setOpen={setOpen} setTransaction={setTransaction}
-                    className="row-span-2"
-                />
-                <Transactions.Upcoming filters={filters} setOpen={setOpen} setTransaction={setTransaction} />
-                <Transactions.Pending filters={filters} setOpen={setOpen} setTransaction={setTransaction} />
-            </div>
-        </>
+        <Card>
+            <CardHeader>
+                <CardTitle>Transactions</CardTitle>
+            </CardHeader>
+            <Accordion type="multiple">
+                {
+                    pendingTransactions.isPending
+                        ? <CardContent className="flex flex-row gap-4 justify-center items-center">
+                            <Throbber /> <p>Fetching</p>
+                        </CardContent>
+                        : !isEmpty(pendingTransactions.data) && !isNil(pendingTransactions.data) && (
+                            <AccordionItem value="upcoming">
+                                <AccordionTrigger className="px-5">Upcoming</AccordionTrigger>
+                                <AccordionContent>
+                                    <CardContent>
+                                        <CardDescription>
+                                            Transactions that will become effective in the next month
+                                        </CardDescription>
+                                    </CardContent>
+                                    <TransactionsUpcoming
+                                        mutation={upcomingTransactions}
+                                        setOpen={setOpen}
+                                        setTransaction={setTransaction}
+                                    />
+                                </AccordionContent>
+                            </AccordionItem>
+                        )
+                }
+                {
+                    pendingTransactions.isPending
+                        ? <CardContent className="flex flex-row gap-4 justify-center items-center">
+                            <Throbber /> <p>Fetching</p>
+                        </CardContent>
+                        : !isEmpty(pendingTransactions.data) && !isNil(pendingTransactions.data) && (
+                            <AccordionItem value="pending">
+                                <AccordionTrigger className="px-5">Pending</AccordionTrigger>
+                                <AccordionContent>
+                                    <CardContent>
+                                        <CardDescription>
+                                            Transactions with unknown Execution Date. Please check that is no longer the case and update the transactions to reflect this
+                                        </CardDescription>
+                                    </CardContent>
+                                    <TransactionsPending
+                                        mutation={pendingTransactions}
+                                        setOpen={setOpen}
+                                        setTransaction={setTransaction}
+                                    />
+                                </AccordionContent>
+                            </AccordionItem>
+                        )
+                }
+            </Accordion>
+            <TransactionsHistory filters={filters} setOpen={setOpen} setTransaction={setTransaction} />
+        </Card>
     )
 }
