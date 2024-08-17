9184e4e010482f6c2bb95a24efb43525eab65e34
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-08-17 04:23:32 +0000

    refactoring transactions pending endpoint to use the query client

--- server/transactions/http/handlers/pending.go
+++ server/transactions/http/handlers/pending.go
@@ -2,13 +2,9 @@ package handlers
 
 import (
 	"encoding/json"
+	"financo/server/transactions/queries/pending_query"
 	"financo/server/types/generic/context_key"
 	"financo/server/types/generic/nullable"
-	"financo/server/types/records/account"
-	"financo/server/types/shared/color"
-	"financo/server/types/shared/currency"
-	"financo/server/types/shared/icon"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -18,126 +14,15 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const (
-	pendingTransactionsQuery = `
-SELECT
-    tr.id,
-    tr.issued_at,
-    tr.executed_at,
-    tr.source_amount,
-    tr.target_amount,
-    tr.notes,
-    tr.created_at,
-    tr.updated_at,
-    src.id,
-    src.kind,
-    src.currency,
-    src.name,
-    src.color,
-    src.icon,
-    src.archived_at,
-    src.created_at,
-    src.updated_at,
-    src_p.id,
-    src_p.kind,
-    src_p.currency,
-    src_p.name,
-    src_p.color,
-    src_p.icon,
-    src_p.archived_at,
-    src_p.created_at,
-    src_p.updated_at,
-    trg.id,
-    trg.kind,
-    trg.currency,
-    trg.name,
-    trg.color,
-    trg.icon,
-    trg.archived_at,
-    trg.created_at,
-    trg.updated_at,
-    trgp.id,
-    trgp.kind,
-    trgp.currency,
-    trgp.name,
-    trgp.color,
-    trgp.icon,
-    trgp.archived_at,
-    trgp.created_at,
-    trgp.updated_at
-FROM
-    transactions tr
-    INNER JOIN accounts src ON src.id = tr.source_id
-    LEFT JOIN accounts src_p ON src_p.id = src.parent_id
-    INNER JOIN accounts trg ON trg.id = tr.target_id
-    LEFT JOIN accounts trgp ON trgp.id = trg.parent_id
-WHERE
-    tr.deleted_at IS NULL
-    AND src.deleted_at IS NULL
-    AND trg.deleted_at IS NULL
-	AND tr.executed_at IS NULL
-	`
-)
-
-type Parent struct {
-	ID         int64                    `json:"id"`
-	Kind       account.Kind             `json:"kind"`
-	Currency   currency.Type            `json:"currency"`
-	Name       string                   `json:"name"`
-	Color      color.Type               `json:"color"`
-	Icon       icon.Type                `json:"icon"`
-	ArchivedAt nullable.Type[time.Time] `json:"archivedAt"`
-	CreatedAt  time.Time                `json:"createdAt"`
-	UpdatedAt  time.Time                `json:"updatedAt"`
-}
-
-type Account struct {
-	ID         int64                    `json:"id"`
-	Kind       account.Kind             `json:"kind"`
-	Currency   currency.Type            `json:"currency"`
-	Name       string                   `json:"name"`
-	Color      color.Type               `json:"color"`
-	Icon       icon.Type                `json:"icon"`
-	ArchivedAt nullable.Type[time.Time] `json:"archivedAt"`
-	CreatedAt  time.Time                `json:"createdAt"`
-	UpdatedAt  time.Time                `json:"updatedAt"`
-	Parent     nullable.Type[Parent]    `json:"parent"`
-}
-
-type Transaction struct {
-	ID           int64                    `json:"id"`
-	IssuedAt     time.Time                `json:"issuedAt"`
-	ExecutedAt   nullable.Type[time.Time] `json:"executedAt"`
-	Source       Account                  `json:"source"`
-	SourceAmount int64                    `json:"sourceAmount"`
-	Target       Account                  `json:"target"`
-	TargetAmount int64                    `json:"targetAmount"`
-	Notes        nullable.Type[string]    `json:"notes"`
-	CreatedAt    time.Time                `json:"createdAt"`
-	UpdatedAt    time.Time                `json:"updatedAt"`
-}
-
-type NullableAccount struct {
-	ID         nullable.Type[int64]     `json:"id"`
-	Kind       nullable.Type[string]    `json:"kind"`
-	Currency   nullable.Type[string]    `json:"currency"`
-	Name       nullable.Type[string]    `json:"name"`
-	Color      nullable.Type[string]    `json:"color"`
-	Icon       nullable.Type[string]    `json:"icon"`
-	ArchivedAt nullable.Type[time.Time] `json:"archivedAt"`
-	CreatedAt  nullable.Type[time.Time] `json:"createdAt"`
-	UpdatedAt  nullable.Type[time.Time] `json:"updatedAt"`
-}
-
 func Pending(w http.ResponseWriter, r *http.Request) {
 	var (
-		ctx         = r.Context()
-		results     = make([]Transaction, 0, 50)
-		query       = pendingTransactionsQuery
-		filters     = make([]any, 0, 1)
-		filterCount = 1
+		ids = make([]int64, 0, 10)
+
+		from nullable.Type[time.Time]
+		to   nullable.Type[time.Time]
 	)
-	db, ok := ctx.Value(context_key.DB).(*pgxpool.Conn)
+
+	conn, ok := r.Context().Value(context_key.DB).(*pgxpool.Conn)
 	if !ok {
 		log.Println("failed to retrieved database connection")
 		http.Error(
@@ -148,15 +33,45 @@ func Pending(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.URL.Query().Has(executedFromKey) {
+		raw, err := time.Parse(time.RFC3339, r.URL.Query().Get(executedFromKey))
+		if err != nil {
+			log.Println("failed to parsed from", err)
+			http.Error(
+				w,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
+		from = nullable.New(raw)
+	}
+
+	if r.URL.Query().Has(executedUntilKey) {
+		raw, err := time.Parse(time.RFC3339, r.URL.Query().Get(executedUntilKey))
+		if err != nil {
+			log.Println("failed to parsed to", err)
+			http.Error(
+				w,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
+		to = nullable.New(raw)
+	}
+
 	if r.URL.Query().Has(accountKey) {
-		var (
-			err error
-			raw = strings.Split(r.URL.Query().Get(accountKey), ",")
-			ids = make([]int64, len(raw))
-		)
+		raw := strings.Split(r.URL.Query().Get(accountKey), ",")
 
 		for i := 0; i < len(raw); i++ {
-			ids[i], err = strconv.ParseInt(raw[i], 10, 64)
+			if raw[i] == "" {
+				continue
+			}
+
+			parsed, err := strconv.ParseInt(raw[i], 10, 64)
 			if err != nil {
 				log.Println("failed to parsed id", err)
 				http.Error(
@@ -166,33 +81,14 @@ func Pending(w http.ResponseWriter, r *http.Request) {
 				)
 				return
 			}
+
+			ids = append(ids, parsed)
 		}
-
-		filters = append(filters, ids)
-
-		direct := fmt.Sprintf(
-			"(tr.source_id = ANY ($%d) OR tr.target_id = ANY ($%d))",
-			filterCount,
-			filterCount,
-		)
-
-		indirect := fmt.Sprintf(
-			"(src.parent_id = ANY ($%d) OR trg.parent_id = ANY ($%d))",
-			filterCount,
-			filterCount,
-		)
-
-		query += fmt.Sprintf(
-			" AND (%s OR %s)",
-			direct,
-			indirect,
-		)
-		filterCount++
 	}
 
-	rows, err := db.Query(ctx, query, filters...)
+	res, err := pending_query.New(from, to, ids, conn).Find(r.Context())
 	if err != nil {
-		log.Println("failed query", err)
+		log.Println("query failed", err)
 		http.Error(
 			w,
 			http.StatusText(http.StatusInternalServerError),
@@ -200,78 +96,8 @@ func Pending(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		var (
-			tr        Transaction
-			srcParent NullableAccount
-			trgParent NullableAccount
-		)
-
-		err = rows.Scan(
-			&tr.ID,
-			&tr.IssuedAt,
-			&tr.ExecutedAt,
-			&tr.SourceAmount,
-			&tr.TargetAmount,
-			&tr.Notes,
-			&tr.CreatedAt,
-			&tr.UpdatedAt,
-			&tr.Source.ID,
-			&tr.Source.Kind,
-			&tr.Source.Currency,
-			&tr.Source.Name,
-			&tr.Source.Color,
-			&tr.Source.Icon,
-			&tr.Source.ArchivedAt,
-			&tr.Source.CreatedAt,
-			&tr.Source.UpdatedAt,
-			&srcParent.ID,
-			&srcParent.Kind,
-			&srcParent.Currency,
-			&srcParent.Name,
-			&srcParent.Color,
-			&srcParent.Icon,
-			&srcParent.ArchivedAt,
-			&srcParent.CreatedAt,
-			&srcParent.UpdatedAt,
-			&tr.Target.ID,
-			&tr.Target.Kind,
-			&tr.Target.Currency,
-			&tr.Target.Name,
-			&tr.Target.Color,
-			&tr.Target.Icon,
-			&tr.Target.ArchivedAt,
-			&tr.Target.CreatedAt,
-			&tr.Target.UpdatedAt,
-			&trgParent.ID,
-			&trgParent.Kind,
-			&trgParent.Currency,
-			&trgParent.Name,
-			&trgParent.Color,
-			&trgParent.Icon,
-			&trgParent.ArchivedAt,
-			&trgParent.CreatedAt,
-			&trgParent.UpdatedAt,
-		)
-		if err != nil {
-			log.Println("failed scan", err)
-			http.Error(
-				w,
-				http.StatusText(http.StatusInternalServerError),
-				http.StatusInternalServerError,
-			)
-			return
-		}
-
-		tr.Source.Parent = buildParent(srcParent)
-		tr.Target.Parent = buildParent(trgParent)
-
-		results = append(results, tr)
-	}
-
-	response, err := json.Marshal(results)
+	response, err := json.Marshal(res)
 	if err != nil {
 		log.Println("failed json Marshal", err)
 		http.Error(
@@ -292,24 +118,5 @@ func Pending(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
 	w.Header().Add("Content-Type", "application/json")
-}
-
-func buildParent(data NullableAccount) nullable.Type[Parent] {
-	if data.ID.Valid {
-		return nullable.New(Parent{
-			ID:         data.ID.Val,
-			Kind:       account.Kind(data.Kind.Val),
-			Currency:   currency.Type(data.Currency.Val),
-			Name:       data.Name.Val,
-			Color:      color.Type(data.Color.Val),
-			Icon:       icon.Type(data.Icon.Val),
-			ArchivedAt: data.ArchivedAt,
-			CreatedAt:  data.CreatedAt.Val,
-			UpdatedAt:  data.UpdatedAt.Val,
-		})
-	}
-
-	return nullable.Type[Parent]{}
 }
---
+++ server/transactions/queries/pending_query/query.go
@@ -0,0 +1,128 @@
+package pending_query
+
+import (
+	"context"
+	"errors"
+	base "financo/server/transactions/queries"
+	"financo/server/transactions/types/response"
+	"financo/server/types/generic/nullable"
+	"financo/server/types/queries"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type query struct {
+	from     nullable.Type[time.Time]
+	to       nullable.Type[time.Time]
+	accounts []int64
+	conn     *pgxpool.Conn
+}
+
+func New(
+	from nullable.Type[time.Time],
+	to nullable.Type[time.Time],
+	accounts []int64,
+	conn *pgxpool.Conn,
+) queries.Query[[]response.Detailed] {
+	return &query{
+		from:     from,
+		to:       to,
+		accounts: accounts,
+		conn:     conn,
+	}
+}
+
+func (q *query) Find(ctx context.Context) ([]response.Detailed, error) {
+	var (
+		query   = base.BaseQueryList + " AND tr.executed_at IS NULL"
+		res     = make([]response.Detailed, 0, 20)
+		filters = make([]any, 0, 3)
+		filter  = 1
+	)
+
+	if q.from.Valid && q.to.Valid {
+		filters = append(filters, q.from.Val, q.to.Val)
+		query += " AND (tr.executed_at BETWEEN $1 AND $2)"
+		filter += 2
+	} else if q.from.Valid {
+		filters = append(filters, q.from.Val)
+		query += " AND tr.executed_at >= $1"
+		filter++
+	} else if q.to.Valid {
+		filters = append(filters, q.to.Val)
+		query += " AND tr.executed_at <= $1"
+		filter++
+	}
+
+	if len(q.accounts) > 0 {
+		query += fmt.Sprintf(" AND (tr.source_id = ANY ($%d) OR tr.target_id = ANY ($%d))", filter, filter)
+		filters = append(filters, q.accounts)
+		filter++
+	}
+
+	rows, err := q.conn.Query(ctx, query, filters...)
+	if err != nil {
+		return res, errors.Join(errors.New("failed to execute query"), err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var row base.BaseQueryListRow
+
+		err = rows.Scan(
+			&row.ID,
+			&row.IssuedAt,
+			&row.ExecutedAt,
+			&row.SourceAmount,
+			&row.TargetAmount,
+			&row.Notes,
+			&row.CreatedAt,
+			&row.UpdatedAt,
+			&row.SrcID,
+			&row.SrcKind,
+			&row.SrcCurrency,
+			&row.SrcName,
+			&row.SrcColor,
+			&row.SrcIcon,
+			&row.SrcArchivedAt,
+			&row.SrcCreatedAt,
+			&row.SrcUpdatedAt,
+			&row.SrcParentID,
+			&row.SrcParentKind,
+			&row.SrcParentCurrency,
+			&row.SrcParentName,
+			&row.SrcParentColor,
+			&row.SrcParentIcon,
+			&row.SrcParentArchivedAt,
+			&row.SrcParentCreatedAt,
+			&row.SrcParentUpdatedAt,
+			&row.TrgID,
+			&row.TrgKind,
+			&row.TrgCurrency,
+			&row.TrgName,
+			&row.TrgColor,
+			&row.TrgIcon,
+			&row.TrgArchivedAt,
+			&row.TrgCreatedAt,
+			&row.TrgUpdatedAt,
+			&row.TrgParentID,
+			&row.TrgParentKind,
+			&row.TrgParentCurrency,
+			&row.TrgParentName,
+			&row.TrgParentColor,
+			&row.TrgParentIcon,
+			&row.TrgParentArchivedAt,
+			&row.TrgParentCreatedAt,
+			&row.TrgParentUpdatedAt,
+		)
+		if err != nil {
+			return res, errors.Join(errors.New("failed to scan query row"), err)
+		}
+
+		res = append(res, base.BuildTransactions(row))
+	}
+
+	return res, nil
+}
