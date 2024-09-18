835066c774373dc5e371cbc9ced0333ac896476b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-18 20:40:11 +0000

    refactoring transaction destroy endpoint

--- server/transactions/commands/delete_command/command.go
+++ server/transactions/commands/delete_command/command.go
@@ -2,63 +2,68 @@ package delete_command
 
 import (
 	"context"
+	"database/sql"
 	"errors"
+	"financo/server/services/postgres_database"
 	"financo/server/transactions/queries/detailed_query"
 	"financo/server/transactions/types/response"
 	"financo/server/types/commands"
-	"log"
 	"time"
-
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type command struct {
 	id        int64
-	conn      *pgxpool.Conn
 	timestamp time.Time
 }
 
-func New(conn *pgxpool.Conn, id int64) commands.Command[response.Detailed] {
+func New(id int64) commands.Command[response.Detailed] {
 	return &command{
 		id:        id,
-		conn:      conn,
 		timestamp: time.Now().UTC(),
 	}
 }
 
 func (c *command) Run(ctx context.Context) (response.Detailed, error) {
-	findTransactionQuery := detailed_query.New(c.id, c.conn)
+	var (
+		findTransactionQuery = detailed_query.New(c.id)
+		postgres             = postgres_database.New()
 
-	res, err := findTransactionQuery.Find(ctx)
+		res response.Detailed
+	)
+
+	conn, err := postgres.Conn(ctx)
+	if err != nil {
+		return res, errors.Join(errors.New("failed to retrieve database connection"), err)
+	}
+	defer conn.Close()
+
+	res, err = findTransactionQuery.Find(ctx)
 	if err != nil {
 		return res, errors.Join(errors.New("failed to find transaction"), err)
 	}
 
-	tx, err := c.conn.Begin(ctx)
+	tx, err := conn.BeginTx(ctx, nil)
 	if err != nil {
 		return res, errors.Join(errors.New("failed to begin database transaction"), err)
 	}
-	defer func() {
-		err := tx.Commit(ctx)
-		if err != nil {
-			log.Println("failed to commit transaction", err)
-		}
-	}()
 
 	res.UpdatedAt = c.timestamp
 
 	err = c.markTransactionAsDeleted(ctx, tx)
 	if err != nil {
-		tx.Rollback(ctx)
-		return res, errors.Join(errors.New("failed to mark transaction as deleted"), err)
+		return res, errors.Join(errors.New("failed to mark transaction as deleted"), err, tx.Rollback())
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return res, errors.Join(errors.New("failed to commit transaction"), err)
 	}
 
 	return res, nil
 }
 
-func (c *command) markTransactionAsDeleted(ctx context.Context, tx pgx.Tx) error {
-	_, err := tx.Exec(
+func (c *command) markTransactionAsDeleted(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.ExecContext(
 		ctx,
 		"UPDATE transactions SET deleted_at = $2, updated_at = $2 WHERE id = $1 AND deleted_at IS NULL",
 		c.id,
