a6bd7e5debc374e3d20cbcabf52e43c22215476f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-19 20:04:01 +0000

    implementing database create command

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 WEBAPP_WORKSPACE=webapp
 SERVER_CMD = server/cmd
 
-.PHONY: webapp webapp-server webapp-lint db-setup db-migrate db-rollback db-reset db-validate-migrations db-seed server
+.PHONY: webapp webapp-server webapp-lint db-setup db-migrate db-rollback db-reset db-create db-seed server
 
 # Webapp targets
 webapp:
@@ -11,7 +11,7 @@ webapp-lint:
 
 
 # Database targets
-db-setup: db-reset db-migrate db-seed
+db-setup: db-create db-reset db-migrate db-seed
 db-migrate:
 	@echo "running migrations"
 	@goose up
@@ -24,6 +24,9 @@ db-reset:
 db-seed:
 	@echo "running database seeds"
 	@go run ${SERVER_CMD}/database/seed/main.go
+db-create:
+	@echo "creating database"
+	@go run ${SERVER_CMD}/database/create/main.go
 
 # Server targets
 server:
--- server/cmd/database/create/main.go
+++ server/cmd/database/create/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	database = os.Getenv("DB_DATABASE")
+	password = os.Getenv("DB_PASSWORD")
+	username = os.Getenv("DB_USERNAME")
+	port     = os.Getenv("DB_PORT")
+	host     = os.Getenv("DB_HOST")
+)
+
+func main() {
+	var (
+		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		connStr     = fmt.Sprintf("postgres://%s:%s@%s:%s/postgres?sslmode=disable", username, password, host, port)
+	)
+	defer cancel()
+
+	log.Println("creating database")
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		log.Fatalln("failed to connect to database server", err)
+	}
+	defer db.Close()
+
+	_, err = db.ExecContext(ctx, fmt.Sprintf("CREATE DATABASE %v;", database))
+	if err != nil {
+		errCreateDatabase(err)
+		return
+	}
+
+	log.Printf("\"%s\" created\n", database)
+}
+
+func errCreateDatabase(err error) {
+	if strings.Contains(err.Error(), fmt.Sprintf("database \"%s\" already exists", database)) {
+		log.Printf("\"%s\" already exists\n", database)
+	} else {
+		log.Fatalln("failed to create data base:", err.Error())
+	}
 }
