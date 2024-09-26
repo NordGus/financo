ec5cea90814ebc01a491479f397b882869a80547
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-26 20:56:34 +0000

    exposing accounts message broker

--- server/accounts/service.go
+++ server/accounts/service.go
@@ -1,10 +1,21 @@
 package accounts
 
 import (
+	"context"
+	"financo/server/accounts/brokers"
 	"financo/server/accounts/http/handlers"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
+
+// NewBroker returns the service's message [brokers.Broker]. If no instance has
+// being memoize yet the [*sync.WaitGroup] is required, please do this on
+// program startup. If the instance is already memoized, pass nil as
+// [*sync.WaitGroup].
+func NewBroker(ctx context.Context, wg *sync.WaitGroup) brokers.Broker {
+	return brokers.New(ctx, wg)
+}
 
 func Routes(r chi.Router) {
 	r.Get("/", handlers.List)
