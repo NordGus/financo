0d8341221acc8b327ab42280ff361dd37612cd8a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-27 17:09:01 +0000

    exposing broker initialization in transactions service

--- server/transactions/service.go
+++ server/transactions/service.go
@@ -1,10 +1,20 @@
 package transactions
 
 import (
+	"financo/server/transactions/brokers"
 	"financo/server/transactions/http/handlers"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
+
+// NewBroker returns the service's message [brokers.Broker]. If no instance has
+// being memoize yet the [*sync.WaitGroup] is required, please do this on
+// program startup. If the instance is already memoized, pass nil as
+// [*sync.WaitGroup].
+func NewBroker(wg *sync.WaitGroup) brokers.Broker {
+	return brokers.New(wg)
+}
 
 func Routes(r chi.Router) {
 	r.Get("/", handlers.List)
