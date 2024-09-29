942ba421cdd0a9281d823ee217315e88cac114be
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-29 15:59:43 +0000

    implementing the  json.Unmarshaler interface for account.Kind

--- server/types/records/account/kind.go
+++ server/types/records/account/kind.go
@@ -1,5 +1,11 @@
 package account
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
 // Kind represents the kind of account the record represents
 type Kind string
 
@@ -20,4 +26,40 @@ func IsExternal(kind Kind) bool {
 
 func IsDebt(kind Kind) bool {
 	return kind == DebtLoan || kind == DebtPersonal || kind == DebtCredit
+}
+
+// UnmarshalJSON receives a buffer b, and ensures that the provided value is a
+// valid [Kind]. So [Kind] satisfies the [json.Unmarshaler] interface.
+//
+// It returns an error if the buffer can't be unmarshal into an string or the
+// provided value is not a valid [Kind].
+func (k *Kind) UnmarshalJSON(b []byte) error {
+	var s string
+
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	switch strings.ToLower(s) {
+	default:
+		return fmt.Errorf("account: invalid account kind \"%s\"", s)
+	case "system_historic":
+		*k = SystemHistoric
+	case "capital_normal":
+		*k = CapitalNormal
+	case "capital_savings":
+		*k = CapitalSavings
+	case "debt_personal":
+		*k = DebtPersonal
+	case "debt_loan":
+		*k = DebtLoan
+	case "debt_credit":
+		*k = DebtCredit
+	case "external_income":
+		*k = ExternalIncome
+	case "external_expense":
+		*k = ExternalExpense
+	}
+
+	return nil
 }
