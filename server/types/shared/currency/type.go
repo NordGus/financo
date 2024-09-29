c9a06d7fadf964b2e1b79263d41ec7736ea023e6
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-09-29 16:43:19 +0000

    implementing json.Marshaler and json.Unmarshaler interfaces into currency.Type

--- server/types/shared/currency/type.go
+++ server/types/shared/currency/type.go
@@ -1,5 +1,11 @@
 package currency
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
 // [ ] TODO: refactor to be able to use better constructions
 
 type Type string
@@ -32,3 +38,75 @@ var (
 		{Name: "Chinese Yuan", Code: "CNY"},
 	}
 )
+
+// UnmarshalJSON receives a buffer b, and ensures that the provided value is a
+// valid [Type]. So [Type] satisfies the [json.Unmarshaler] interface.
+//
+// It returns an error if the buffer can't be unmarshal into an string or the
+// provided value is not a supported [Type].
+func (c *Type) UnmarshalJSON(b []byte) error {
+	var s string
+
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	switch strings.ToUpper(s) {
+	default:
+		return fmt.Errorf("currency: invalid currency \"%s\"", s)
+	case "CAD":
+		*c = CAD
+	case "USD":
+		*c = USD
+	case "AUD":
+		*c = AUD
+	case "EUR":
+		*c = EUR
+	case "CHF":
+		*c = CHF
+	case "GBP":
+		*c = GBP
+	case "RUB":
+		*c = RUB
+	case "JPY":
+		*c = JPY
+	case "CNY":
+		*c = CNY
+	}
+
+	return nil
+}
+
+// MarshalJSON returns the json encoding of [Type]. So [Type] satisfies the
+// [json.Marshaler] interface.
+//
+// It returns an error if [Type] is an unsupported value or if json encoding
+// fails.
+func (c Type) MarshalJSON() ([]byte, error) {
+	var s string
+
+	switch c {
+	default:
+		return []byte{}, fmt.Errorf("currency: invalid currency \"%s\"", s)
+	case CAD:
+		s = "CAD"
+	case USD:
+		s = "USD"
+	case AUD:
+		s = "AUD"
+	case EUR:
+		s = "EUR"
+	case CHF:
+		s = "CHF"
+	case GBP:
+		s = "GBP"
+	case RUB:
+		s = "RUB"
+	case JPY:
+		s = "JPY"
+	case CNY:
+		s = "CNY"
+	}
+
+	return json.Marshal(s)
+}
