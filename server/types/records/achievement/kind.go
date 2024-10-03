b70610dc8e0e1979dbdefeb76a643b427a31715d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-03 17:26:54 +0000

    making achievement.Kind implement the sql.Scanner interface

--- server/types/records/achievement/kind.go
+++ server/types/records/achievement/kind.go
@@ -1,7 +1,27 @@
 package achievement
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Kind string
 
 const (
 	SavingsGoal Kind = "savings_goal"
 )
+
+// Scan takes the value returned by the SQL database and maps it to
+// [Kind]. So [Kind] satisfies the [sql.Scanner] interface.
+//
+// It returns an error if [Kind] is an unsupported value.
+func (k *Kind) Scan(value string) error {
+	switch strings.ToLower(value) {
+	default:
+		return fmt.Errorf("achievement: invalid achievement kind \"%s\"", value)
+	case "savings_goal":
+		*k = SavingsGoal
+	}
+
+	return nil
+}
