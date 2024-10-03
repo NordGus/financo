36085b644b16e2fd921dbe3cb1e28df5891196c9
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-03 16:53:22 +0000

    making Settings implement the sql.Scanner interface

--- server/savings_goals/records/settings.go
+++ server/savings_goals/records/settings.go
@@ -1,6 +1,7 @@
 package records
 
 import (
+	"encoding/json"
 	"financo/server/types/shared/currency"
 )
 
@@ -9,4 +10,18 @@ type Settings struct {
 	Target   int64         `json:"target"`
 	Saved    int64         `json:"saved"`
 	Currency currency.Type `json:"currency"`
+}
+
+// Scan takes the json value returned by the SQL database and maps it to
+// [Settings]. So [Settings] satisfies the [sql.Scanner] interface.
+//
+// It returns an error if [Settings] can't be mapped to the json given by the
+// SQL database.
+func (s *Settings) Scan(value []byte) error {
+
+	if err := json.Unmarshal(value, &s); err != nil {
+		return err
+	}
+
+	return nil
 }
