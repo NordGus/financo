d52c88a12c499852af5aa5383e22f6911d498006
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-03 17:08:05 +0000

    adding json tags to achievement record type

--- server/types/records/achievement/record.go
+++ server/types/records/achievement/record.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Record[Settings any] struct {
-	ID         int64
-	Kind       Kind
-	Name       string
-	Settings   Settings
-	AchievedAt nullable.Type[time.Time]
-	DeletedAt  nullable.Type[time.Time]
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID         int64                    `json:"id"`
+	Kind       Kind                     `json:"kind"`
+	Name       string                   `json:"name"`
+	Settings   Settings                 `json:"settings"`
+	AchievedAt nullable.Type[time.Time] `json:"achievedAt"`
+	DeletedAt  nullable.Type[time.Time] `json:"deletedAt"`
+	CreatedAt  time.Time                `json:"createdAt"`
+	UpdatedAt  time.Time                `json:"updatedAt"`
 }
