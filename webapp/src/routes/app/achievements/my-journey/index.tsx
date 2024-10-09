f89a6778d9539e2065ed9d65f6d0dc09a820cf84
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-09 20:03:08 +0000

    commenting unused code

--- webapp/src/routes/app/achievements/my-journey/index.tsx
+++ webapp/src/routes/app/achievements/my-journey/index.tsx
@@ -1,10 +1,7 @@
-import { useLoaderData, useOutletContext } from "react-router-dom"
-import { AchievementsOutletContext } from "../layout"
-import { loader } from "./loader"
 
 export default function MyJourney() {
-    const { onSetSavingsGoal } = useOutletContext<AchievementsOutletContext>()
-    const { timestamp } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
+    // const { onSetSavingsGoal } = useOutletContext<AchievementsOutletContext>()
+    // const { timestamp } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
 
     return (
         <div>
--- webapp/src/routes/app/achievements/progress/index.tsx
+++ webapp/src/routes/app/achievements/progress/index.tsx
@@ -1,11 +1,10 @@
-import { useLoaderData, useOutletContext } from "react-router-dom"
+import { useOutletContext } from "react-router-dom"
 import { AchievementsOutletContext } from "../layout"
 import SavingsGoals from "../savings-goals"
-import { loader } from "./loader"
 
 export default function Index() {
     const { onSetSavingsGoal, onCreateSavingsGoal } = useOutletContext<AchievementsOutletContext>()
-    const { timestamp } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
+    // const { timestamp } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
 
     return (
         <div className="grid grid-cols-3 gap-2">
