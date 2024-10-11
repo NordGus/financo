f5c40fe9a4dd350edb32c8e275a24d634a9ed144
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-11 21:07:42 +0000

    adding description field to achievement.Record type

--- server/types/records/achievement/record.go
+++ server/types/records/achievement/record.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Record[Settings any] struct {
-	ID         int64                    `json:"id"`
-	Kind       Kind                     `json:"kind"`
-	Name       string                   `json:"name"`
-	Settings   Settings                 `json:"settings"`
-	AchievedAt nullable.Type[time.Time] `json:"achievedAt"`
-	DeletedAt  nullable.Type[time.Time] `json:"deletedAt"`
-	CreatedAt  time.Time                `json:"createdAt"`
-	UpdatedAt  time.Time                `json:"updatedAt"`
+	ID          int64                    `json:"id"`
+	Kind        Kind                     `json:"kind"`
+	Name        string                   `json:"name"`
+	Description nullable.Type[string]    `json:"description"`
+	Settings    Settings                 `json:"settings"`
+	AchievedAt  nullable.Type[time.Time] `json:"achievedAt"`
+	DeletedAt   nullable.Type[time.Time] `json:"deletedAt"`
+	CreatedAt   time.Time                `json:"createdAt"`
+	UpdatedAt   time.Time                `json:"updatedAt"`
 }
