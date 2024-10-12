3d0abdb74f611d6248e18aa13d31b4d5e0975997
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-12 11:45:09 +0000

    implementing transactions seed logic

--- cmd/database/seed/transactions/seed.go
+++ cmd/database/seed/transactions/seed.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"errors"
 	"financo/cmd/database/seed/accounts"
+	"financo/server/types/records/transaction"
+	"fmt"
 	"log"
 	"time"
 )
@@ -26,6 +28,44 @@ func SeedTransactions(
 	defer tx.Rollback()
 
 	// implemented seeding
+	for i := 0; i < len(transactions); i++ {
+		var (
+			data   = transactions[i]
+			source = seeds[data.Source.Key].Account
+			target = seeds[data.Target.Key].Account
+		)
+
+		if data.Source.ParentKey.Valid {
+			source = seeds[data.Source.ParentKey.Val].Children[data.Source.Key]
+		}
+
+		if data.Target.ParentKey.Valid {
+			source = seeds[data.Target.ParentKey.Val].Children[data.Target.Key]
+		}
+
+		tr := transaction.Record{
+			SourceID:     source.ID,
+			TargetID:     target.ID,
+			SourceAmount: data.SourceAmount,
+			TargetAmount: data.TargetAmount,
+			Notes:        data.Notes,
+			IssuedAt:     data.IssuedAt(timestamp.UTC()),
+			ExecutedAt:   data.ExecutedAt(timestamp.UTC()),
+			DeletedAt:    data.DeletedAt(timestamp.UTC()),
+			CreatedAt:    timestamp.UTC(),
+			UpdatedAt:    timestamp.UTC(),
+		}
+
+		err := create(ctx, tr, tx)
+		if err != nil {
+			return errors.Join(
+				fmt.Errorf("transactions: failed to seed transaction between %s and %s", source.Name, target.Name),
+				err,
+			)
+		}
+
+		summary += 1
+	}
 
 	err = tx.Commit()
 	if err != nil {
@@ -35,4 +75,38 @@ func SeedTransactions(
 	log.Printf("\t\t%d transactions seeded\n", summary)
 
 	return nil
+}
+
+func create(ctx context.Context, tr transaction.Record, tx *sql.Tx) error {
+	err := tx.QueryRowContext(
+		ctx,
+		`
+		INSERT INTO
+			transactions(
+				source_id,
+				target_id,
+				source_amount,
+				target_amount,
+				notes,
+				issued_at,
+				executed_at,
+				created_at,
+				updated_at
+			)
+		VALUES
+			($1, $2, $3, $4, $5, $6, $7, $8, $9)
+		RETURNING id
+		`,
+		tr.SourceID,
+		tr.TargetID,
+		tr.SourceAmount,
+		tr.TargetAmount,
+		tr.Notes,
+		tr.IssuedAt,
+		tr.ExecutedAt,
+		tr.CreatedAt,
+		tr.UpdatedAt,
+	).Scan(&tr.ID)
+
+	return err
 }
