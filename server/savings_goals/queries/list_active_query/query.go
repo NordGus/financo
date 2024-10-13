c42e9704db8febb03ab940d544048678df9ed58a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-13 13:00:44 +0000

    implementing list_active_query for savings goals

---
+++ server/savings_goals/queries/list_active_query/query.go
@@ -0,0 +1,102 @@
+package list_active_query
+
+import (
+	"context"
+	"errors"
+	"financo/server/savings_goals/types/response"
+	"financo/server/services/postgres_database"
+	"financo/server/types/queries"
+	"financo/server/types/records/achievement"
+	"financo/server/types/records/achievement/savings_goal"
+)
+
+type query struct {
+	db postgres_database.Service
+}
+
+func New(db postgres_database.Service) queries.Query[[]response.Active] {
+	return &query{
+		db: db,
+	}
+}
+
+func (q *query) Find(ctx context.Context) ([]response.Active, error) {
+	var (
+		res = make([]response.Active, 0, 10)
+		idx = -1
+	)
+
+	conn, err := q.db.Conn(ctx)
+	if err != nil {
+		return res, errors.Join(errors.New("failed to get database connection"), err)
+	}
+	defer conn.Close()
+
+	rows, err := conn.QueryContext(
+		ctx,
+		`
+		SELECT
+			id,
+			kind,
+			name,
+			description,
+			settings,
+			achieved_at,
+			deleted_at,
+			created_at,
+			updated_at
+		FROM achievements
+		WHERE
+			kind = $1
+			AND achieved_at IS NULL
+			AND deleted_at IS NULL
+		ORDER BY
+			settings->'currency', settings->'position' ASC
+		`,
+		achievement.SavingsGoal,
+	)
+	if err != nil {
+		return res, errors.Join(errors.New("failed to execute database"), err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var record savings_goal.Record
+
+		err = rows.Scan(
+			&record.ID,
+			&record.Kind,
+			&record.Name,
+			&record.Description,
+			&record.Settings,
+			&record.AchievedAt,
+			&record.DeletedAt,
+			&record.CreatedAt,
+			&record.UpdatedAt,
+		)
+		if err != nil {
+			return res, errors.Join(errors.New("failed to scan rows"), err)
+		}
+
+		if idx < 0 {
+			res = append(res, buildResponse(record))
+			idx = 0
+		}
+
+		if res[idx].Currency != record.Settings.Currency {
+			res = append(res, buildResponse(record))
+			idx++
+		}
+
+		res[idx].Goals = append(res[idx].Goals, record)
+	}
+
+	return res, nil
+}
+
+func buildResponse(record savings_goal.Record) response.Active {
+	return response.Active{
+		Currency: record.Settings.Currency,
+		Goals:    make([]savings_goal.Record, 0, 10),
+	}
+}
