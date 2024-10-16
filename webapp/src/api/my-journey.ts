3f472f094107c3239e53d4f80abdbc07cdca8c72
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-16 20:48:01 +0000

    fixing return type

--- webapp/src/api/my-journey.ts
+++ webapp/src/api/my-journey.ts
@@ -1,7 +1,7 @@
 import { Achievable } from "@/types/achievable";
 import { Settings as SavingsGoals } from "@/types/savings-goal";
 
-export async function getAchieved(): Promise<Achievable<SavingsGoals>> {
+export async function getAchieved(): Promise<Achievable<SavingsGoals>[]> {
     const response = await fetch("/api/my_journey/achieved")
 
     if (!response.ok) throw response
