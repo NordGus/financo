bb832a1db1f6280817436a74f23d5b6e4843f26a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-16 20:48:17 +0000

    mocking my-journey view

--- webapp/src/routes/app/achievements/my-journey/index.tsx
+++ webapp/src/routes/app/achievements/my-journey/index.tsx
@@ -1,11 +1,71 @@
+import { Kind } from "@/types/achievable"
+import { SavingsGoal } from "@/types/savings-goal"
+import { getAchieved } from "@api/my-journey"
+import { Throbber } from "@components/Throbber"
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card"
+import { staleTimeDefault } from "@queries/client"
+import { useQuery } from "@tanstack/react-query"
+import { isEmpty, isNil } from "lodash"
 
 export default function MyJourney() {
     // const { onSetSavingsGoal } = useOutletContext<AchievementsOutletContext>()
     // const { timestamp } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
 
+    const { data, isFetching, isError, error } = useQuery({
+        queryKey: ["my-journey", "achievements"],
+        queryFn: getAchieved,
+        staleTime: staleTimeDefault,
+    })
+
+    if (isError) throw error
+
+    if (isFetching) {
+        return (
+            <div className="m-auto">
+                <Throbber />
+            </div>
+        )
+    }
+
+    if (isEmpty(data) || isNil(data)) {
+        return (
+            <div>
+                There's no achievements yet
+            </div>
+        )
+    }
+
     return (
         <div>
-            This is how far I've come
+            {
+                data.map((achievement) => {
+                    switch (achievement.kind) {
+                        case Kind.SavingsGoal:
+                            return <SavingsGoalEntry key={achievement.id} goal={achievement} />
+                        default:
+                            throw Error(`invalid achievable kind: ${achievement.kind}`)
+                    }
+                })
+            }
         </div>
+    )
+}
+
+interface SavingsGoalEntryProps {
+    goal: SavingsGoal
+}
+
+function SavingsGoalEntry({ goal }: SavingsGoalEntryProps) {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    {goal.name}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p>{goal.description}</p>
+            </CardContent>
+        </Card>
     )
 }
