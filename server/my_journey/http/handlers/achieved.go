0bfc01ec703f414f42273e8897a0ecb729cb859e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-16 20:04:38 +0000

    implementing http handler

---
+++ server/my_journey/http/handlers/achieved.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"encoding/json"
+	"financo/server/my_journey/queries/list_achieved_query"
+	"financo/server/services/postgres_database"
+	"log"
+	"net/http"
+)
+
+func Achieved(w http.ResponseWriter, r *http.Request) {
+	var (
+		postgres = postgres_database.New()
+	)
+
+	res, err := list_achieved_query.New(postgres).Find(r.Context())
+	if err != nil {
+		log.Println("query failed", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	response, err := json.Marshal(res)
+	if err != nil {
+		log.Println("failed json Marshal", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	_, err = w.Write(response)
+	if err != nil {
+		log.Println("failed to write response", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+}
