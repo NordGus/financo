0955a58e66f1b4e5798849e98a08d705b01cadfc
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-20 19:43:01 +0000

    writing inmemory implementation of DeletedBroker

---
+++ core/accounts/infrastructure/deleted_broker/inmemory.go
@@ -0,0 +1,48 @@
+package created_broker
+
+import (
+	"context"
+	"financo/core/accounts/domain/brokers"
+	"financo/core/accounts/domain/messages"
+	"financo/server/types/message_bus"
+	"fmt"
+	"sync"
+)
+
+type inMemoryBroker struct {
+	ctx context.Context
+	wg  *sync.WaitGroup
+	bus message_bus.Bus[messages.Deleted]
+}
+
+func NewInMemory(ctx context.Context, wg *sync.WaitGroup) brokers.DeletedBroker {
+	return &inMemoryBroker{
+		ctx: ctx,
+		wg:  wg,
+		bus: message_bus.New[messages.Deleted](wg, "account_deleted"),
+	}
+}
+
+func (b *inMemoryBroker) Subscribe(consumer message_bus.Consumer[messages.Deleted]) error {
+	b.wg.Add(1)
+	defer b.wg.Done()
+
+	select {
+	case <-b.ctx.Done():
+		return fmt.Errorf("created_broker: failed to subscribe: %s", b.ctx.Err())
+	default:
+		return b.bus.Subscribe(consumer)
+	}
+}
+
+func (b *inMemoryBroker) Publish(message messages.Deleted) error {
+	b.wg.Add(1)
+	defer b.wg.Done()
+
+	select {
+	case <-b.ctx.Done():
+		return fmt.Errorf("created_broker: failed to publish: %s", b.ctx.Err())
+	default:
+		return b.bus.Publish(message)
+	}
+}
