3abeda50b33159f42fd9cdc3389672e4cc8e0e4e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-21 22:25:55 +0000

    updating UpdateAccountRepository interface to fullfil the debt_account update_command

--- core/scope_accounts/application/update_command/debt_account/command.go
+++ core/scope_accounts/application/update_command/debt_account/command.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"errors"
 	"financo/core/scope_accounts/domain/brokers"
+	"financo/core/scope_accounts/domain/messages"
 	"financo/core/scope_accounts/domain/repositories"
 	"financo/core/scope_accounts/domain/requests"
 	"financo/core/scope_accounts/domain/responses"
 	"financo/server/types/commands"
+	"financo/server/types/generic/nullable"
+	"financo/server/types/records/account"
+	"financo/server/types/records/transaction"
+	"time"
 )
 
 type command struct {
@@ -29,5 +34,84 @@ func New(
 }
 
 func (c *command) Run(ctx context.Context) (responses.Detailed, error) {
-	return responses.Detailed{}, errors.New("update_debt_account: not implemented")
+	var (
+		res     responses.Detailed
+		history nullable.Type[transaction.Record]
+
+		timestamp = time.Now().UTC()
+		record    = account.Record{
+			ID:          c.req.ID,
+			Kind:        c.req.Kind,
+			Currency:    c.req.Currency,
+			Name:        c.req.Name,
+			Description: c.req.Description,
+			Capital:     c.req.Capital,
+			Color:       c.req.Color,
+			Icon:        c.req.Icon,
+			UpdatedAt:   timestamp,
+		}
+	)
+
+	if c.req.Archive {
+		record.ArchivedAt = nullable.New(timestamp)
+	}
+
+	if record.Kind == account.DebtCredit && record.Capital < 0 {
+		record.Capital = -record.Capital
+	}
+
+	records, err := c.repo.FindAccountWithHistory(ctx, record.ID)
+	if err != nil {
+		return res, err
+	}
+
+	if records.Record.Kind != record.Kind {
+		return res, errors.New("update_command: account's kind can't be changed")
+	}
+
+	history = c.mapHistoryTransaction(records)
+
+	res, err = c.repo.SaveAccountWithHistory(ctx, repositories.SaveAccountWithHistoryArgs{
+		Record:      record,
+		Transaction: history,
+	})
+	if err != nil {
+		return res, err
+	}
+
+	err = c.broker.Publish(messages.Updated{
+		Previous: records.Record,
+		Current:  record,
+	})
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+func (c *command) mapHistoryTransaction(records repositories.AccountWithHistory) nullable.Type[transaction.Record] {
+	if !c.req.History.Present {
+		return nullable.Type[transaction.Record]{}
+	}
+
+	timestamp := time.Now().UTC()
+	tr := transaction.Record{
+		SourceID:     records.Record.ID,
+		TargetID:     records.History.ID,
+		SourceAmount: c.req.History.Balance.Val,
+		TargetAmount: c.req.History.Balance.Val,
+		IssuedAt:     c.req.History.At.Val,
+		ExecutedAt:   c.req.History.At,
+		UpdatedAt:    timestamp,
+		CreatedAt:    timestamp,
+	}
+
+	if tr.SourceAmount < 0 {
+		tr.SourceID, tr.TargetID = tr.TargetID, tr.SourceID
+		tr.SourceAmount = -tr.SourceAmount
+		tr.TargetAmount = -tr.TargetAmount
+	}
+
+	return nullable.New(tr)
 }
