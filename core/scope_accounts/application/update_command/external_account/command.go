68f35ebec35a778bafadd82cb12411f8a8d5bb23
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-21 21:10:53 +0000

    mocking different update subcommands

---
+++ core/scope_accounts/application/update_command/external_account/command.go
@@ -0,0 +1,33 @@
+package external_account
+
+import (
+	"context"
+	"errors"
+	"financo/core/scope_accounts/domain/brokers"
+	"financo/core/scope_accounts/domain/repositories"
+	"financo/core/scope_accounts/domain/requests"
+	"financo/core/scope_accounts/domain/responses"
+	"financo/server/types/commands"
+)
+
+type command struct {
+	req    requests.Update
+	repo   repositories.UpdateAccountRepository
+	broker brokers.UpdatedBroker
+}
+
+func New(
+	req requests.Update,
+	repo repositories.UpdateAccountRepository,
+	broker brokers.UpdatedBroker,
+) commands.Command[responses.Detailed] {
+	return &command{
+		req:    req,
+		repo:   repo,
+		broker: broker,
+	}
+}
+
+func (c *command) Run(ctx context.Context) (responses.Detailed, error) {
+	return responses.Detailed{}, errors.New("update_external_account: not implemented")
+}
