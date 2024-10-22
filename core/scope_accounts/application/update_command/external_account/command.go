8666fa90b2f56dd1bff8a830fb07fe5e97610478
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-22 22:40:17 +0000

    implementing external account update command

--- core/scope_accounts/application/update_command/external_account/command.go
+++ core/scope_accounts/application/update_command/external_account/command.go
@@ -1,24 +1,29 @@
 package external_account
 
 import (
+	"cmp"
 	"context"
 	"errors"
 	"financo/core/scope_accounts/domain/brokers"
+	"financo/core/scope_accounts/domain/messages"
 	"financo/core/scope_accounts/domain/repositories"
 	"financo/core/scope_accounts/domain/requests"
 	"financo/core/scope_accounts/domain/responses"
 	"financo/server/types/commands"
+	"financo/server/types/records/account"
+	"slices"
+	"time"
 )
 
 type command struct {
 	req    requests.Update
-	repo   repositories.UpdateAccountRepository
+	repo   repositories.UpdateAccountWithChildrenRepository
 	broker brokers.UpdatedBroker
 }
 
 func New(
 	req requests.Update,
-	repo repositories.UpdateAccountRepository,
+	repo repositories.UpdateAccountWithChildrenRepository,
 	broker brokers.UpdatedBroker,
 ) commands.Command[responses.Detailed] {
 	return &command{
@@ -29,5 +34,63 @@ func New(
 }
 
 func (c *command) Run(ctx context.Context) (responses.Detailed, error) {
-	return responses.Detailed{}, errors.New("update_external_account: not implemented")
+	var (
+		res responses.Detailed
+
+		timestamp = time.Now().UTC()
+		children  = make([]account.Record, 0, 10)
+		record    = requests.UpdateToAccountRecord(c.req, timestamp)
+	)
+
+	record.Capital = 0
+
+	for i := 0; i < len(c.req.Children); i++ {
+		child := requests.UpdateChildToAccountRecord(record, c.req.Children[i], timestamp)
+
+		children = append(children, child)
+	}
+
+	records, err := c.repo.FindWithChildren(ctx, record.ID)
+	if err != nil {
+		return res, err
+	}
+
+	if records.Record.Kind != record.Kind {
+		return res, errors.New("update_command: account's kind can't be changed")
+	}
+
+	record.CreatedAt = records.Record.CreatedAt
+
+	slices.SortFunc(children, func(a, b account.Record) int {
+		return cmp.Compare(a.ID, b.ID)
+	})
+
+	offset := 0
+
+	for i := 0; i < len(children); i++ {
+		if children[i].ID <= 0 {
+			offset++
+			continue
+		}
+
+		children[i].CreatedAt = records.Children[i-offset].CreatedAt
+	}
+
+	res, err = c.repo.SaveWithChildren(ctx, repositories.SaveAccountWithChildrenArgs{
+		Record:   record,
+		Children: children,
+	})
+	if err != nil {
+		return res, err
+	}
+
+	err = c.broker.Publish(messages.Updated{
+		Previous: records.Record,
+		Current:  record,
+	})
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
 }
