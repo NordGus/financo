a1a3250aa35158ab2cbe7a485be37b3467dd4813
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-23 16:09:47 +0000

    implementing FindWithChildren

--- core/scope_accounts/infrastructure/update_account_repository/postgresql.go
+++ core/scope_accounts/infrastructure/update_account_repository/postgresql.go
@@ -2,26 +2,53 @@ package update_account_repository
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"financo/core/domain/databases"
 	"financo/core/scope_accounts/domain/repositories"
 	"financo/core/scope_accounts/domain/responses"
+	"financo/server/types/records/account"
 )
 
 type repository struct {
 	db databases.SQLAdapter
 }
 
-// TODO: Implement
 func NewPostgreSQL(db databases.SQLAdapter) repositories.UpdateAccountRepository {
 	return &repository{
 		db: db,
 	}
 }
 
-// FindWithChildren implements repositories.UpdateAccountRepository.
 func (r *repository) FindWithChildren(ctx context.Context, id int64) (repositories.AccountWithChildren, error) {
-	return repositories.AccountWithChildren{}, errors.New("not implemented")
+	var (
+		children []account.Record
+		record   account.Record
+		out      repositories.AccountWithChildren
+	)
+
+	conn, err := r.db.Conn(ctx)
+	if err != nil {
+		return out, err
+	}
+	defer conn.Close()
+
+	record, err = findRecord(ctx, conn, id)
+	if err != nil {
+		return out, err
+	}
+
+	children, err = findChildren(ctx, conn, record.ID)
+	if err != nil {
+		return out, err
+	}
+
+	out = repositories.AccountWithChildren{
+		Record:   record,
+		Children: children,
+	}
+
+	return out, nil
 }
 
 // FindWithHistory implements repositories.UpdateAccountRepository.
@@ -43,4 +70,114 @@ func (r *repository) SaveWithHistory(
 	args repositories.SaveAccountWithHistoryArgs,
 ) (responses.Detailed, error) {
 	return responses.Detailed{}, errors.New("not implemented")
+}
+
+func findRecord(ctx context.Context, conn *sql.Conn, id int64) (account.Record, error) {
+	var record account.Record
+
+	err := conn.QueryRowContext(
+		ctx,
+		`
+		SELECT
+			id,
+			parent_id,
+			kind,
+			currency,
+			name,
+			description,
+			color,
+			icon,
+			capital,
+			archived_at,
+			deleted_at,
+			created_at,
+			updated_at
+		FROM accounts
+		WHERE
+			id = $1 AND
+			kind != $2 AND
+			deleted_at IS NULL AND
+			parent_id IS NULL
+		`,
+		id,
+		account.SystemHistoric,
+	).Scan(
+		&record.ID,
+		&record.ParentID,
+		&record.Kind,
+		&record.Currency,
+		&record.Name,
+		&record.Description,
+		&record.Color,
+		&record.Icon,
+		&record.Capital,
+		&record.ArchivedAt,
+		&record.DeletedAt,
+		&record.CreatedAt,
+		&record.UpdatedAt,
+	)
+
+	return record, err
+}
+
+func findChildren(ctx context.Context, conn *sql.Conn, parentID int64) ([]account.Record, error) {
+	children := make([]account.Record, 0, 10)
+
+	rows, err := conn.QueryContext(
+		ctx,
+		`
+		SELECT
+			id,
+			parent_id,
+			kind,
+			currency,
+			name,
+			description,
+			color,
+			icon,
+			capital,
+			archived_at,
+			deleted_at,
+			created_at,
+			updated_at
+		FROM accounts
+		WHERE
+			parent_id = $1 AND
+			kind != $2 AND
+			deleted_at IS NULL
+		`,
+		parentID,
+		account.SystemHistoric,
+	)
+	if err != nil {
+		return children, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var child account.Record
+
+		err = rows.Scan(
+			&child.ID,
+			&child.ParentID,
+			&child.Kind,
+			&child.Currency,
+			&child.Name,
+			&child.Description,
+			&child.Color,
+			&child.Icon,
+			&child.Capital,
+			&child.ArchivedAt,
+			&child.DeletedAt,
+			&child.CreatedAt,
+			&child.UpdatedAt,
+		)
+		if err != nil {
+			return children, err
+		}
+
+		children = append(children, child)
+	}
+
+	return children, nil
 }
