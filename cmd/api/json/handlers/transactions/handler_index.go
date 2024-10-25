d9313019070733489a68c2cfdeab2ba1afddfcd0
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-25 20:13:50 +0000

    refactoring handlers to use new executed_query and executed_for_account

--- cmd/api/json/handlers/transactions/handler_index.go
+++ cmd/api/json/handlers/transactions/handler_index.go
@@ -2,8 +2,11 @@ package transactions
 
 import (
 	"encoding/json"
+	"financo/core/scope_transactions/application/queries/executed_query"
+	"financo/core/scope_transactions/domain/requests"
+	"financo/core/scope_transactions/infrastructure/executed_transactions_repository"
 	"financo/lib/nullable"
-	"financo/server/transactions/queries/list_query"
+	"financo/services/postgresql_database"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,11 +16,10 @@ import (
 
 func index(w http.ResponseWriter, r *http.Request) {
 	var (
-		accounts   = make([]int64, 0, 10)
-		categories = make([]int64, 0, 10)
-
-		from nullable.Type[time.Time]
-		to   nullable.Type[time.Time]
+		req = requests.Executed{
+			AccountIDs:  make([]int64, 0, 10),
+			CategoryIDs: make([]int64, 0, 10),
+		}
 	)
 
 	if r.URL.Query().Has(executedFromKey) {
@@ -32,7 +34,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		from = nullable.New(raw)
+		req.From = nullable.New(raw)
 	}
 
 	if r.URL.Query().Has(executedUntilKey) {
@@ -47,7 +49,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		to = nullable.New(raw)
+		req.To = nullable.New(raw)
 	}
 
 	if r.URL.Query().Has(accountKey) {
@@ -69,7 +71,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			accounts = append(accounts, parsed)
+			req.AccountIDs = append(req.AccountIDs, parsed)
 		}
 	}
 
@@ -92,11 +94,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			categories = append(categories, parsed)
+			req.CategoryIDs = append(req.CategoryIDs, parsed)
 		}
 	}
 
-	res, err := list_query.New(from, to, accounts, categories).Find(r.Context())
+	repo := executed_transactions_repository.New(postgresql_database.New())
+
+	res, err := executed_query.New(req, repo).Find(r.Context())
 	if err != nil {
 		log.Println("query failed", err)
 		http.Error(
