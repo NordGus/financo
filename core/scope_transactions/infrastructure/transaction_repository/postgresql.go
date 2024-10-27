0b6fda3dc3bebd46eda36c0f40a4b3fd4f82356f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-27 23:02:44 +0000

    migrating delete_command into new hexagonal core

---
+++ core/scope_transactions/infrastructure/transaction_repository/postgresql.go
@@ -0,0 +1,76 @@
+package transaction_repository
+
+import (
+	"context"
+	"database/sql"
+	"financo/core/domain/databases"
+	"financo/core/scope_transactions/domain/repositories"
+	"financo/models/transaction"
+)
+
+type repository struct {
+	db databases.SQLAdapter
+}
+
+func NewPostgreSQL(db databases.SQLAdapter) repositories.TransactionRepository {
+	return &repository{
+		db: db,
+	}
+}
+
+func (r *repository) Find(ctx context.Context, id int64) (transaction.Record, error) {
+	var record transaction.Record
+
+	conn, err := r.db.Conn(ctx)
+	if err != nil {
+		return record, err
+	}
+	defer conn.Close()
+
+	record, err = find(ctx, conn, id)
+	if err != nil {
+		return record, err
+	}
+
+	return record, nil
+}
+
+func find(ctx context.Context, conn *sql.Conn, id int64) (transaction.Record, error) {
+	var record transaction.Record
+
+	err := conn.QueryRowContext(
+		ctx,
+		`
+		SELECT
+			id,
+			source_id,
+			target_id,
+			source_amount,
+			target_amount,
+			notes,
+			issued_at,
+			executed_at,
+			deleted_at,
+			created_at,
+			updated_at
+		FROM transactions
+		WHERE deleted_at IS NULL
+			AND id = $1
+		`,
+		id,
+	).Scan(
+		&record.ID,
+		&record.SourceID,
+		&record.TargetID,
+		&record.SourceAmount,
+		&record.TargetAmount,
+		&record.Notes,
+		&record.IssuedAt,
+		&record.ExecutedAt,
+		&record.DeletedAt,
+		&record.CreatedAt,
+		&record.UpdatedAt,
+	)
+
+	return record, err
+}
