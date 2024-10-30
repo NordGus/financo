2234dbb9d51aa1f3aa59b3d273b89ee854404081
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-30 06:11:14 +0000

    implementing new graphs net worth handler

--- cmd/api/json/handlers/graphs/entrypoint.go
+++ cmd/api/json/handlers/graphs/entrypoint.go
@@ -7,7 +7,7 @@ import (
 func Routes(r chi.Router) {
 	r.Get("/capital", Capital)
 	r.Get("/debts", Debts)
-	// r.Get("/net_worth", NetWorth)
+	r.Get("/net-worth", NetWorth)
 	// r.Get("/available_credit", AvailableCredit)
 
 	// r.Route("/for_account", for_account.Routes)
