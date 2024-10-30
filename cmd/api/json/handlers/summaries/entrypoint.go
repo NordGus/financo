0d83da45c0f2b6757e345c13d72b104d02b656af
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-10-30 06:17:47 +0000

    removing unused code

--- cmd/api/json/handlers/summaries/entrypoint.go
+++ cmd/api/json/handlers/summaries/entrypoint.go
@@ -7,9 +7,6 @@ import (
 )
 
 func Routes(r chi.Router) {
-	r.Get("/capital", Capital)
-	r.Get("/debts", Debts)
-	r.Get("/net_worth", NetWorth)
 	r.Get("/available_credit", AvailableCredit)
 
 	r.Route("/for_account", for_account.Routes)
