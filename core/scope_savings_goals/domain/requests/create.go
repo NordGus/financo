232e85a605c9bc45d75be236e611fbec86fc2783
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-03 14:19:44 +0000

    adding mappers between request/response and record

--- core/scope_savings_goals/domain/requests/create.go
+++ core/scope_savings_goals/domain/requests/create.go
@@ -3,6 +3,9 @@ package requests
 import (
 	"financo/lib/currency"
 	"financo/lib/nullable"
+	"financo/models/achievement"
+	"financo/models/achievement/savings_goal"
+	"time"
 )
 
 type Create struct {
@@ -10,4 +13,21 @@ type Create struct {
 	Description nullable.Type[string] `json:"description"`
 	Currency    currency.Type         `json:"currency"`
 	Target      int64                 `json:"target"`
+}
+
+func (r Create) ToRecord(timestamp time.Time) savings_goal.Record {
+	return savings_goal.Record{
+		ID:          -1,
+		Kind:        achievement.SavingsGoal,
+		Name:        r.Name,
+		Description: r.Description,
+		Settings: savings_goal.Settings{
+			Position: -1,
+			Target:   r.Target,
+			Saved:    0,
+			Currency: r.Currency,
+		},
+		UpdatedAt: timestamp,
+		CreatedAt: timestamp,
+	}
 }
--- core/scope_savings_goals/domain/responses/created.go
+++ core/scope_savings_goals/domain/responses/created.go
@@ -1,9 +1,20 @@
 package responses
 
-import "financo/lib/currency"
+import (
+	"financo/lib/currency"
+	"financo/models/achievement/savings_goal"
+)
 
 type Created struct {
 	Name     string        `json:"name"`
 	Currency currency.Type `json:"currency"`
 	Target   int64         `json:"target"`
+}
+
+func RecordToCreated(r savings_goal.Record) Created {
+	return Created{
+		Name:     r.Name,
+		Currency: r.Settings.Currency,
+		Target:   r.Settings.Target,
+	}
 }
