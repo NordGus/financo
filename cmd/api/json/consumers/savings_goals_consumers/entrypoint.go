71768de10df1d9d21764c8020b86ce7fe679ab6c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-14 19:41:04 +0000

    implementing on_account_deleted message consumer

--- cmd/api/json/consumers/savings_goals_consumers/entrypoint.go
+++ cmd/api/json/consumers/savings_goals_consumers/entrypoint.go
@@ -1,10 +1,11 @@
 package savings_goals_consumers
 
 import (
-	account_messages "financo/core/scope_accounts/domain/messages"
+	acc_msg "financo/core/scope_accounts/domain/messages"
 	accounts_broker "financo/core/scope_accounts/infrastructure/broker_handler"
 	"financo/core/scope_savings_goals/infrastructure/consumers/on_account_created"
-	"financo/lib/message_bus"
+	"financo/core/scope_savings_goals/infrastructure/consumers/on_account_deleted"
+	bus "financo/lib/message_bus"
 )
 
 func Subscribe() error {
@@ -13,9 +14,12 @@ func Subscribe() error {
 		return err
 	}
 
-	err = accounts.CreatedBroker().Subscribe(
-		message_bus.ConsumerFunc[account_messages.Created](on_account_created.NewInMemory),
-	)
+	err = accounts.CreatedBroker().Subscribe(bus.ConsumerFunc[acc_msg.Created](on_account_created.NewInMemory))
+	if err != nil {
+		return err
+	}
+
+	err = accounts.DeletedBroker().Subscribe(bus.ConsumerFunc[acc_msg.Deleted](on_account_deleted.NewInMemory))
 	if err != nil {
 		return err
 	}
