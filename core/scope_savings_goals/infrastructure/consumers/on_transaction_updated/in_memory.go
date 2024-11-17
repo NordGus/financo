da77c2b8e13afd85fdd9fa7b1e50e3a24ebd058c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-17 22:32:44 +0000

    implementing consumer

---
+++ core/scope_savings_goals/infrastructure/consumers/on_transaction_updated/in_memory.go
@@ -0,0 +1,27 @@
+package on_transaction_updated
+
+import (
+	"financo/core/scope_savings_goals/application/event_handlers/on_transaction_updated"
+	"financo/core/scope_savings_goals/infrastructure/repositories/on_transaction_operated_repository"
+	"financo/core/scope_transactions/domain/messages"
+	"financo/services/postgresql_database"
+	"log"
+	"sync"
+)
+
+func NewInMemory(wg *sync.WaitGroup, payload messages.Updated) {
+	defer wg.Done()
+
+	var (
+		db = postgresql_database.New()
+	)
+
+	err := on_transaction_updated.New(
+		on_transaction_operated_repository.NewPostgreSQL(db),
+	).Handle(payload)
+	if err != nil {
+		log.Println("something went wrong while handling transaction updated message.", err)
+
+		return
+	}
+}
