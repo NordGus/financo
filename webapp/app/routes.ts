2f4c37da1989b54dba65c7e0d950c987d5322372
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-23 23:17:53 +0000

    cleaning base code

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -1,7 +1,6 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
     layout("authentication/layout.tsx", [
         route("login", "routes/login.tsx"),
     ])
