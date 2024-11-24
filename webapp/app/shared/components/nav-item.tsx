76581c5e92dad6e22d942dcc40aa00979cc1c787
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-24 04:56:06 +0000

    adjusting styling

--- webapp/app/shared/components/nav-item.tsx
+++ webapp/app/shared/components/nav-item.tsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router";
 import { cn } from "~/lib/utils";
 
 const linkVariants = cva(
-  "inline-flex items-center justify-center gap-2 py-2 rounded-md [&_svg]:pointer-events-none [&_svg]:size-5 [&_svg]:shrink-0 leading-none",
+  "inline-flex items-center justify-center gap-2 py-2 rounded-md [&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0 leading-none",
   {
     variants: {
       variant: {
-        "active": "text-foreground dark:text-foreground [&_svg]:size-6 text-lg",
+        "active": "text-foreground dark:text-foreground",
         "pending": "bg-background text-foreground animate-pulse",
         "default": "text-foreground/50 hover:text-foreground"
       }
