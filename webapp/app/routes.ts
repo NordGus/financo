2d7c906a6b3ef2094183c151f5d348d879a2e5af
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-24 11:27:05 +0000

    mocking ledger section

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -2,14 +2,20 @@ import {
   type RouteConfig,
   index,
   layout,
+  prefix,
   route
 } from "@react-router/dev/routes";
 
 export default [
   layout("shared/layout.tsx", [
     index("routes/home.tsx"),
-    route("/dashboard", "routes/dashboard.tsx"),
-    route("/accounts", "routes/accounts/index.tsx")
+    route("dashboard", "routes/dashboard.tsx"),
+    ...prefix("accounts", [
+      index("routes/accounts/index.tsx")
+    ]),
+    ...prefix("ledger", [
+      index("routes/ledger/index.tsx")
+    ])
   ]),
   layout("modules/authentication/layout.tsx", [
     route("login", "routes/login.tsx"),
---
+++ webapp/app/routes/ledger/index.tsx
@@ -0,0 +1,19 @@
+import { Screen } from "~/modules/ledger/screens";
+import { Route } from "./+types/index";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "financo - ledger" },
+    { name: "description", content: "manage your wealth's transactions" }
+  ]
+}
+
+export async function loader({ }: Route.LoaderArgs) {
+  return {
+    breadcrumb: "ledger"
+  }
+}
+
+export default function Index() {
+  return <Screen />
+}
