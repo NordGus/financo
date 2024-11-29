b75e79103ce6ecc074777df1e6801f15b50cf2c5
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-29 20:44:40 +0000

    adding a more complex concurrency mechanism for message_buses

--- lib/message_bus/bus.go
+++ lib/message_bus/bus.go
@@ -13,10 +13,11 @@ type Bus[Payload any] interface {
 	// It returns an error if the [Bus] can't handle more consumers.
 	Subscribe(consumer Consumer[Payload]) error
 
-	// Publish publishes the given message and its payload to the [Bus].
+	// Publish publishes the given message and its payload to the [Bus] and
+	// returns a channel to signal it was consumed by all subscribed [Consumer].
 	//
 	// It returns an error if something fails during publishing.
-	Publish(payload Payload) error
+	Publish(payload Payload) (<-chan struct{}, error)
 }
 
 type messageBus[Payload any] struct {
@@ -54,16 +55,29 @@ func (m *messageBus[Payload]) Subscribe(consumer Consumer[Payload]) error {
 	return nil
 }
 
-func (m *messageBus[Payload]) Publish(payload Payload) error {
+func (m *messageBus[Payload]) Publish(payload Payload) (<-chan struct{}, error) {
 	m.RWMutex.RLock()
-	defer m.RWMutex.RUnlock()
 
-	m.wg.Add(m.consumerCount + 1)
-	defer m.wg.Done()
+	m.wg.Add(1)
 
-	for i := 0; i < m.consumerCount; i++ {
-		m.consumers[i].Consume(m.wg, payload)
-	}
+	done := make(chan struct{}, 1)
 
-	return nil
+	go func() {
+		defer m.wg.Done()
+		defer close(done)
+
+		wg := new(sync.WaitGroup)
+		wg.Add(m.consumerCount)
+
+		for i := 0; i < m.consumerCount; i++ {
+			go m.consumers[i].Consume(wg, payload)
+		}
+
+		m.RWMutex.RUnlock()
+		wg.Wait()
+
+		done <- struct{}{}
+	}()
+
+	return done, nil
 }
