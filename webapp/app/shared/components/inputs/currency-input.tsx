0aef6ae53312cb3eff05906461daaf1656e9004c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-11-30 00:12:31 +0000

    implementing a currency input

---
+++ webapp/app/shared/components/inputs/currency-input.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useFetcher } from "react-router";
+import { clientLoader } from "~/routes/currencies/for-select";
+import { Currency } from "~/shared/types/currency";
+import { FormControl } from "../ui/form";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+
+interface Props {
+  defaultValue?: Currency
+  onValueChange: (value: Currency) => void
+}
+
+export function CurrencyInput({ onValueChange, defaultValue }: Props) {
+  const fetcher = useFetcher<typeof clientLoader>({ key: "currencies.input" })
+  const [currencies, setCurrencies] = useState(fetcher.data?.currencies || [])
+
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      await fetcher.load("/currencies/for-select")
+
+      console.log(fetcher.data)
+
+      setCurrencies(fetcher.data?.currencies || [])
+    }
+
+    fetchCurrencies()
+  }, [])
+
+  return (
+    <Select onValueChange={onValueChange} defaultValue={defaultValue}>
+      <FormControl>
+        <SelectTrigger>
+          <SelectValue placeholder={fetcher.state !== "idle" ? "Loading..." : "Select a currency"} />
+        </SelectTrigger>
+      </FormControl>
+      <SelectContent>
+        {
+          currencies.map(({ code, name }) => (
+            <SelectItem value={code} key={`currency.${code}`}>{name}</SelectItem>
+          ))
+        }
+      </SelectContent>
+    </Select>
+  )
+}
