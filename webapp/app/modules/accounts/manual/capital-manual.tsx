6575a5e296e35acef8328d19abf64cc3e2e86aa6
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-05 19:09:30 +0000

    cleaning copy

--- webapp/app/modules/accounts/manual/capital-manual.tsx
+++ webapp/app/modules/accounts/manual/capital-manual.tsx
@@ -7,7 +7,10 @@ export const capitalManual: Copy = {
       For Loan and Personal Debt Accounts, capital is the total amount of the debt. Meanwhile for Credit Accounts it is the credit limit for the Account.
     </p>
     <p>
-      Please keep in mind that a negative capital value indicates that you owe capital, whereas a positive one indicates that you are owed capital. Is the same idea as with balance your balance, negative/red numbers are negative to your position while positive/black numbers are positive to your position.
+      Please keep in mind that a negative capital value indicates that you owe capital, whereas a positive one indicates that you are owed capital.
+    </p>
+    <p>
+      Is the same idea as with your balance, negative/red numbers are negative to your position while positive/black numbers are positive to your position.
     </p>
   </>
 }
