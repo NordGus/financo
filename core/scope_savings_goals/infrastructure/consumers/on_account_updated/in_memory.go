1b3d9a2f8d03bc35029060555c0c8ab86e8e19ef
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-07 23:56:45 +0000

    moving business logic into event_handler

--- core/scope_savings_goals/infrastructure/consumers/on_account_updated/in_memory.go
+++ core/scope_savings_goals/infrastructure/consumers/on_account_updated/in_memory.go
@@ -5,7 +5,6 @@ import (
 	"financo/core/scope_savings_goals/application/event_handlers/on_account_updated"
 	"financo/core/scope_savings_goals/infrastructure/lock"
 	"financo/core/scope_savings_goals/infrastructure/repositories/on_account_operated_repository"
-	"financo/models/account"
 	"financo/services/postgresql_database"
 	"log"
 	"sync"
@@ -14,21 +13,10 @@ import (
 func NewInMemory(wg *sync.WaitGroup, payload messages.Updated) {
 	defer wg.Done()
 
-	if payload.Current.Kind != account.CapitalSavings && payload.Previous.Kind != account.CapitalSavings {
-		return // Only process savings account
-	}
+	lock.GlobalLock().Lock()
+	defer lock.GlobalLock().Unlock()
 
-	var (
-		l  = lock.GlobalLock()
-		db = postgresql_database.New()
-	)
-
-	l.Lock()
-	defer l.Unlock()
-
-	err := on_account_updated.New(
-		on_account_operated_repository.NewPostgreSQL(db),
-	).Handle(payload)
+	err := on_account_updated.New(on_account_operated_repository.NewPostgreSQL(postgresql_database.New())).Handle(payload)
 	if err != nil {
 		log.Println("something went wrong while handling account updated message.", err)
 
