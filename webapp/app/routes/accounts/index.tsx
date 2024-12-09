0a11f312385b60fd9236f425539b5273720496c0
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-09 18:27:48 +0000

    cleaning action

--- webapp/app/routes/accounts/index.tsx
+++ webapp/app/routes/accounts/index.tsx
@@ -1,9 +1,6 @@
 import { useLoaderData } from "react-router";
-import { toast } from "sonner";
-import { createAccount } from "~/modules/accounts/api/commands/create-account";
 import { getAccountsPreviews } from "~/modules/accounts/api/queries/get-accounts-previews";
 import { Screen } from "~/modules/accounts/screens";
-import { Create } from "~/modules/accounts/types/create";
 import { Route } from "./+types/index";
 
 export function meta({ }: Route.MetaArgs) {
@@ -11,38 +8,6 @@ export function meta({ }: Route.MetaArgs) {
     { title: "financo - Accounts" },
     { name: "description", content: "Manage your Accounts" }
   ]
-}
-
-interface ActionRequestBody extends Create {
-  intent: "create"
-}
-
-export async function clientAction({ request }: Route.ClientActionArgs) {
-  const values: ActionRequestBody = await request.json()
-
-  if (values.intent !== "create") throw new Error("invalid action")
-
-  try {
-    const response = createAccount({ ...values })
-
-    toast.promise(response, {
-      loading: "Creating...",
-      success: (data) => {
-        return `${data.name} created`
-      },
-      error: "Oops!. Something went wrong"
-    })
-
-    const created = await response
-
-    return created
-  } catch (error) {
-    if (error instanceof Response && error.status === 401) throw error
-
-    console.error(error)
-
-    return null
-  }
 }
 
 export async function clientLoader({ }: Route.ClientLoaderArgs) {
