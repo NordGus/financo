ead49815f7b566677e3174cdd0a91a050c5ac40c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-26 20:48:33 +0000

    setting up child creation method

--- webapp/app/modules/categories/components/forms/create.tsx
+++ webapp/app/modules/categories/components/forms/create.tsx
@@ -1,13 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { PlusIcon } from "lucide-react";
+import { useEffect, useReducer, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 import { useFetcher } from "react-router";
 import { z } from "zod";
 import { CurrencyInput } from "~/shared/components/inputs/currency-input";
 import { IconInput } from "~/shared/components/inputs/icon-input";
 import { Throbber } from "~/shared/components/throbber";
 import { Button } from "~/shared/components/ui/button";
-import { DialogFooter, DialogHeader, DialogTitle } from "~/shared/components/ui/dialog";
+import {
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "~/shared/components/ui/dialog";
 import {
   Form,
   FormControl,
@@ -21,9 +26,11 @@ import { Input } from "~/shared/components/ui/input";
 import { Textarea } from "~/shared/components/ui/textarea";
 import { isCategory, isExpense, Kind } from "~/shared/types/account";
 import { Currency } from "~/shared/types/currency";
-import { ICONS } from "~/shared/types/icon";
+import { Icon, ICONS } from "~/shared/types/icon";
 import { Created } from "../../types/create";
 import { schema } from "../schemas/create";
+import { CreateChild } from "./child/create";
+import { Preview } from "./child/preview";
 
 interface Props {
   kind: Kind
@@ -31,20 +38,102 @@ interface Props {
   onSuccess: () => void
 }
 
+interface ChildData {
+  name: string
+  description?: string
+  icon: Icon
+}
+
+interface ChildState {
+  data: ChildData
+  onSubmit: (data: ChildData) => void
+  open: boolean
+  action: "add" | "edit"
+}
+
+interface ChildInitialState {
+  name?: string
+  description?: string
+  icon?: Icon
+}
+
+const __childAction = {
+  ADD: "ADD",
+  EDIT: "EDIT",
+  CLOSE: "CLOSE",
+  OPEN_CHANGED: "OPEN_CHANGED"
+} as const
+
+type ChildActions = typeof __childAction
+
+type ChildAction =
+  { type: ChildActions["ADD"], onSubmit: (data: ChildData) => void } |
+  { type: ChildActions["EDIT"], data: ChildData, onSubmit: (data: ChildData) => void } |
+  { type: ChildActions["CLOSE"] } |
+  { type: ChildActions["OPEN_CHANGED"], open: boolean }
+
+function reducer(state: ChildState, action: ChildAction): ChildState {
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        data: { name: "", description: undefined, icon: ICONS.bookmark },
+        onSubmit: action.onSubmit,
+        action: "add",
+        open: true
+      }
+    case "EDIT":
+      return {
+        ...state,
+        data: { ...action.data },
+        onSubmit: action.onSubmit,
+        action: "edit",
+        open: true
+      }
+    case "CLOSE":
+      return {
+        ...state,
+        data: { name: "", description: undefined, icon: ICONS.bookmark },
+        onSubmit: (__data) => { },
+        open: false
+      }
+    case "OPEN_CHANGED":
+      return { ...state, onSubmit: (__data) => { }, open: action.open }
+    default:
+      throw new Error(`ChildForm invalid action`)
+  }
+}
+
+function initChildForm({ name = "", description, icon = ICONS.bookmark }: ChildInitialState): ChildState {
+  return {
+    data: { name, description, icon },
+    onSubmit: (__data) => { },
+    open: false,
+    action: "add"
+  }
+}
+
 export function CreateCategory({ kind, defaultCurrency, onSuccess }: Props) {
   if (!isCategory(kind)) throw new Error(`CreateCategory invalid kind ${kind}`)
 
   const [loading, setLoading] = useState(false)
+  const [child, childDispatch] = useReducer(reducer, {}, initChildForm)
   const fetcher = useFetcher<Created | null>({ key: `categories.create.${kind}` })
+  const defaultIcon = isExpense(kind) ? ICONS.bookmark : ICONS.banknote
 
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: {
       kind: kind,
       currency: defaultCurrency,
-      icon: isExpense(kind) ? ICONS.bookmark : ICONS.banknote,
+      icon: defaultIcon,
       intent: "create"
     }
+  })
+  const { fields: children, append, update } = useFieldArray({
+    name: "children",
+    control: form.control,
+    keyName: "identity"
   })
 
   const onSubmit = async (values: z.infer<typeof schema>) => {
@@ -55,6 +144,25 @@ export function CreateCategory({ kind, defaultCurrency, onSuccess }: Props) {
       { action: "/categories", method: "post", encType: "application/json" }
     )
   }
+
+  const onChildOpenChanged = (open: boolean) => childDispatch({ type: "OPEN_CHANGED", open })
+  const onChildSubmitted = () => childDispatch({ type: "CLOSE" })
+  const onAddChildClicked = () => childDispatch({
+    type: "ADD",
+    onSubmit: (data) => {
+      append({ name: data.name, description: data.description, icon: data.icon })
+
+      onChildSubmitted()
+    }
+  })
+  const onEditChildClicked = (c: ChildData, idx: number) => childDispatch({
+    type: "EDIT",
+    data: { name: c.name, description: c.description, icon: c.icon },
+    onSubmit: (data) => {
+      update(idx, { ...data })
+      onChildSubmitted()
+    }
+  })
 
   useEffect(() => {
     setLoading(false)
@@ -146,6 +254,25 @@ export function CreateCategory({ kind, defaultCurrency, onSuccess }: Props) {
               </FormItem>
             )}
           />
+          <Button
+            variant="link"
+            type="button"
+            className="flex justify-center items-center leading-snug gap-2 border-2 border-dashed rounded-md"
+            onClick={onAddChildClicked}
+          >
+            <PlusIcon /> Add Child
+          </Button>
+          <div className="divide-y-2 divide-primary-foreground">
+            {children.map((c, idx) => (
+              <Preview
+                key={`child.${idx}`}
+                name={c.name}
+                description={c.description}
+                icon={c.icon}
+                onClick={() => onEditChildClicked(c, idx)}
+              />
+            ))}
+          </div>
           <DialogFooter>
             <Button type="submit" className="min-w-24" disabled={loading}>
               {loading ? <Throbber size={"sm"} /> : "Create"}
@@ -153,6 +280,15 @@ export function CreateCategory({ kind, defaultCurrency, onSuccess }: Props) {
           </DialogFooter>
         </form>
       </Form>
+
+      <CreateChild
+        action={child.action}
+        child={child.data}
+        open={child.open}
+        onSubmit={child.onSubmit}
+        onOpenChange={onChildOpenChanged}
+        defaultIcon={defaultIcon}
+      />
     </>
   )
 }
