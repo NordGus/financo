356e83536b96d900b592a4bddfb97843d274a78b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-26 22:08:39 +0000

    implementing create_repository for scope_accounts

---
+++ core/scope_categories/infrastructure/repositories/create_repository/postgresql.go
@@ -0,0 +1,94 @@
+package create_repository
+
+import (
+	"context"
+	"database/sql"
+	"financo/core/domain/databases"
+	"financo/core/scope_categories/domain/repositories"
+	"financo/lib/nullable"
+	"financo/models/account"
+)
+
+type postgresql struct {
+	db databases.SQLAdapter
+}
+
+func NewPostgreSQL(db databases.SQLAdapter) repositories.CreateRepository {
+	return &postgresql{
+		db: db,
+	}
+}
+
+func (p *postgresql) Save(ctx context.Context, r account.Record, c []account.Record) (account.Record, error) {
+	conn, err := p.db.Conn(ctx)
+	if err != nil {
+		return r, err
+	}
+	defer conn.Close()
+
+	tx, err := conn.BeginTx(ctx, nil)
+	if err != nil {
+		return r, err
+	}
+
+	r, err = p.persist(ctx, tx, r)
+	if err != nil {
+		_ = tx.Rollback()
+		return r, err
+	}
+
+	for i := 0; i < len(c); i++ {
+		child := c[i]
+		child.ParentID = nullable.New(r.ID)
+
+		c[i], err = p.persist(ctx, tx, child)
+		if err != nil {
+			_ = tx.Rollback()
+			return r, err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		_ = tx.Rollback()
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (p *postgresql) persist(ctx context.Context, tx *sql.Tx, r account.Record) (account.Record, error) {
+	err := tx.QueryRowContext(
+		ctx,
+		`
+		INSERT INTO accounts(
+			parent_id,
+			kind,
+			currency,
+			name,
+			description,
+			color,
+			icon,
+			capital,
+			dynamic_data,
+			created_at,
+			updated_at
+		)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+		RETURNING id
+		`,
+		r.ParentID,
+		r.Kind,
+		r.Currency,
+		r.Name,
+		r.Description,
+		r.Color,
+		r.Icon,
+		r.Capital,
+		r.DynamicData,
+		r.CreatedAt,
+		r.UpdatedAt,
+	).Scan(&r.ID)
+
+	return r, err
+}
