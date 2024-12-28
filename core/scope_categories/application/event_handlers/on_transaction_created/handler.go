b4e69ac5ce881c14e0ba99016a01aa838082ba9e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-28 21:57:30 +0000

    adding protections against non-category accounts

--- core/scope_categories/application/event_handlers/on_transaction_created/handler.go
+++ core/scope_categories/application/event_handlers/on_transaction_created/handler.go
@@ -31,6 +31,8 @@ func (h *handler) Handle(event messages.Created) error {
 	var (
 		ctx       = context.Background()
 		timestamp = time.Now().UTC()
+		ids       = make([]int64, 0, 2)
+		update    = make([]category.Record, 0, 2)
 	)
 
 	source, err := h.categoryRepo.Find(ctx, event.Record.SourceID)
@@ -43,20 +45,34 @@ func (h *handler) Handle(event messages.Created) error {
 		return err
 	}
 
-	ids := []int64{source.Account.ID, target.Account.ID}
+	if source.Account.ID > 0 {
+		ids = append(ids, source.Account.ID)
+	}
+
+	if target.Account.ID > 0 {
+		ids = append(ids, target.Account.ID)
+	}
 
 	counts, err := h.transactionsRepo.CountFor(ctx, ids)
 	if err != nil {
 		return err
 	}
 
-	source.Account.UpdatedAt = timestamp
-	source.Account.DynamicData.Transactions = counts[source.Account.ID]
+	if source.Account.ID > 0 {
+		source.Account.UpdatedAt = timestamp
+		source.Account.DynamicData.Transactions = counts[source.Account.ID]
 
-	target.Account.UpdatedAt = timestamp
-	target.Account.DynamicData.Transactions = counts[target.Account.ID]
+		update = append(update, source)
+	}
 
-	err = h.repo.Save(ctx, []category.Record{source, target})
+	if target.Account.ID > 0 {
+		target.Account.UpdatedAt = timestamp
+		target.Account.DynamicData.Transactions = counts[target.Account.ID]
+
+		update = append(update, target)
+	}
+
+	err = h.repo.Save(ctx, update)
 	if err != nil {
 		return err
 	}
