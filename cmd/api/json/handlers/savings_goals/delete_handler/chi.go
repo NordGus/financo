2f27247f1aad0f8a5b9e0d008b65654a9560bf4d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-30 15:38:03 +0000

    refactoring delete_handler

---
+++ cmd/api/json/handlers/savings_goals/delete_handler/chi.go
@@ -0,0 +1,69 @@
+package delete_handler
+
+import (
+	"encoding/json"
+	"financo/core/scope_savings_goals/application/commands/delete_command"
+	"financo/core/scope_savings_goals/domain/requests"
+	"financo/core/scope_savings_goals/infrastructure/repositories/savings_goals_repository"
+	"financo/core/scope_savings_goals/infrastructure/repositories/update_repository"
+	"financo/core/scope_savings_goals/infrastructure/services/message_broker"
+	"financo/services/postgresql_database"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func HandlerFunc(w http.ResponseWriter, r *http.Request) {
+	var (
+		db     = postgresql_database.New()
+		goals  = savings_goals_repository.NewPostgreSQL(db)
+		delete = update_repository.NewPostgreSQL(db)
+
+		req requests.Delete
+	)
+
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	if err != nil {
+		log.Println("failed to parse account id", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	req.ID = id
+
+	broker, err := message_broker.Instance()
+	if err != nil {
+		log.Println("savings_goals: delete_handler: failed to retrieve message broker, reason:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	res, err := delete_command.New(req, goals, delete, broker.Deleted()).Run(r.Context())
+	if err != nil {
+		log.Println("savings_goals: delete_handler: command failed, reason:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(res)
+	if err != nil {
+		log.Println("savings_goals: delete_handler: command failed to marshal response, reason:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		log.Println("savings_goals: delete_handler: command failed to write response, reason:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+}
--- cmd/api/json/handlers/savings_goals/entrypoint.go
+++ cmd/api/json/handlers/savings_goals/entrypoint.go
@@ -2,6 +2,7 @@ package savings_goals
 
 import (
 	"financo/cmd/api/json/handlers/savings_goals/create_handler"
+	"financo/cmd/api/json/handlers/savings_goals/delete_handler"
 	"financo/cmd/api/json/handlers/savings_goals/update_handler"
 
 	"github.com/go-chi/chi/v5"
@@ -14,7 +15,7 @@ func Routes(r chi.Router) {
 	r.Patch("/reorder", Reorder)
 
 	r.Route("/{id}", func(r chi.Router) {
-		r.Delete("/", Destroy)
+		r.Delete("/", delete_handler.HandlerFunc)
 		r.Put("/", update_handler.HandlerFunc)
 		r.Patch("/mark-as-achieved", MarkAsAchieved)
 	})
