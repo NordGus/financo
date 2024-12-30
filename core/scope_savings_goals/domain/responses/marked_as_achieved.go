95a85fb7c031bc9680546a685f2dc9d8d883c35a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-30 15:51:34 +0000

    refactoring mark_as_achieved_command for scope_savings_goals

--- core/scope_savings_goals/domain/responses/marked_as_achieved.go
+++ core/scope_savings_goals/domain/responses/marked_as_achieved.go
@@ -1,20 +1,28 @@
 package responses
 
 import (
+	"financo/lib/currency"
 	"financo/lib/nullable"
 	"financo/models/achievement/savings_goal"
+	"time"
 )
 
 type MarkedAsAchieved struct {
 	ID          int64                 `json:"id"`
 	Name        string                `json:"name"`
 	Description nullable.Type[string] `json:"description"`
+	Currency    currency.Type         `json:"currency"`
+	Target      int64                 `json:"target"`
+	At          time.Time             `json:"at"`
 }
 
-func RecordToMarkedAsAchieved(r savings_goal.Record) MarkedAsAchieved {
+func NewMarkedAsAchieved(r savings_goal.Record) MarkedAsAchieved {
 	return MarkedAsAchieved{
 		ID:          r.ID,
 		Name:        r.Name,
 		Description: r.Description,
+		Currency:    r.Settings.Currency,
+		Target:      r.Settings.Target,
+		At:          r.AchievedAt.Val,
 	}
 }
