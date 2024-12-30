04cd50fade3854f3649dafd2f568fc9e8ce33dab
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-30 15:37:46 +0000

    refactoring delete_command for scope_savings_goals

--- core/scope_savings_goals/domain/responses/deleted.go
+++ core/scope_savings_goals/domain/responses/deleted.go
@@ -1,17 +1,22 @@
 package responses
 
 import (
+	"financo/lib/currency"
 	"financo/models/achievement/savings_goal"
 )
 
 type Deleted struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID       int64         `json:"id"`
+	Name     string        `json:"name"`
+	Currency currency.Type `json:"currency"`
+	Target   int64         `json:"target"`
 }
 
-func RecordToDeleted(r savings_goal.Record) Deleted {
+func NewDeleted(r savings_goal.Record) Deleted {
 	return Deleted{
-		ID:   r.ID,
-		Name: r.Name,
+		ID:       r.ID,
+		Name:     r.Name,
+		Currency: r.Settings.Currency,
+		Target:   r.Settings.Target,
 	}
 }
