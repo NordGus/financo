15503a7dadbd55d0997ae05a88f2822422910cba
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-12-31 12:51:42 +0000

    refactoring create command

--- core/scope_savings_goals/application/commands/create_command/command.go
+++ core/scope_savings_goals/application/commands/create_command/command.go
@@ -9,31 +9,27 @@ import (
 	"financo/core/scope_savings_goals/domain/repositories"
 	"financo/core/scope_savings_goals/domain/requests"
 	"financo/core/scope_savings_goals/domain/responses"
-	"financo/lib/currency"
 	"time"
 )
 
 type command struct {
-	req     requests.Create
-	savings repositories.SavingsRepository
-	goals   repositories.SavingsGoalsRepository
-	create  repositories.CreateRepository
-	broker  brokers.Created
+	req    requests.Create
+	goals  repositories.SavingsGoalsRepository
+	create repositories.CreateRepository
+	broker brokers.Created
 }
 
 func New(
 	req requests.Create,
-	savings repositories.SavingsRepository,
 	goals repositories.SavingsGoalsRepository,
 	create repositories.CreateRepository,
 	broker brokers.Created,
 ) commands.Command[responses.Created] {
 	return &command{
-		req:     req,
-		savings: savings,
-		goals:   goals,
-		create:  create,
-		broker:  broker,
+		req:    req,
+		goals:  goals,
+		create: create,
+		broker: broker,
 	}
 }
 
@@ -42,15 +38,9 @@ func (c *command) Run(ctx context.Context) (responses.Created, error) {
 		timestamp = time.Now().UTC()
 		record    = c.req.ToRecord(timestamp)
 		goalsF    = filters.SavingsGoals{Currency: c.req.Currency}
-		savingsF  = filters.Savings{Currencies: []currency.Type{c.req.Currency}}
 
 		res responses.Created
 	)
-
-	savings, err := c.savings.Where(ctx, savingsF)
-	if err != nil {
-		return res, err
-	}
 
 	goals, err := c.goals.Where(ctx, goalsF)
 	if err != nil {
@@ -59,27 +49,7 @@ func (c *command) Run(ctx context.Context) (responses.Created, error) {
 
 	record.Settings.Position = int16(len(goals)) + 1
 
-	goals = append(goals, record)
-	saved := savings[c.req.Currency]
-	last := len(goals) - 1
-
-	for i := 0; i < len(goals); i++ {
-		if saved < goals[i].Settings.Target && saved > 0 {
-			goals[i].Settings.Saved = saved
-			saved = 0
-			continue
-		}
-
-		if saved <= 0 {
-			goals[i].Settings.Saved = 0
-			continue
-		}
-
-		goals[i].Settings.Saved = goals[i].Settings.Target
-		saved -= goals[i].Settings.Target
-	}
-
-	record, err = c.create.Save(ctx, goals[last])
+	record, err = c.create.Save(ctx, record)
 	if err != nil {
 		return res, err
 	}
