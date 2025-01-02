ef73f220ccb2f2d537e680c59a718b7896889319
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-02 12:31:29 +0000

    refactoring active_savings goals query

--- cmd/api/json/handlers/savings_goals/entrypoint.go
+++ cmd/api/json/handlers/savings_goals/entrypoint.go
@@ -1,6 +1,7 @@
 package savings_goals
 
 import (
+	"financo/cmd/api/json/handlers/savings_goals/active_goals_for_handler"
 	"financo/cmd/api/json/handlers/savings_goals/create_handler"
 	"financo/cmd/api/json/handlers/savings_goals/delete_handler"
 	"financo/cmd/api/json/handlers/savings_goals/mark_as_achieved_handler"
@@ -12,8 +13,7 @@ import (
 
 func Routes(r chi.Router) {
 	r.Post("/", create_handler.HandlerFunc)
-
-	r.Get("/active", Active)
+	r.Get("/", active_goals_for_handler.HandlerFunc)
 	r.Patch("/reorder", reorder_handler.HandlerFunc)
 
 	r.Route("/{id}", func(r chi.Router) {
