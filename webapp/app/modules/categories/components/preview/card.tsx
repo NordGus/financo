485c59e2444b831503e79f38c5b43fd653f384b6
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-21 22:52:44 +0000

    updating preview card design

--- webapp/app/modules/categories/components/preview/card.tsx
+++ webapp/app/modules/categories/components/preview/card.tsx
@@ -1,67 +1,52 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { redirect } from "react-router"
-import { cn } from "~/lib/utils"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/shared/components/ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "~/shared/components/ui/card"
 import { Drawer, DrawerContent } from "~/shared/components/ui/drawer"
 import { icons } from "~/shared/components/ui/icon"
-import { accountKindToHuman } from "~/shared/helpers/account-kind-to-human"
 import { colorContrast } from "~/shared/helpers/color-contrast"
-import { isCategory, isExpense, isIncome } from "~/shared/types/account"
+import { isCategory } from "~/shared/types/account"
 import { Account } from "../../types/preview"
 import { UpdateCategory } from "../forms/update"
-import { ActionablesMenu } from "./actionables-menu"
 
 interface Props {
   account: Account
   isArchived: (archivedAt?: string | null) => boolean
 }
 
-export function Preview({ account, isArchived }: Props) {
+export function Preview({ account }: Props) {
   const [openEdit, setOpenEdit] = useState(false)
+  const activeCount = useMemo(() => account.children.filter((c) => !c.archivedAt).length, [account.updatedAt])
+  const archivedCount = useMemo(() => account.children.filter((c) => !!c.archivedAt).length, [account.updatedAt])
 
   return (
     <>
       <Card
-        className={cn("flex flex-col", isCategory(account.kind) && "cursor-pointer")}
-        style={{ backgroundColor: account.color }}
+        className={"flex flex-col cursor-pointer min-h-28"}
+        style={{
+          backgroundColor: account.color,
+          color: colorContrast(account.color)
+        }}
+        onClick={() => setOpenEdit(true)}
       >
-        <CardHeader
-          className="min-h-28"
-          style={{ color: colorContrast(account.color) }}
-          onClick={() => setOpenEdit(true)}
-        >
+        <CardHeader className="px-4 pt-4 pb-2">
           <CardTitle className="flex flex-row gap-1 items-center [&_svg]:size-5">
             {icons[account.icon]} {account.name}
           </CardTitle>
-          <CardDescription style={{ color: colorContrast(account.color), opacity: "70%", }}>
+          <CardDescription className="opacity-70" style={{ color: colorContrast(account.color) }}>
             {account.description}
           </CardDescription>
         </CardHeader>
-        <CardContent
-          className="flex flex-wrap gap-2 grow"
-          style={{ color: colorContrast(account.color) }}
-          onClick={() => setOpenEdit(true)}
-        >
-          {account.children.filter((c) => isArchived(c.archivedAt)).map((child) => (
-            <span
-              key={`category.${account.id}.${child.id}`}
-              className="rounded-md px-2 py-0.5 border text-sm"
-              style={{ borderColor: colorContrast(account.color) }}
-            >
-              {child.name}
+        <CardFooter className="flex justify-end gap-2 px-4 pt-2 pb-4 text-sm">
+          {activeCount > 0 && !account.archivedAt && (
+            <span>
+              {activeCount} active {activeCount === 1 ? "child" : "children"}
             </span>
-          ))}
-        </CardContent>
-        <CardFooter className="flex gap-2 items-center">
-          {
-            !(isExpense(account.kind) || isIncome(account.kind)) && (
-              <span style={{ color: colorContrast(account.color) }}>
-                {accountKindToHuman(account.kind)}
-              </span>
-            )
-          }
-          <span className="grow content-['']" />
-          <ActionablesMenu account={account} />
+          )}
+          {archivedCount > 0 && !account.archivedAt && (
+            <span>
+              {archivedCount} archived {archivedCount === 1 ? "child" : "children"}
+            </span>
+          )}
         </CardFooter>
       </Card>
 
