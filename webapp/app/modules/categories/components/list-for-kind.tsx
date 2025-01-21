52dd1bc86eb156cb11ea388cc9b09417d92c32be
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-21 23:15:45 +0000

    updating list-for-kind to follow new standard

--- webapp/app/modules/categories/components/list-for-kind.tsx
+++ webapp/app/modules/categories/components/list-for-kind.tsx
@@ -1,93 +1,39 @@
-import { PlusIcon } from "lucide-react";
-import { useState } from "react";
-import { redirect } from "react-router";
-import { cn } from "~/lib/utils";
-import { Button } from "~/shared/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader } from "~/shared/components/ui/card";
-import { Drawer, DrawerContent } from "~/shared/components/ui/drawer";
-import { Kind } from "~/shared/types/account";
+import { Card } from "~/shared/components/ui/card";
+import { accountKindToHuman } from "~/shared/helpers/account-kind-to-human";
+import { ModuleKind } from "../types/account";
 import { Account } from "../types/preview";
-import { CreateCategory } from "./forms/create";
-import { Preview } from "./preview/card";
-
-const SECOND_BUTTON_HIDDEN_COUNT = 5
+import { PreviewCard } from "./preview-card";
 
 interface Props {
   accounts: Account[]
-  kind: Kind
-  archived: boolean
+  kind: ModuleKind
+  forArchived: boolean
+  onClick: (account: Account) => void
 }
 
-interface NewCategoryButtonProps {
-  archived: boolean
-  onClick: () => void
-  className?: string
-}
-
-function NewCategoryButton({ archived, onClick, className }: NewCategoryButtonProps) {
-  if (archived) return null
-
-  return (
-    <Button
-      variant="link"
-      className={cn(
-        "flex justify-center items-center text-base leading-snug gap-2 h-auto p-6 border-2 border-dashed rounded-xl",
-        className
-      )}
-      onClick={onClick}
-    >
-      <PlusIcon /> New
-    </Button>
-  )
-}
-
-export function ListForKind({ accounts, archived, kind }: Props) {
-  const [openCreate, setOpenCreate] = useState(false)
-
-  const isArchived = (archivedAt?: string | null) => archived ? !!archivedAt : !archivedAt
+export function ListForKind({ accounts, forArchived, kind, onClick }: Props) {
+  const filtered = accounts
+    .filter((account) => account.kind === kind)
+    .filter(({ archivedAt }) => !!archivedAt === forArchived)
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      <NewCategoryButton
-        archived={archived}
-        onClick={() => setOpenCreate(true)}
-      />
-
-      {accounts.map((account) => <Preview key={`category.${account.id}`} account={account} isArchived={isArchived} />)}
-
+      {filtered.map((account) => (
+        <PreviewCard
+          key={`category.${account.id}`}
+          account={account}
+          onClick={() => onClick(account)}
+        />
+      ))}
       {
-        archived && accounts.length === 0 && (
-          <Card>
-            <CardHeader></CardHeader>
-            <CardContent className="text-muted-foreground text-center">
-              No categories have been archived
-            </CardContent>
-            <CardFooter></CardFooter>
+        filtered.length === 0 && (
+          <Card className="text-muted-foreground text-center p-4">
+            {
+              forArchived
+                ? `No ${accountKindToHuman(kind)} Categories have been archived`
+                : `No ${accountKindToHuman(kind)} Categories are active`
+            }
           </Card>
-        )
-      }
-
-      <NewCategoryButton
-        archived={archived}
-        onClick={() => setOpenCreate(true)}
-        className={cn("lg:hidden", accounts.length < SECOND_BUTTON_HIDDEN_COUNT && "hidden")}
-      />
-
-      {
-        !archived && (
-          <Drawer modal open={openCreate} onOpenChange={setOpenCreate}>
-            <DrawerContent>
-              <CreateCategory
-                kind={kind}
-                defaultCurrency={"EUR"}
-                onSuccess={() => {
-                  setOpenCreate(false)
-
-                  redirect(".")
-                }}
-              />
-            </DrawerContent>
-          </Drawer>
         )
       }
     </div>
