9305aca150b9b7efed7477a353aef9ce0bb0a9af
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-22 00:59:30 +0000

    adding archived at to schema

--- webapp/app/modules/categories/components/schemas/update.ts
+++ webapp/app/modules/categories/components/schemas/update.ts
@@ -20,6 +20,7 @@ export const schema = z.object({
     name: z.string().trim().min(3).max(128),
     description: z.string().trim().max(256).optional(),
     icon: z.nativeEnum(ICONS),
+    archivedAt: z.string().datetime().optional(),
   }).array(),
   intent: z.nativeEnum(INTENTS)
 })
