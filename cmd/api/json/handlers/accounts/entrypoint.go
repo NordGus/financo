293e804aaf2fc53ad01b469b169b3ad7d6a97dce
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-25 11:30:27 +0000

    refactoring account unarchive handler to new standard

--- cmd/api/json/handlers/accounts/entrypoint.go
+++ cmd/api/json/handlers/accounts/entrypoint.go
@@ -5,6 +5,7 @@ import (
 	"financo/cmd/api/json/handlers/accounts/create_handler"
 	"financo/cmd/api/json/handlers/accounts/destroy_handler"
 	"financo/cmd/api/json/handlers/accounts/list_handler"
+	"financo/cmd/api/json/handlers/accounts/unarchive_handler"
 	"financo/cmd/api/json/handlers/accounts/update_handler"
 
 	"github.com/go-chi/chi/v5"
@@ -18,6 +19,6 @@ func Routes(r chi.Router) {
 		r.Delete("/", destroy_handler.HandlerFunc)
 		r.Put("/", update_handler.HandlerFunc)
 		r.Patch("/archive", archive_handler.HandlerFunc)
-		r.Patch("/unarchive", unarchive)
+		r.Patch("/unarchive", unarchive_handler.HandlerFunc)
 	})
 }
