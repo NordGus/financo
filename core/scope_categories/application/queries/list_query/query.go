b268f4f6445a91132595ddd03944d965029fac6f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-25 01:12:48 +0000

    using new mapper to list_query

--- core/scope_categories/application/queries/list_query/query.go
+++ core/scope_categories/application/queries/list_query/query.go
@@ -33,40 +33,10 @@ func (q *query) Find(ctx context.Context) ([]responses.Listed, error) {
 	}
 
 	for i := 0; i < len(records); i++ {
-		r := responses.Listed{
-			ID:           records[i].Parent.ID,
-			Kind:         records[i].Parent.Kind,
-			Currency:     records[i].Parent.Currency,
-			Name:         records[i].Parent.Name,
-			Description:  records[i].Parent.Description,
-			Color:        records[i].Parent.Color,
-			Icon:         records[i].Parent.Icon,
-			ArchivedAt:   records[i].Parent.ArchivedAt,
-			DeletedAt:    records[i].Parent.DeletedAt,
-			CreatedAt:    records[i].Parent.CreatedAt,
-			UpdatedAt:    records[i].Parent.UpdatedAt,
-			Transactions: records[i].Parent.DynamicData.Transactions,
-			Children:     make([]responses.ListedChild, 0, len(records[i].Children)),
-		}
-
-		for j := 0; j < len(records[i].Children); j++ {
-			r.Children = append(r.Children, responses.ListedChild{
-				ID:           records[i].Children[j].ID,
-				Kind:         records[i].Children[j].Kind,
-				Currency:     records[i].Children[j].Currency,
-				Name:         records[i].Children[j].Name,
-				Description:  records[i].Children[j].Description,
-				Color:        records[i].Children[j].Color,
-				Icon:         records[i].Children[j].Icon,
-				ArchivedAt:   records[i].Children[j].ArchivedAt,
-				DeletedAt:    records[i].Children[j].DeletedAt,
-				CreatedAt:    records[i].Children[j].CreatedAt,
-				UpdatedAt:    records[i].Children[j].UpdatedAt,
-				Transactions: records[i].Children[j].DynamicData.Transactions,
-			})
-		}
-
-		res = append(res, r)
+		res = append(
+			res,
+			responses.NewListedFromCategoryRecord(records[i]),
+		)
 	}
 
 	return res, nil
