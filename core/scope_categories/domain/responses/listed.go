925c66c79e68098820b4ae11ec5f30881801f70d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-25 01:10:31 +0000

    adding a mapper from category to response.Listed

--- core/scope_categories/domain/responses/listed.go
+++ core/scope_categories/domain/responses/listed.go
@@ -1,6 +1,7 @@
 package responses
 
 import (
+	"financo/core/scope_categories/domain/models/category"
 	"financo/lib/color"
 	"financo/lib/currency"
 	"financo/lib/icon"
@@ -38,4 +39,41 @@ type ListedChild struct {
 	CreatedAt    time.Time                `json:"createdAt"`
 	UpdatedAt    time.Time                `json:"updatedAt"`
 	Transactions int64                    `json:"transactions"`
+}
+
+func NewListedFromCategoryRecord(r category.Record) Listed {
+	out := Listed{
+		ID:           r.Parent.ID,
+		Kind:         r.Parent.Kind,
+		Currency:     r.Parent.Currency,
+		Name:         r.Parent.Name,
+		Description:  r.Parent.Description,
+		Color:        r.Parent.Color,
+		Icon:         r.Parent.Icon,
+		ArchivedAt:   r.Parent.ArchivedAt,
+		DeletedAt:    r.Parent.DeletedAt,
+		CreatedAt:    r.Parent.CreatedAt,
+		UpdatedAt:    r.Parent.UpdatedAt,
+		Transactions: r.Parent.DynamicData.Transactions,
+		Children:     make([]ListedChild, 0, len(r.Children)),
+	}
+
+	for i := 0; i < len(out.Children); i++ {
+		out.Children = append(out.Children, ListedChild{
+			ID:           r.Children[i].ID,
+			Kind:         r.Children[i].Kind,
+			Currency:     r.Children[i].Currency,
+			Name:         r.Children[i].Name,
+			Description:  r.Children[i].Description,
+			Color:        r.Children[i].Color,
+			Icon:         r.Children[i].Icon,
+			ArchivedAt:   r.Children[i].ArchivedAt,
+			DeletedAt:    r.Children[i].DeletedAt,
+			CreatedAt:    r.Children[i].CreatedAt,
+			UpdatedAt:    r.Children[i].UpdatedAt,
+			Transactions: r.Children[i].DynamicData.Transactions,
+		})
+	}
+
+	return out
 }
