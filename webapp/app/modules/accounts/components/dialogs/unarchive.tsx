cfc137e9061416bfcd06858e614d1b51e3d608d7
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-26 12:07:41 +0000

    refactoring accounts confirmation dialogs to use shared confirmation dialog

--- webapp/app/modules/accounts/components/dialogs/unarchive.tsx
+++ webapp/app/modules/accounts/components/dialogs/unarchive.tsx
@@ -1,14 +1,4 @@
-import { Throbber } from "~/shared/components/throbber";
-import { Button } from "~/shared/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle
-} from "~/shared/components/ui/drawer";
+import { ConfirmationDialog } from "~/shared/components/dialogs/confirmation";
 import { Account } from "../../types/account";
 
 interface Props {
@@ -23,25 +13,8 @@ export function UnarchiveAccount({ account, open, onOpenChange, onConfirm, submi
   const { id, name } = account
 
   return (
-    <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Are you sure?</DrawerTitle>
-          <DrawerDescription className="space-y-2">
-            You are about to unarchive <span className="font-bold">{name}</span>. This will make it reappear as an selectable option in <span className="font-bold text-foreground">financo</span>. <span className="font-bold">This action can be reverted from the Accounts</span>.
-          </DrawerDescription>
-        </DrawerHeader>
-        <DrawerFooter>
-          <Button onClick={() => onConfirm(id)} disabled={submitting}>
-            {submitting ? <Throbber size={"sm"} /> : "Confirm"}
-          </Button>
-          <DrawerClose asChild>
-            <Button variant={"outline"} disabled={submitting}>
-              Cancel
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+    <ConfirmationDialog open={open} onOpenChange={onOpenChange} onConfirm={() => onConfirm(id)} submitting={submitting}>
+      You are about to unarchive <span className="font-bold">{name}</span>. This will make it reappear as an selectable option in <span className="font-bold text-foreground">financo</span>. <span className="font-bold">This action can be reverted from the Accounts</span>.
+    </ConfirmationDialog>
   )
 }
