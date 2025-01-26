58bbf2140f5998b6c454876255428afb60b0b491
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-26 11:22:59 +0000

    fixing bug in arhive category command calling the incorrect repository command

--- core/scope_categories/application/commands/archive_command/command.go
+++ core/scope_categories/application/commands/archive_command/command.go
@@ -53,7 +53,7 @@ func (c *command) Run(ctx context.Context) (responses.Listed, error) {
 		return res, fmt.Errorf("archive_command: category id=(%d) not found", c.req.ID)
 	}
 
-	record, err = c.archival.Unarchive(ctx, record, timestamp)
+	record, err = c.archival.Archive(ctx, record, timestamp)
 	if err != nil {
 		return res, err
 	}
