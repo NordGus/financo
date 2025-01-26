39954a97530bf06996a90bd4cb05f06782658f38
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-26 11:48:29 +0000

    implementing shared confirm dialog component

---
+++ webapp/app/shared/components/dialogs/confirm-async-action.tsx
@@ -0,0 +1,50 @@
+import { PropsWithChildren } from "react";
+import { Throbber } from "~/shared/components/throbber";
+import { Button } from "~/shared/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle
+} from "~/shared/components/ui/drawer";
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  onConfirm: () => void
+  submitting: boolean
+}
+
+export function ConfirmAsyncActionDialog({
+  open,
+  onOpenChange,
+  onConfirm,
+  submitting,
+  children
+}: PropsWithChildren<Props>) {
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you sure?</DrawerTitle>
+          <DrawerDescription className="space-y-2">
+            {children}
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button onClick={onConfirm} disabled={submitting}>
+            {submitting ? <Throbber size={"sm"} /> : "Confirm"}
+          </Button>
+          <DrawerClose asChild>
+            <Button variant={"outline"} disabled={submitting}>
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
