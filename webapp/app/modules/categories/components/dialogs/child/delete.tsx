2f985caa832e0ee36430a5ef924e4f49d76300ea
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-26 12:01:18 +0000

    refactoring categories confirmation dialogs to use shared confirmation dialog

--- webapp/app/modules/categories/components/dialogs/child/delete.tsx
+++ webapp/app/modules/categories/components/dialogs/child/delete.tsx
@@ -1,14 +1,4 @@
-import { Throbber } from "~/shared/components/throbber";
-import { Button } from "~/shared/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle
-} from "~/shared/components/ui/drawer";
+import { ConfirmationDialog } from "~/shared/components/dialogs/confirmation";
 
 interface Props {
   open: boolean
@@ -28,25 +18,8 @@ export function DeleteDialog({
   submitting
 }: Props) {
   return (
-    <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription className="space-y-2">
-            You are about to permanently delete <span className="font-bold">{name}</span> from your Categories and its related <span className="font-bold">{transactions}</span> transaction(s) from your ledger. <span className="font-bold">This action cannot be undone</span>.
-          </DrawerDescription>
-        </DrawerHeader>
-        <DrawerFooter>
-          <Button onClick={onConfirm} disabled={submitting}>
-            {submitting ? <Throbber size={"sm"} /> : "Confirm"}
-          </Button>
-          <DrawerClose asChild>
-            <Button variant={"outline"} disabled={submitting}>
-              Cancel
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+    <ConfirmationDialog open={open} onOpenChange={onOpenChange} onConfirm={onConfirm} submitting={submitting}>
+      You are about to permanently delete <span className="font-bold">{name}</span> from your Categories and its related <span className="font-bold">{transactions}</span> transaction(s) from your ledger. <span className="font-bold">This action cannot be undone</span>.
+    </ConfirmationDialog>
   )
 }
