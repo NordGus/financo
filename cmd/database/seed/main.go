ece5a0d45a08f9a8ed677222147e9b195dc0eedb
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-26 20:35:46 +0000

    adding shutdown service to seeds

--- cmd/database/seed/main.go
+++ cmd/database/seed/main.go
@@ -8,8 +8,8 @@ import (
 	"financo/cmd/database/seed/transactions"
 	"financo/services/message_buses"
 	"financo/services/postgresql_database"
+	"financo/services/shutdown"
 	"log"
-	"os"
 	"sync"
 	"time"
 )
@@ -20,40 +20,53 @@ func main() {
 		ctx   = context.Background()
 		start = time.Now()
 
-		postgresqlService   = postgresql_database.New()
-		messageBusesService = message_buses.Initialize(wg)
+		pg = postgresql_database.New()
+		mb = message_buses.Initialize(wg)
 	)
 
-	defer func() {
-		if err := postgresqlService.Close(); err != nil {
-			log.Printf("failed to close postgresql database connection: %s\n", err)
-		}
-	}()
+	shutdown.Defer(shutdown.Closure{
+		Name: "postgresql_database",
+		Func: func() {
+			if err := pg.Close(); err != nil {
+				log.Printf("failed to close postgresql database connection: %s\n", err)
+			}
+			log.Println("database connection close")
+		},
+	})
 
-	defer func() {
-		if err := messageBusesService.Close(); err != nil {
-			log.Printf("failed to close message busses connection: %s\n", err)
-		}
-	}()
+	shutdown.Defer(shutdown.Closure{
+		Name: "message_buses",
+		Func: func() {
+			if err := mb.Close(); err != nil {
+				log.Printf("failed to close message busses connection: %s\n", err)
+			}
+			log.Println("message bus connection close")
+		},
+	})
+
+	shutdown.Defer(shutdown.Closure{
+		Name: "sync.WaitGroup.Wait",
+		Func: func() { wg.Wait() },
+	})
 
 	log.Println("seeding database")
 
 	createdSG, err := savings_goals.CreateSavingsGoals(ctx)
 	if err != nil {
 		log.Println("failed to seed savings goals, reason:", err.Error())
-		os.Exit(1)
+		shutdown.Exit(1)
 	}
 
 	acc, err := accounts.SeedAccounts(ctx, start.UTC())
 	if err != nil {
 		log.Println("failed to seed accounts, reason:", err.Error())
-		os.Exit(1)
+		shutdown.Exit(1)
 	}
 
 	cat, err := categories.SeedCategories(ctx, start.UTC())
 	if err != nil {
 		log.Println("failed to seed categories, reason:", err.Error())
-		os.Exit(1)
+		shutdown.Exit(1)
 	}
 
 	for key, id := range cat {
@@ -63,14 +76,15 @@ func main() {
 	err = transactions.SeedTransactions(ctx, acc, start.UTC())
 	if err != nil {
 		log.Println("failed to seed transactions, reason:", err.Error())
-		os.Exit(1)
+		shutdown.Exit(1)
 	}
 
 	_, err = savings_goals.AchieveSavingsGoals(ctx, createdSG)
 	if err != nil {
 		log.Println("failed to mark savings goals as achieve, reason:", err.Error())
-		os.Exit(1)
+		shutdown.Exit(1)
 	}
 
 	log.Printf("database seeded (took %s)\n", time.Since(start))
+	shutdown.Exit(0)
 }
