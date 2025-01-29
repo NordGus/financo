ece301188dd0ce045683ba0772effc34407d5e68
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-01-29 07:06:56 +0000

    adjusting design

--- webapp/app/modules/ledger/screens/index.tsx
+++ webapp/app/modules/ledger/screens/index.tsx
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import { CalendarIcon, ListFilterIcon, PlusIcon } from "lucide-react";
+import { CalendarIcon, ListFilterIcon, MoveHorizontalIcon, PlusIcon } from "lucide-react";
 import { Fragment } from "react";
 import { Button } from "~/modules/shared/components/ui/button";
 
@@ -7,6 +7,14 @@ export function Screen() {
   return (
     <Fragment>
       <div className="relative overflow-hidden h-full">
+        <div className="absolute top-0 left-0 right-0 p-4 flex gap-4 justify-stretch w-full">
+          <Button
+            variant={"secondary"}
+            className="shadow-lg w-full"
+          >
+            {format(new Date(), "LLL do, yyyy")} <MoveHorizontalIcon /> {format(new Date(), "LLL do, yyyy")}
+          </Button>
+        </div>
         <div className="absolute bottom-0 right-0 p-4 inline-flex gap-4 flex-wrap justify-end">
           <Button
             size={"icon"}
@@ -14,14 +22,15 @@ export function Screen() {
             className="shadow-lg"
             onClick={() => { }}
           >
-            <ListFilterIcon />
+            <CalendarIcon />
           </Button>
           <Button
+            size={"icon"}
             variant={"secondary"}
             className="shadow-lg"
             onClick={() => { }}
           >
-            <CalendarIcon /> {format(new Date(), "LLL d, yyyy")} to {format(new Date(), "LLL d, yyyy")}
+            <ListFilterIcon />
           </Button>
           <Button
             size={"icon"}
@@ -32,9 +41,10 @@ export function Screen() {
           </Button>
         </div>
         <div className="overflow-x-hidden overflow-y-auto h-full p-4">
+          <span className="content-[''] h-9 block my-2" />
           <div className="flex flex-col gap-4">
-            <span className="content-[''] h-9" />
           </div>
+          <span className="content-[''] h-9 block my-2" />
         </div>
       </div>
     </Fragment>
