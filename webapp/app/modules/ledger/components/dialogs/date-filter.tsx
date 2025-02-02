61043eddff7e7025a8d70c08cd0f402ebd46177b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-02-02 00:40:04 +0000

    implementing calendar filter picker

---
+++ webapp/app/modules/ledger/components/dialogs/date-filter.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import { DateRange } from "react-day-picker"
+import { Button } from "~/modules/shared/components/ui/button"
+import { Calendar } from "~/modules/shared/components/ui/calendar"
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle } from "~/modules/shared/components/ui/drawer"
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  range: DateRange | undefined
+  onConfirm: (range: DateRange | undefined) => void
+  submitting: boolean
+}
+
+export function DateFilter({ open, onOpenChange, range, onConfirm, submitting }: Props) {
+  const [date, setDate] = useState<DateRange | undefined>(range)
+
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Period</DrawerTitle>
+          <DrawerDescription className="hidden" data-hidden>
+            Select which period you want preview
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="flex justify-center">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+          />
+        </div>
+        <DrawerFooter>
+          <Button onClick={() => onConfirm(date)} disabled={submitting}>
+            Apply
+          </Button>
+          <DrawerClose asChild>
+            <Button variant={"outline"} disabled={submitting}>
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
