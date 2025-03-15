0f6d8f9c11559f9750b76652c14c71fc63347333
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-03-15 00:03:25 +0000

    updating manual copy

--- webapp/app/modules/accounts/manual/account-kinds-manual.tsx
+++ webapp/app/modules/accounts/manual/account-kinds-manual.tsx
@@ -9,7 +9,7 @@ export const accountKindsManual: Record<ModuleKind, Copy> = {
         A Capital Account represents your personal bank account, cash wallet, Paypal account, Steam wallet, the bank account you use for your freelance payments, etc.
       </p>
       <p>
-        Basically any kind of account where you store capital, but it generates no interest.
+        Basically any kind of account where you store your capital.
       </p>
     </>
   },
@@ -17,10 +17,10 @@ export const accountKindsManual: Record<ModuleKind, Copy> = {
     title: "What is a Savings Account",
     message: <>
       <p>
-        A Savings Account represents your personal bank savings account, bank deposits, government bonds or any other really simple financial instrument that generates interest in your favour.
+        A Savings Account represents your personal bank savings account, bank deposits, government bonds or any other financial instrument that generates that represents your savings for the future.
       </p>
       <p>
-        Basically any kind of capital store that generates interests in your favour.
+        Basically any account or asset where you save your capital for the future.
       </p>
     </>
   },
@@ -28,10 +28,10 @@ export const accountKindsManual: Record<ModuleKind, Copy> = {
     title: "What is a Debt Account?",
     message: <>
       <p>
-        A Loan Account represents your student loan, car loan, smartphone loan, etc.
+        A Debt Account represents your student loan, car loan, smartphone loan, friendly debt, etc.
       </p>
       <p>
-        Basically any kind of simple debt you owe or are owed that generates interests for or against you.
+        Basically any kind of debt you owe or are owed.
       </p>
     </>
   },
@@ -42,7 +42,7 @@ export const accountKindsManual: Record<ModuleKind, Copy> = {
         A Credit Account represents your credit cards, credit lines, etc.
       </p>
       <p>
-        Basically any kind of credit line you owe or are owed that generates interest for or against you and has a set limit and your are somewhat free to tap into for whatever you want to expend it on.
+        Basically any kind of credit line you owe or are owed that generates interest for or against you and has a set limit and you are somewhat free to tap into for whatever you want to expend it on.
       </p>
     </>
   },
