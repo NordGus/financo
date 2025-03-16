39e5471012f2d0cf5ce59d5242d847b228c2fc8d
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2025-03-16 18:10:45 +0000

    refactoring create transaction command to handle new fields

--- core/scope_transactions/domain/requests/create.go
+++ core/scope_transactions/domain/requests/create.go
@@ -1,6 +1,7 @@
 package requests
 
 import (
+	"financo/lib/currency"
 	"financo/lib/nullable"
 	"financo/models/transaction"
 	"time"
@@ -10,10 +11,12 @@ type Create struct {
 	IssuedAt     time.Time                `json:"issuedAt"`
 	ExecutedAt   nullable.Type[time.Time] `json:"executedAt"`
 	Notes        nullable.Type[string]    `json:"notes"`
-	SourceID     int64                    `json:"sourceID"`
-	TargetID     int64                    `json:"targetID"`
+	Currency     currency.Type            `json:"currency"`
+	SourceID     int64                    `json:"sourceId"`
+	TargetID     int64                    `json:"targetId"`
 	SourceAmount int64                    `json:"sourceAmount"`
 	TargetAmount int64                    `json:"targetAmount"`
+	Kind         transaction.Kind         `json:"kind"`
 }
 
 func (r Create) ToTransactionRecord(timestamp time.Time) transaction.Record {
@@ -24,6 +27,7 @@ func (r Create) ToTransactionRecord(timestamp time.Time) transaction.Record {
 		SourceAmount: r.SourceAmount,
 		TargetAmount: r.TargetAmount,
 		Notes:        r.Notes,
+		Currency:     r.Currency,
 		IssuedAt:     r.IssuedAt.UTC(),
 		ExecutedAt:   r.ExecutedAt,
 		DeletedAt:    nullable.Type[time.Time]{},
@@ -34,6 +38,8 @@ func (r Create) ToTransactionRecord(timestamp time.Time) transaction.Record {
 	if record.ExecutedAt.Valid {
 		record.ExecutedAt = nullable.New(record.ExecutedAt.Val.UTC())
 	}
+
+	record.Metadata.Kind = r.Kind
 
 	return record
 }
