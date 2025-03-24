bd72476b5ebf3c0e977e5fb1ce197ecf09c3e699
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-03-24 07:01:31 +0000

    updating create type

--- webapp/app/modules/ledger/types/create.ts
+++ webapp/app/modules/ledger/types/create.ts
@@ -1,9 +1,9 @@
 import { Currency } from "~/modules/shared/types/currency"
-import { Kind } from "./transactions"
+import { Kind, Transaction } from "./transactions"
 
 export type Create = {
   issuedAt: string // it is a date formatted YYYY-MM-DD
-  executedAt: string | null // it is a date formatted YYYY-MM-DD
+  executedAt?: string | null // it is a date formatted YYYY-MM-DD
   notes?: string | null
   currency: Currency
   sourceId: number
@@ -17,7 +17,6 @@ type OnCreateTransactionSuccess = () => void
 type OnCreateTransactionFailure = () => void
 
 export type CreateTransactionAction =
-  (values: Create, success: OnCreateTransactionSuccess, failure: OnCreateTransactionFailure) => Promise<void>
+  (values: Create, success: OnCreateTransactionSuccess, failure: OnCreateTransactionFailure) => Promise<Transaction>
 export type OnSubmitCreateTransactionAction =
   (values: Create) => Promise<void>
-
