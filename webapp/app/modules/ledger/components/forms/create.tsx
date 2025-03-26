2ab1ec7841ab2d90d9f2dd42b54f7e3b5a912a09
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-03-26 00:41:54 +0000

    wireframing form

--- webapp/app/modules/ledger/components/forms/create.tsx
+++ webapp/app/modules/ledger/components/forms/create.tsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns"
+import { useRef } from "react"
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "~/modules/shared/components/ui/drawer"
 import { useCreationStore } from "../../stores/creation"
 import { OnSubmitCreateTransactionAction } from "../../types/create"
@@ -44,9 +46,49 @@ interface FormProps {
 }
 
 function CreateForm({ }: FormProps) {
+  const today = useRef<Date>(new Date())
+
   return (
-    <>
-      <p>Form goes here</p>
-    </>
+    <div className="p-4 grid grid-cols-2 grid-rows-auto gap-2">
+      <div className="rounded-lg border h-16">
+        From account
+      </div>
+      <div className="rounded-lg border h-16">
+        To account
+      </div>
+      <div className="rounded-lg bg-zinc-700">
+        Source amount
+      </div>
+      <div className="rounded-lg bg-zinc-700">
+        Target amount
+      </div>
+      <div className="rounded-lg border col-span-2">
+        Notes
+      </div>
+      <div className="col-span-2 grid grid-cols-5 grid-rows-4 gap-2">
+        <div className="rounded-lg border aspect-square">div</div>
+        <div className="rounded-lg border aspect-square">7</div>
+        <div className="rounded-lg border aspect-square">8</div>
+        <div className="rounded-lg border aspect-square">9</div>
+        <div className="rounded-lg border aspect-square">back</div>
+        <div className="rounded-lg border aspect-square">by</div>
+        <div className="rounded-lg border aspect-square">4</div>
+        <div className="rounded-lg border aspect-square">5</div>
+        <div className="rounded-lg border aspect-square">6</div>
+        <div className="rounded-lg border aspect-square">date</div>
+        <div className="rounded-lg border aspect-square">minus</div>
+        <div className="rounded-lg border aspect-square">1</div>
+        <div className="rounded-lg border aspect-square">2</div>
+        <div className="rounded-lg border aspect-square">3</div>
+        <div className="rounded-lg border row-span-2">done</div>
+        <div className="rounded-lg border aspect-square">plus</div>
+        <div className="rounded-lg border aspect-square">curr</div>
+        <div className="rounded-lg border aspect-square">0</div>
+        <div className="rounded-lg border aspect-square">info</div>
+      </div>
+      <div className="text-sm col-span-2 text-center">
+        Issued {format(today.current, "LLL dd, y")}. Effective {format(today.current, "LLL dd, y")}
+      </div>
+    </div>
   )
 }
