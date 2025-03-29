917abd73f3d8fe58bd2a3036308531350442e567
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-03-29 17:55:54 +0000

    refactor filters module: remove deprecated functions and enhance type definitions with documentation

--- webapp/app/modules/ledger/defaults/filters.ts
+++
@@ -1,9 +0,0 @@
-import { add } from "date-fns"
-
-export function filterTo() {
-  return new Date()
-}
-
-export function filterFrom() {
-  return add(filterTo(), { months: -1 })
-}
--- webapp/app/modules/ledger/types/filters.ts
+++ webapp/app/modules/ledger/types/filters.ts
@@ -1,18 +1,42 @@
-import { add, format, getDayOfYear, isFirstDayOfMonth, isLastDayOfMonth, isSameDay, isSameWeek, isSaturday, isSunday, lastDayOfYear } from "date-fns"
+/*
+  This module defines types and functions for handling filters in a ledger application.
+  It includes period estimation, URL search parameters conversion, and default values.
+*/
+
+import {
+  add,
+  format,
+  getDayOfYear,
+  isFirstDayOfMonth,
+  isLastDayOfMonth,
+  isSameDay,
+  isSameWeek,
+  isSaturday,
+  isSunday,
+  lastDayOfYear
+} from "date-fns"
 import { URLSearchParamsInit } from "react-router"
 
-export { fromURLSearchParams, toURLSearchParamsInit }
+export { FiltersSearchParamsKeys, fromURLSearchParams, toURLSearchParamsInit }
 export type { Filters, Period }
 
-// URLSearchParams keys
-const PERIOD = "period"
-const FROM = "from"
-const TO = "to"
-const ACCOUNTS = "accounts"
-const CATEGORIES = "categories"
+/**
+  Defines the keys in the URLSearchParams that map to Filters.
+*/
+const FiltersSearchParamsKeys = {
+  PERIOD: "period",
+  FROM: "from",
+  TO: "to",
+  ACCOUNTS: "accounts",
+  CATEGORIES: "categories",
+} as const
+
 // Date format strings
 const DATE_FORMAT = "yyyy-MM-dd"
 
+/**
+ Filters interface defines the structure of filter parameters used in the ledger application.
+*/
 type Filters = {
   period: Period
   from?: Date
@@ -21,6 +45,15 @@ type Filters = {
   categories: number[]
 }
 
+/**
+  Defines the possible period values for filtering transactions.
+  - UNLIMITED: Entire ledger history
+  - DAILY: Transactions within a single day
+  - WEEKLY: Transactions within a week
+  - MONTHLY: Transactions within a month
+  - YEARLY: Transactions within a year
+  - CUSTOM: Transactions within a finite none structured date range
+*/
 const _periods = {
   UNLIMITED: "unlimited",
   DAILY: "daily",
@@ -30,8 +63,20 @@ const _periods = {
   CUSTOM: "custom",
 } as const
 
+/**
+  Defines the possible period values for filtering transactions.
+*/
 type Periods = typeof _periods
 
+/**
+  Period is the different periods that the ledger can be filtered by.
+  - unlimited: Entire ledger history
+  - daily: Transactions within a single day
+  - weekly: Transactions within a week
+  - monthly: Transactions within a month
+  - yearly: Transactions within a year
+  - custom: Transactions within a finite none structured date range
+*/
 type Period =
   Periods["UNLIMITED"] |
   Periods["DAILY"] |
@@ -40,6 +85,14 @@ type Period =
   Periods["YEARLY"] |
   Periods["CUSTOM"]
 
+/**
+  Estimates the period based on the provided date range.
+  - If the start date is the first day of the year and the end date is the last day of the year, it returns "yearly".
+  - If the start date is the first day of the month and the end date is the last day of the month, it returns "monthly".
+  - If the start date is the first day of the week and the end date is the last day of the week, it returns "weekly".
+  - If the start date and end date are the same, it returns "daily".
+  - Otherwise, it returns "custom".
+*/
 function estimatePeriod(from?: Date | null, to?: Date | null): Period {
   if (!from) return "custom"
   if (!to) return "custom"
@@ -58,6 +111,11 @@ function estimatePeriod(from?: Date | null, to?: Date | null): Period {
   }
 }
 
+/**
+  Converts a given URLSearchParam value to a Period.
+  - If the value is "custom", "daily", "weekly", "monthly", "yearly", or "unlimited", it returns the corresponding Period.
+  - Otherwise, it estimates the period based on the provided date range.
+*/
 function toPeriod(value: string | null | undefined, from?: Date, to?: Date): Period {
   switch (value) {
     case _periods.CUSTOM:
@@ -77,49 +135,81 @@ function toPeriod(value: string | null | undefined, from?: Date, to?: Date): Per
   }
 }
 
+/**
+  Converts a given URLSearchParam value to an optional Date.
+  - If the value is null or undefined, it returns undefined.
+  - Otherwise, it returns a new Date object created from the value.
+*/
 function toOptionalDate(value?: string | null) {
   if (!value) return undefined
   return new Date(value)
 }
 
+/**
+  Converts a given URLSearchParam values to an array of numbers.
+  - If the value is null or empty, it returns an empty array.
+  - Otherwise, transform it to number[].
+*/
 function toNumbers(values?: string[] | null) {
   if (!values) return []
   if (values.length === 0) return []
   return values.map(val => Number(val))
 }
 
+/**
+  Converts an optional Date to a URLSearchParam value.
+  - If the date is null or undefined, it returns undefined.
+  - Otherwise, it returns the date formatted as a string using the DATE_FORMAT format string.
+*/
 function optionalDateToParam(date?: Date | null) {
   if (!date) return undefined
   return format(date, DATE_FORMAT)
 }
 
+/**
+  Converts an array of numbers to a URLSearchParam value.
+  - If the array is empty, it returns undefined.
+  - Otherwise, it returns the values as string[].
+*/
 function numbersToParam(numbers: number[]) {
   if (numbers.length > 0) return numbers.map(n => n.toString())
   return undefined
 }
 
+/**
+  Converts Filters to URLSearchParamsInit.
+  Filters that are not set are omitted from the URLSearchParams.
+*/
 function toURLSearchParamsInit({ period, from, to, accounts, categories }: Filters): URLSearchParamsInit {
   return Object.fromEntries([
-    [PERIOD, period],
-    [FROM, optionalDateToParam(from)],
-    [TO, optionalDateToParam(to)],
-    [ACCOUNTS, numbersToParam(accounts)],
-    [CATEGORIES, numbersToParam(categories)]
+    [FiltersSearchParamsKeys.PERIOD, period],
+    [FiltersSearchParamsKeys.FROM, optionalDateToParam(from)],
+    [FiltersSearchParamsKeys.TO, optionalDateToParam(to)],
+    [FiltersSearchParamsKeys.ACCOUNTS, numbersToParam(accounts)],
+    [FiltersSearchParamsKeys.CATEGORIES, numbersToParam(categories)]
   ].filter(([, val]) => !!val))
 }
 
+/**
+  Converts URLSearchParams to Filters.
+  If the period is not specified, it returns default values.
+*/
 function fromURLSearchParams(params: URLSearchParams): Filters {
-  if (!params.get(PERIOD)) return defaultValues()
+  if (!params.get(FiltersSearchParamsKeys.PERIOD)) return defaultValues()
 
-  const from = toOptionalDate(params.get(FROM))
-  const to = toOptionalDate(params.get(TO))
-  const accounts = toNumbers(params.getAll(ACCOUNTS))
-  const categories = toNumbers(params.getAll(CATEGORIES))
-  const period = toPeriod(params.get(PERIOD), from, to)
+  const from = toOptionalDate(params.get(FiltersSearchParamsKeys.FROM))
+  const to = toOptionalDate(params.get(FiltersSearchParamsKeys.TO))
+  const accounts = toNumbers(params.getAll(FiltersSearchParamsKeys.ACCOUNTS))
+  const categories = toNumbers(params.getAll(FiltersSearchParamsKeys.CATEGORIES))
+  const period = toPeriod(params.get(FiltersSearchParamsKeys.PERIOD), from, to)
 
   return { period, from, to, accounts, categories }
 }
 
+/**
+  Returns default values for Filters.
+  The default period is "custom", and the date range is set to the last month.
+*/
 function defaultValues(): Filters {
   return {
     period: "custom",
