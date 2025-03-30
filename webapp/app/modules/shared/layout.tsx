ad33d40ec01e274f585d90a31754be0d9016191e
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-03-30 09:12:58 +0000

    adding global throbber to indicate navigation to a different screen

--- webapp/app/modules/shared/layout.tsx
+++ webapp/app/modules/shared/layout.tsx
@@ -10,9 +10,10 @@ import {
   VaultIcon
 } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation, useNavigation } from "react-router";
 import { Breadcrumbs } from "./components/breadcrumbs";
 import { NavItem } from "./components/nav-item";
+import { Throbber } from "./components/throbber";
 import { Button } from "./components/ui/button";
 import {
   Drawer,
@@ -25,6 +26,7 @@ import { Toaster } from "./components/ui/sonner";
 
 export default function Layout() {
   const location = useLocation()
+  const { state: navigationState } = useNavigation()
   const [openNav, setOpenNav] = useState(false)
 
   useEffect(() => setOpenNav(false), [location.pathname])
@@ -35,7 +37,13 @@ export default function Layout() {
         className="h-full w-full overflow-hidden grid grid-rows-layout items-stretch"
       >
         <main className="grow h-full overflow-hidden">
-          <Outlet />
+          {
+            navigationState === "loading" && location.search.length === 0 ? (
+              <div className="flex items-center justify-center h-full">
+                <Throbber />
+              </div>
+            ) : <Outlet />
+          }
           <Toaster position="top-center" closeButton richColors />
         </main>
         <div className="flex justify-between items-center bg-transparent border-t">
@@ -55,29 +63,29 @@ export default function Layout() {
             <DrawerDescription>Navigate through financo</DrawerDescription>
           </DrawerHeader>
           <nav className="grid grid-cols-2 gap-2 p-4">
-            <NavItem to="/morning-brew">
+            <NavItem to="/morning-brew" onClick={() => setOpenNav(false)}>
               <CoffeeIcon /> Morning Brew
             </NavItem>
-            <NavItem to="/accounts">
+            <NavItem to="/accounts" onClick={() => setOpenNav(false)}>
               <VaultIcon /> Accounts
             </NavItem>
-            <NavItem to="/categories">
+            <NavItem to="/categories" onClick={() => setOpenNav(false)}>
               <BookmarkIcon /> Categories
             </NavItem>
-            <NavItem to="/ledger">
+            <NavItem to="/ledger" onClick={() => setOpenNav(false)}>
               <BookMarkedIcon /> Ledger
             </NavItem>
-            <NavItem to="/budgets">
+            <NavItem to="/budgets" onClick={() => setOpenNav(false)}>
               <NotebookTabsIcon /> Budgets
             </NavItem>
-            <NavItem to="/payment-plans">
+            <NavItem to="/payment-plans" onClick={() => setOpenNav(false)}>
               <RouteIcon /> Payment plans
             </NavItem>
             <span className="grow contents-[''] h-10 col-span-2" />
-            <NavItem to="/achievements">
+            <NavItem to="/achievements" onClick={() => setOpenNav(false)}>
               <TrophyIcon /> Achievements
             </NavItem>
-            <NavItem to="/settings">
+            <NavItem to="/settings" onClick={() => setOpenNav(false)}>
               <SettingsIcon /> Settings
             </NavItem>
           </nav>
