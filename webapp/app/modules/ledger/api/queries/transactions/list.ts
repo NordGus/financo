5b800ee723ed1c702b668e9e311022654db7016a
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-03-30 12:49:15 +0000

    using new toURLSearchParams mapper for filters

--- webapp/app/modules/ledger/api/queries/transactions/list.ts
+++ webapp/app/modules/ledger/api/queries/transactions/list.ts
@@ -1,19 +1,8 @@
-import { format } from "date-fns";
-import { Filters, Transaction } from "~/modules/ledger/types/transactions";
+import { Filters, toURLSearchParams } from "~/modules/ledger/types/filters";
+import { Transaction } from "~/modules/ledger/types/transactions";
 
-export async function list(filters: Filters, signal: AbortSignal): Promise<Transaction[]> {
-  const params = Object.entries(filters)
-    .filter(([__key, value]) => {
-      if (!value) return false
-      if (value instanceof Date) return true
-      return value.length > 0
-    })
-    .map(([key, value]) => {
-      if (value instanceof Date) return [key, format(value, "yyyy-MM-dd")]
-      return [key, value.map((val) => val.toString())]
-    })
-
-  const query = new URLSearchParams(Object.fromEntries(params))
+export async function list(filters: Filters): Promise<Transaction[]> {
+  const query = new URLSearchParams(toURLSearchParams(filters))
 
   const response = await fetch(
     `/api/transactions?${query.toString()}`,
@@ -21,7 +10,6 @@ export async function list(filters: Filters, signal: AbortSignal): Promise<Trans
       headers: {
         "Content-Type": "application/json; charset=UTF-8"
       },
-      signal,
     }
   )
 
