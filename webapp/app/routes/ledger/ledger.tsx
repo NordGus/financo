7eed7369377bff8e3e7657fab238f3f0cf3d8a01
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-04-07 20:42:01 +0000

    refactoring ledger transactions

--- webapp/app/routes/ledger/ledger.tsx
+++ webapp/app/routes/ledger/ledger.tsx
@@ -1,23 +1,24 @@
 import { Outlet } from "react-router";
 import { list as listTransactionsQuery } from "~/modules/ledger/api/queries/transactions/list";
+import { TransactionsSearchResults } from "~/modules/ledger/components/transactions-search-results";
+import { mapToExecutedTransactions } from "~/modules/ledger/types/transactions";
 import { getFilters } from "~/modules/ledger/utils/router-requests";
 import { Route } from "./+types/ledger";
 
 export async function clientLoader({ request }: Route.LoaderArgs) {
-  const transactions = await listTransactionsQuery(getFilters(request))
+  const executedTransactions = mapToExecutedTransactions(await listTransactionsQuery(getFilters(request)))
 
-  return { transactions }
+  return { executedTransactions }
 }
 
-export default function Index({ }: Route.ComponentProps) {
-  // const accounts = useAccountsMap()
-  // const [, setSearchParams] = useSearchParams()
+export default function Index({ loaderData: { executedTransactions }, matches }: Route.ComponentProps) {
+  const { data: { accountsMap } } = matches[2] // extracting data from webapp/app/routes/ledger/_layout.tsx's loader.
 
   return (
     <>
-      <section className="h-full py-2 overflow-y-auto">
-        <div className="h-full border rounded-lg">
-          listado
+      <section className="flex flex-col h-full max-h-full overflow-y-auto no-scrollbar">
+        <div className="my-4 grow border rounded-lg overflow-clip">
+          <TransactionsSearchResults transactions={executedTransactions} accounts={accountsMap} />
         </div>
       </section>
       <Outlet />
