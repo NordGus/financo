cca62fd1bed685e8b029c60aa66ca30b46ec9ddb
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-04-07 20:39:53 +0000

    adding new accounts-filter-bar component

--- webapp/app/routes/ledger/_layout.tsx
+++ webapp/app/routes/ledger/_layout.tsx
@@ -1,10 +1,12 @@
 import { Plus } from "lucide-react";
-import { Outlet } from "react-router";
+import { Outlet, useSearchParams } from "react-router";
 import { list as listAccountsQuery } from "~/modules/ledger/api/queries/accounts/list";
+import { AccountsFilterBar } from "~/modules/ledger/components/accounts-filter-bar";
 import { DateFilter } from "~/modules/ledger/components/date-filter";
 import { AccountsContextProvider } from "~/modules/ledger/contexts/accounts-context";
 import { FiltersContextProvider } from "~/modules/ledger/contexts/filters-contenxt";
 import { Accounts } from "~/modules/ledger/types/accounts";
+import { updateURLSearchParams } from "~/modules/ledger/types/filters";
 import { getFilters } from "~/modules/ledger/utils/router-requests";
 import { ToolBar } from "~/modules/shared/components/tool-bar";
 import { Button } from "~/modules/shared/components/ui/button";
@@ -19,8 +21,9 @@ export function meta({ }: Route.MetaArgs) {
 
 export async function clientLoader({ request }: Route.LoaderArgs) {
   const filters = getFilters(request)
-  const accounts = await listAccountsQuery()
-  const accountsMap: Accounts = new Map(accounts.map((account) => ([account.id, account])))
+  const accountsData = await listAccountsQuery()
+  const accountsMap: Accounts = new Map(accountsData.map((account) => ([account.id, account])))
+  const accounts = accountsData.filter(account => !account.parentId)
 
   return {
     breadcrumb: "Ledger",
@@ -31,6 +34,22 @@ export async function clientLoader({ request }: Route.LoaderArgs) {
 }
 
 export default function Layout({ loaderData: { filters, accounts, accountsMap } }: Route.ComponentProps) {
+  const [, setSearchParams] = useSearchParams()
+
+  const onAccountFilterClicked = (id: number) => {
+    setSearchParams(prev => updateURLSearchParams(
+      prev,
+      { ...filters, accounts: filters.accounts.filter(selected => selected !== id) }
+    ))
+  }
+
+  const onCategoryFilterClicked = (id: number) => {
+    setSearchParams(prev => updateURLSearchParams(
+      prev,
+      { ...filters, categories: filters.categories.filter(selected => selected !== id) }
+    ))
+  }
+
   return (
     <AccountsContextProvider accounts={accounts} accountsMap={accountsMap}>
       <FiltersContextProvider filters={filters}>
@@ -46,6 +65,8 @@ export default function Layout({ loaderData: { filters, accounts, accountsMap } 
             <Plus />
           </Button>
         </ToolBar>
+        <AccountsFilterBar selected={filters.accounts} onSelectedClick={onAccountFilterClicked} />
+        <AccountsFilterBar selected={filters.categories} onSelectedClick={onCategoryFilterClicked} />
         <div className="grow overflow-hidden no-scrollbar grid grid-cols-2 gap-4 px-4">
           <Outlet />
         </div>
