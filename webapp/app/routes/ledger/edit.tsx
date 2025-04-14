a2b291e4dd27ee32da8c3d3c0bd1a33ff0605e15
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-04-14 10:13:06 +0000

    fixing edit transaction route

--- webapp/app/routes/ledger/edit.tsx
+++ webapp/app/routes/ledger/edit.tsx
@@ -1,17 +1,19 @@
-import { format } from "date-fns";
-import { useRef } from "react";
 import { useLocation, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
+import { get as getTransactionQuery } from "~/modules/ledger/api/queries/transactions/get";
+import { FormTemplate } from "~/modules/ledger/components/form-template";
 import { AccountsContextProvider } from "~/modules/ledger/contexts/accounts-context";
 import { FullScreenThrobber } from "~/modules/shared/components/throbber";
 import { CurrenciesContextProvider } from "~/modules/shared/contexts/currencies-context";
-import { Route } from "./+types/new";
+import { Route } from "./+types/edit";
 
-export function clientLoader({ }: Route.ClientLoaderArgs) {
-  return { breadcrumb: "Edit Transaction" }
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const transaction = await getTransactionQuery(Number(params.id))
+
+  return { breadcrumb: "Edit Transaction", transaction }
 }
 
-export default function Edit({ matches }: Route.ComponentProps) {
+export default function Edit({ loaderData: { transaction }, matches }: Route.ComponentProps) {
   // extracting data from webapp/app/modules/shared/layout.tsx's loader.
   const { data: { currencies } } = matches[1]
   // extracting data from webapp/app/routes/ledger/_layout.tsx's loader.
@@ -20,65 +22,29 @@ export default function Edit({ matches }: Route.ComponentProps) {
   const { pathname } = useLocation() // current location
   const { state: navigationState, location } = useNavigation() // navigation location
 
-  const today = useRef<Date>(new Date())
-
-
   return (
     <CurrenciesContextProvider currencies={currencies}>
       <AccountsContextProvider accounts={accounts} accountsMap={accountsMap}>
-        <section className="grid grid-cols-2 grid-rows-[0.75fr_1fr_0.5fr_min-content_0.75fr_min-content] h-full max-h-full py-4 gap-2 overflow-y-auto no-scrollbar relative">
+        <section className="flex flex-col h-full max-h-full py-4 overflow-hidden relative">
           <FullScreenThrobber
             className={cn(
               "absolute inset-0 z-50",
               (navigationState === "idle" || location.pathname === pathname) && "hidden"
             )}
           />
-          <div className="rounded-lg border">
-            From account
-          </div>
-          <div className="rounded-lg border">
-            To account
-          </div>
-          <div className="rounded-lg bg-zinc-700">
-            Source amount
-          </div>
-          <div className="rounded-lg bg-zinc-700">
-            Target amount
-          </div>
-          <div className="rounded-lg border">
-            Issued {format(today.current, "LLL dd, y")}
-          </div>
-          <div className="rounded-lg border">
-            Effective {format(today.current, "LLL dd, y")}
-          </div>
-          <div className="col-span-2">
-            Date controls
-          </div>
-          <div className="rounded-lg border col-span-2">
-            Notes
-          </div>
-
-          <div className="grid col-span-2 grid-cols-5 grid-rows-4 gap-2 mx-auto w-full max-w-[45dvh]">
-            <div className="rounded-lg border aspect-square">div</div>
-            <div className="rounded-lg border aspect-square">7</div>
-            <div className="rounded-lg border aspect-square">8</div>
-            <div className="rounded-lg border aspect-square">9</div>
-            <div className="rounded-lg border aspect-square">back</div>
-            <div className="rounded-lg border aspect-square">by</div>
-            <div className="rounded-lg border aspect-square">4</div>
-            <div className="rounded-lg border aspect-square">5</div>
-            <div className="rounded-lg border aspect-square">6</div>
-            <div className="rounded-lg border aspect-square">date</div>
-            <div className="rounded-lg border aspect-square">minus</div>
-            <div className="rounded-lg border aspect-square">1</div>
-            <div className="rounded-lg border aspect-square">2</div>
-            <div className="rounded-lg border aspect-square">3</div>
-            <div className="rounded-lg border row-span-2">done</div>
-            <div className="rounded-lg border aspect-square">plus</div>
-            <div className="rounded-lg border aspect-square">curr</div>
-            <div className="rounded-lg border aspect-square">0</div>
-            <div className="rounded-lg border aspect-square">info</div>
-          </div>
+          <FormTemplate
+            transaction={{
+              sourceId: transaction.sourceId,
+              targetId: transaction.targetId,
+              sourceAmount: transaction.sourceAmount,
+              targetAmount: transaction.targetAmount,
+              issuedAt: new Date(transaction.issuedAt),
+              executedAt: transaction.executedAt ? new Date(transaction.executedAt) : null,
+              currency: transaction.currency,
+              kind: transaction.metadata.kind
+            }}
+            role="update"
+          />
         </section>
       </AccountsContextProvider>
     </CurrenciesContextProvider>
