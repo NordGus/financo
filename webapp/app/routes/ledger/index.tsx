c941b11942cde362be70c18e63f26e39285a435d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-12 18:44:09 +0000

    moving pending transaction to ledger route

--- webapp/app/routes/ledger/index.tsx
+++ webapp/app/routes/ledger/index.tsx
@@ -1,31 +1,16 @@
-import { Info } from "lucide-react";
 import { useLocation, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
-import { list as listPendingTransactionsQuery } from "~/modules/ledger/api/queries/transactions/list-pending";
-import { TransactionsSearchResults } from "~/modules/ledger/components/transactions-search-results";
-import { mapToPendingTransactions } from "~/modules/ledger/types/transactions";
-import { getFilters } from "~/modules/ledger/utils/router-requests";
 import { FullScreenThrobber } from "~/modules/shared/components/throbber";
-import { Heading3 } from "~/modules/shared/components/ui/headings";
-import { Tooltip, TooltipContent, TooltipTrigger } from "~/modules/shared/components/ui/tooltip";
 import { CurrenciesContextProvider } from "~/modules/shared/contexts/currencies-context";
 import { Route } from "./+types/index";
 
-export async function clientLoader({ request }: Route.ClientLoaderArgs) {
-  const transactions = await listPendingTransactionsQuery(getFilters(request))
-
-  const pendingTransactions = mapToPendingTransactions(transactions)
-
-  return {
-    pendingTransactions
-  }
+export async function clientLoader({ }: Route.ClientLoaderArgs) {
+  return {}
 }
 
-export default function Index({ loaderData: { pendingTransactions }, matches }: Route.ComponentProps) {
+export default function Index({ matches }: Route.ComponentProps) {
   // extracting data from webapp/app/modules/shared/layout.tsx's loader.
   const { data: { currencies } } = matches[1]
-  // extracting data from webapp/app/modules/shared/_layout.tsx's loader.
-  const { data: { accountsMap: accounts } } = matches[2]
   // extracting data from webapp/app/routes/ledger/ledger.tsx's loader.
   const { data: { executedTransactions } } = matches[3]
 
@@ -34,36 +19,13 @@ export default function Index({ loaderData: { pendingTransactions }, matches }: 
 
   return (
     <CurrenciesContextProvider currencies={currencies}>
-      <section className="flex flex-col gap-2 h-full max-h-full overflow-y-hidden no-scrollbar relative my-4">
+      <section className="flex flex-col gap-2 overflow-y-hidden no-scrollbar relative my-2">
         <FullScreenThrobber
           className={cn(
             "absolute inset-0 z-50",
             (navigationState === "idle" || location?.search === search) && "hidden"
           )}
         />
-        {
-          pendingTransactions.length > 0 && (
-            <>
-              <div className="flex gap-4 items-center">
-                <Heading3>Pending Transactions</Heading3>
-                <Tooltip>
-                  <TooltipTrigger className="[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 [&_svg]:text-muted-foreground cursor-pointer">
-                    <Info />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    {"Transactions which do not have execution date"}
-                  </TooltipContent>
-                </Tooltip>
-              </div>
-              <div className="flex-1 flex flex-col h-full overflow-y-auto no-scrollbar relative rounded-lg overflow-clip border max-h-[45dvh]">
-                <TransactionsSearchResults
-                  transactions={pendingTransactions}
-                  accounts={accounts}
-                />
-              </div>
-            </>
-          )
-        }
         <div className="flex-2">
           <p>
             Transactions filtered {executedTransactions.length}
--- webapp/app/routes/ledger/ledger.tsx
+++ webapp/app/routes/ledger/ledger.tsx
@@ -1,24 +1,40 @@
+import { Info } from "lucide-react";
 import { Outlet, useLocation, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
 import { list as listTransactionsQuery } from "~/modules/ledger/api/queries/transactions/list";
+import { list as listPendingTransactionsQuery } from "~/modules/ledger/api/queries/transactions/list-pending";
 import { TransactionsSearchResults } from "~/modules/ledger/components/transactions-search-results";
-import { mapToExecutedTransactions } from "~/modules/ledger/types/transactions";
+import { mapToExecutedTransactions, mapToPendingTransactions } from "~/modules/ledger/types/transactions";
 import { getFilters } from "~/modules/ledger/utils/router-requests";
 import { FullScreenThrobber } from "~/modules/shared/components/throbber";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/modules/shared/components/ui/accordion";
+import { Tooltip, TooltipContent, TooltipTrigger } from "~/modules/shared/components/ui/tooltip";
 import { Route } from "./+types/ledger";
 
-export async function clientLoader({ request }: Route.LoaderArgs) {
-  const transactions = await listTransactionsQuery(getFilters(request))
 
-  const executedTransactions = mapToExecutedTransactions(transactions)
+export async function clientLoader({ request }: Route.LoaderArgs) {
+  const [executed, pending] = await Promise.allSettled([
+    listTransactionsQuery(getFilters(request)),
+    listPendingTransactionsQuery(getFilters(request))
+  ])
+
+  if (executed.status === "rejected") throw executed.reason
+  if (pending.status === "rejected") throw pending.reason
 
   return {
     breadcrumb: "Ledger",
-    executedTransactions
+    executedTransactions: mapToExecutedTransactions(executed.value),
+    pendingTransactions: mapToPendingTransactions(pending.value)
   }
 }
 
-export default function Index({ loaderData: { executedTransactions }, matches }: Route.ComponentProps) {
+export default function Index({
+  loaderData: {
+    executedTransactions,
+    pendingTransactions
+  },
+  matches
+}: Route.ComponentProps) {
   const { data: { accountsMap } } = matches[2] // extracting data from webapp/app/routes/ledger/_layout.tsx's loader.
 
   const { search } = useLocation() // current location
@@ -26,14 +42,47 @@ export default function Index({ loaderData: { executedTransactions }, matches }:
 
   return (
     <>
-      <section className="flex flex-col h-full max-h-full overflow-y-auto no-scrollbar relative">
+      <section className="flex flex-col gap-2 overflow-y-hidden no-scrollbar relative my-2">
         <FullScreenThrobber
           className={cn(
             "absolute inset-0 z-50",
             (navigationState === "idle" || location?.search === search) && "hidden"
           )}
         />
-        <div className="my-4 grow border rounded-lg overflow-clip">
+        {
+          pendingTransactions.length > 0 && (
+            <Accordion
+              className="max-h-[45dvh] flex flex-col justify-stretch"
+              type="single"
+              collapsible
+            >
+              <AccordionItem value="pending" className="flex flex-col h-full overflow-y-auto no-scrollbar relative rounded-lg overflow-clip border!">
+                <AccordionTrigger className="items-center px-4 [&[data-state=open]]:border-b! rounded-none">
+                  <div className="flex gap-2 items-center">
+                    <p className="leading-none">Pending Transactions</p>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <span className="[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-3 [&_svg]:shrink-0 [&_svg]:text-muted-foreground cursor-pointer">
+                          <Info />
+                        </span>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        {"Transactions which do not have execution date"}
+                      </TooltipContent>
+                    </Tooltip>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="flex-1 flex flex-col h-full overflow-y-auto no-scrollbar relative">
+                  <TransactionsSearchResults
+                    transactions={pendingTransactions}
+                    accounts={accountsMap}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          )
+        }
+        <div className="flex-2 flex flex-col h-full overflow-y-auto no-scrollbar relative rounded-lg overflow-clip border">
           <TransactionsSearchResults transactions={executedTransactions} accounts={accountsMap} futureEnable />
         </div>
       </section>
