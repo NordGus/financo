7b2871e3b76429d4a08ad32289218aae982558b1
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-12 20:23:12 +0000

    mocking transactions graph data

--- webapp/app/routes/ledger/index.tsx
+++ webapp/app/routes/ledger/index.tsx
@@ -1,18 +1,38 @@
 import { useLocation, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
+import { list as listTransactionsQuery } from "~/modules/ledger/api/queries/transactions/list";
+import { ExecutedTransaction } from "~/modules/ledger/types/transactions";
+import { getFilters } from "~/modules/ledger/utils/router-requests";
 import { FullScreenThrobber } from "~/modules/shared/components/throbber";
+import { Heading2 } from "~/modules/shared/components/ui/headings";
 import { CurrenciesContextProvider } from "~/modules/shared/contexts/currencies-context";
 import { Route } from "./+types/index";
 
-export async function clientLoader({ }: Route.ClientLoaderArgs) {
-  return {}
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  const transactions = await listTransactionsQuery(getFilters(request))
+
+  const executed = transactions.filter(({ executedAt }) => executedAt !== null) as ExecutedTransaction[]
+
+  const incomeTransactions = executed.filter(({ metadata: { kind } }) => kind === "income")
+  const expenseTransactions = executed.filter(({ metadata: { kind } }) => kind === "expense")
+
+  return {
+    incomeTransactions,
+    expenseTransactions
+  }
 }
 
-export default function Index({ matches }: Route.ComponentProps) {
+export default function Index({
+  loaderData: {
+    incomeTransactions,
+    expenseTransactions
+  },
+  matches
+}: Route.ComponentProps) {
   // extracting data from webapp/app/modules/shared/layout.tsx's loader.
   const { data: { currencies } } = matches[1]
-  // extracting data from webapp/app/routes/ledger/ledger.tsx's loader.
-  const { data: { executedTransactions } } = matches[3]
+  // extracting data from webapp/app/routes/ledger/_layout.tsx's loader.
+  // const { data: { accountsMap, accountsChildren } } = matches[2]
 
   const { search } = useLocation() // current location
   const { state: navigationState, location } = useNavigation() // navigation location
@@ -26,10 +46,13 @@ export default function Index({ matches }: Route.ComponentProps) {
             (navigationState === "idle" || location?.search === search) && "hidden"
           )}
         />
-        <div className="flex-2">
-          <p>
-            Transactions filtered {executedTransactions.length}
-          </p>
+        <Heading2>Expenses</Heading2>
+        <div className="flex-1">
+          Expense transactions filtered {expenseTransactions.length}
+        </div>
+        <Heading2>Income</Heading2>
+        <div className="flex-1">
+          Income transactions filtered {incomeTransactions.length}
         </div>
       </section>
     </CurrenciesContextProvider>
