7b35769eaabfa603476170eab222dc4cca03efb2
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-14 00:23:57 +0000

    creating the first simple transactions by category graph

---
+++ webapp/app/modules/ledger/components/graphs/transactions-by-category.tsx
@@ -0,0 +1,155 @@
+import { DynamicIcon } from "lucide-react/dynamic"
+import { useMemo } from "react"
+import { Label, Pie, PieChart } from "recharts"
+import { cn } from "~/lib/utils"
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "~/modules/shared/components/ui/chart"
+import { colorContrast } from "~/modules/shared/helpers/color-contrast"
+import { currencyAmountToHuman } from "~/modules/shared/helpers/currency-amount-to-human"
+import { Currency } from "~/modules/shared/types/currency"
+import { Accounts } from "../../types/accounts"
+import { ExecutedTransaction } from "../../types/transactions"
+
+type Props = {
+  transactions: ExecutedTransaction[]
+  accounts: Accounts
+  title: string
+}
+
+type ChartEntry = {
+  category: string
+  amount: number
+  currency: Currency
+  fill: string
+}
+
+export function TransactionsByCategory({ transactions, accounts, title }: Props) {
+  const currency = "EUR"
+  const baseConfig = {
+    amount: {
+      label: "Amount"
+    },
+  } satisfies ChartConfig
+
+  const data = useMemo(() => {
+    return Array.from(transactions.filter(transaction => transaction.currency === currency).reduce(
+      (accumulator: Map<number, ChartEntry>, transaction: ExecutedTransaction) => {
+        const id = transaction.metadata.kind === "income" ? transaction.sourceId : transaction.targetId
+        const amount = transaction.metadata.kind === "income" ? transaction.sourceAmount : transaction.targetAmount
+        const account = accounts.get(id)!
+        const category = account.parentId === null ? account : accounts.get(account.parentId)!
+
+        if (accumulator.has(category.id)) {
+          const entry = accumulator.get(category.id)!
+
+          accumulator.set(category.id, {
+            ...entry,
+            amount: amount + entry.amount
+          })
+
+          return accumulator
+        }
+
+        accumulator.set(category.id, {
+          category: category.id.toString(),
+          amount: amount,
+          fill: `var(--color-${category.id})`,
+          currency: transaction.currency
+        })
+
+        return accumulator
+      },
+      new Map<number, ChartEntry>()
+    ).values())
+  }, [transactions, accounts])
+
+  const total = data.reduce((acc, entry) => acc + entry.amount, 0)
+  const chartConfig = Array.from(accounts.values()).reduce((config, account) => {
+    if (account.parentId) return config
+    return {
+      ...config,
+      [account.id.toString()]: {
+        label: account.name,
+        color: account.color,
+        icon: () => <DynamicIcon name={account.icon} color={account.color} />
+      }
+    } satisfies ChartConfig
+  }, baseConfig)
+
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-[350px]"
+    >
+      <PieChart>
+        <ChartTooltip
+          cursor={false}
+          content={
+            <ChartTooltipContent
+              hideLabel
+              labelClassName="size-5"
+              formatter={(value, name) => {
+                const account = accounts.get(Number(name))!
+
+                return (
+                  <span className="flex flex-col gap-1">
+                    <span
+                      className="flex items-center gap-1 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0"
+                    >
+                      <span
+                        className={cn(
+                          "flex justify-center items-center size-5 rounded-md",
+                          (account.kind === "expense" || account.kind === "income") && "rounded-full"
+                        )}
+                        style={{
+                          backgroundColor: account.color,
+                          color: colorContrast(account.color)
+                        }}
+                      >
+                        <DynamicIcon name={account.icon} />
+                      </span>
+                      <span className="">
+                        {account.name}
+                      </span>
+                    </span>
+                    {currencyAmountToHuman(value as number, currency)}
+                  </span>
+                )
+              }}
+            />
+          }
+        />
+        <Pie data={data} dataKey="amount" nameKey="category" innerRadius={100} strokeWidth={2} paddingAngle={0.5}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-3xl font-bold"
+                    >
+                      {currencyAmountToHuman(total, currency)}
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground"
+                    >
+                      {title}
+                    </tspan>
+                  </text>
+                )
+              }
+            }}
+          />
+        </Pie>
+      </PieChart>
+    </ChartContainer>
+  )
+}
