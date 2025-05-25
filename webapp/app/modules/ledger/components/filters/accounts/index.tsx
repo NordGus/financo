f85766af775bc92f0bb00c113617e09899faaf0e
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-25 20:39:34 +0000

    implementing first version of accounts filters

--- webapp/app/modules/ledger/components/filters/accounts/index.tsx
+++ webapp/app/modules/ledger/components/filters/accounts/index.tsx
@@ -1,7 +1,10 @@
+import { ChevronRight, CreditCard, HandCoins, Landmark, PiggyBank } from "lucide-react"
+import { DynamicIcon } from "lucide-react/dynamic"
 import { use, useDeferredValue, useEffect, useMemo, useState } from "react"
 import { AccountsContext } from "~/modules/ledger/contexts/accounts-context"
 import { isAccount, isArchived } from "~/modules/ledger/types/accounts"
-import { SidebarMenu, SidebarMenuItem } from "~/modules/shared/components/ui/sidebar"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/modules/shared/components/ui/collapsible"
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub } from "~/modules/shared/components/ui/sidebar"
 import { Tabs, TabsList, TabsTrigger } from "~/modules/shared/components/ui/tabs"
 
 type Tab = "active" | "archived"
@@ -15,10 +18,10 @@ export function ByAccount({ value, onChange }: Props) {
   const { accounts } = use(AccountsContext)
 
   const [tab, setTab] = useState<Tab>("active")
-  const [selected, __setSelected] = useState(value)
+  const [selected, setSelected] = useState(value)
   const deferredSelected = useDeferredValue(selected)
 
-  const __activeAccounts = useMemo(
+  const activeAccounts = useMemo(
     () => {
       const filteredAccounts = accounts.filter(account => isAccount(account) && !isArchived(account))
 
@@ -32,7 +35,7 @@ export function ByAccount({ value, onChange }: Props) {
     [accounts]
   )
 
-  const __archivedAccounts = useMemo(
+  const archivedAccounts = useMemo(
     () => {
       const filteredAccounts = accounts.filter(account => isAccount(account) && isArchived(account))
 
@@ -50,10 +53,12 @@ export function ByAccount({ value, onChange }: Props) {
     onChange(deferredSelected)
   }, [deferredSelected])
 
+  const accountsToDisplay = tab === "active" ? activeAccounts : archivedAccounts
+
   return (
     <>
       <SidebarMenu>
-        <SidebarMenuItem>
+        <SidebarMenuItem className="py-1">
           <Tabs value={tab} onValueChange={(value) => setTab(value as Tab)}>
             <TabsList className="w-full">
               <TabsTrigger value="active">
@@ -65,6 +70,201 @@ export function ByAccount({ value, onChange }: Props) {
             </TabsList>
           </Tabs>
         </SidebarMenuItem>
+        {!!accountsToDisplay.capital.length && (
+          <SidebarMenuItem>
+            <Collapsible
+              className="group/collapsible [&[data-state=open]>button>svg:last-child]:rotate-90"
+              defaultOpen={true}
+            >
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton>
+                  <span className="flex-1 flex items-center gap-2 [&>svg]:size-5 [&>svg]:shrink-0">
+                    <Landmark /> Capital
+                  </span>
+                  <ChevronRight className="transition-transform" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {
+                    accountsToDisplay.capital.map((account) => {
+                      const isActive = selected.includes(account.id)
+
+                      return (
+                        <SidebarMenuItem
+                          key={`account.${account.id}`}
+                        >
+                          <SidebarMenuButton
+                            isActive={selected.includes(account.id)}
+                            onClick={() => setSelected(prev => {
+                              if (isActive) return prev.filter(id => id !== account.id)
+                              return [...prev, account.id]
+                            })}
+                            className="overflow-ellipsis"
+                            tooltip={{
+                              hidden: false,
+                              children: account.name
+                            }}
+                          >
+                            <DynamicIcon name={account.icon} /> <span>{account.name}</span>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      )
+                    })
+                  }
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </Collapsible>
+          </SidebarMenuItem>
+        )}
+        {!!accountsToDisplay.savings.length && (
+          <SidebarMenuItem>
+            <Collapsible
+              className="group/collapsible [&[data-state=open]>button>svg:last-child]:rotate-90"
+              defaultOpen={!accountsToDisplay.capital.length}
+            >
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton>
+                  <span className="flex-1 flex items-center gap-2 [&>svg]:size-5 [&>svg]:shrink-0">
+                    <PiggyBank /> Savings
+                  </span>
+                  <ChevronRight className="transition-transform" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {
+                    accountsToDisplay.savings.map((account) => {
+                      const isActive = selected.includes(account.id)
+
+                      return (
+                        <SidebarMenuItem
+                          key={`account.${account.id}`}
+                        >
+                          <SidebarMenuButton
+                            isActive={selected.includes(account.id)}
+                            onClick={() => setSelected(prev => {
+                              if (isActive) return prev.filter(id => id !== account.id)
+                              return [...prev, account.id]
+                            })}
+                            className="overflow-ellipsis"
+                            tooltip={{
+                              hidden: false,
+                              children: account.name
+                            }}
+                          >
+                            <DynamicIcon name={account.icon} /> <span>{account.name}</span>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      )
+                    })
+                  }
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </Collapsible>
+          </SidebarMenuItem>
+        )}
+        {!!accountsToDisplay.debts.length && (
+          <SidebarMenuItem>
+            <Collapsible
+              className="group/collapsible [&[data-state=open]>button>svg:last-child]:rotate-90"
+              defaultOpen={
+                !accountsToDisplay.capital.length &&
+                !accountsToDisplay.savings.length
+              }
+            >
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton>
+                  <span className="flex-1 flex items-center gap-2 [&>svg]:size-5 [&>svg]:shrink-0">
+                    <HandCoins /> Debts
+                  </span>
+                  <ChevronRight className="transition-transform" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {
+                    accountsToDisplay.debts.map((account) => {
+                      const isActive = selected.includes(account.id)
+
+                      return (
+                        <SidebarMenuItem
+                          key={`account.${account.id}`}
+                        >
+                          <SidebarMenuButton
+                            isActive={selected.includes(account.id)}
+                            onClick={() => setSelected(prev => {
+                              if (isActive) return prev.filter(id => id !== account.id)
+                              return [...prev, account.id]
+                            })}
+                            className="overflow-ellipsis"
+                            tooltip={{
+                              hidden: false,
+                              children: account.name
+                            }}
+                          >
+                            <DynamicIcon name={account.icon} /> <span>{account.name}</span>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      )
+                    })
+                  }
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </Collapsible>
+          </SidebarMenuItem>
+        )}
+        {!!accountsToDisplay.credit.length && (
+          <SidebarMenuItem>
+            <Collapsible
+              className="group/collapsible [&[data-state=open]>button>svg:last-child]:rotate-90"
+              defaultOpen={
+                !accountsToDisplay.capital.length &&
+                !accountsToDisplay.savings.length &&
+                !accountsToDisplay.debts.length
+              }
+            >
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton>
+                  <span className="flex-1 flex items-center gap-2 [&>svg]:size-5 [&>svg]:shrink-0">
+                    <CreditCard /> Credit
+                  </span>
+                  <ChevronRight className="transition-transform" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {
+                    accountsToDisplay.credit.map((account) => {
+                      const isActive = selected.includes(account.id)
+
+                      return (
+                        <SidebarMenuItem
+                          key={`account.${account.id}`}
+                        >
+                          <SidebarMenuButton
+                            isActive={selected.includes(account.id)}
+                            onClick={() => setSelected(prev => {
+                              if (isActive) return prev.filter(id => id !== account.id)
+                              return [...prev, account.id]
+                            })}
+                            className="overflow-ellipsis"
+                            tooltip={{
+                              hidden: false,
+                              children: account.name
+                            }}
+                          >
+                            <DynamicIcon name={account.icon} /> <span>{account.name}</span>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      )
+                    })
+                  }
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </Collapsible>
+          </SidebarMenuItem>
+        )}
       </SidebarMenu>
     </>
   )
