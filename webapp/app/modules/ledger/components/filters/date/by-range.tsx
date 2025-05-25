0ff0f7c07c5ec733d6b82d5746d187feea8de3a0
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-25 16:58:55 +0000

    implementing a by range date filter

---
+++ webapp/app/modules/ledger/components/filters/date/by-range.tsx
@@ -0,0 +1,57 @@
+import { ComponentProps, useDeferredValue, useEffect, useState } from "react"
+import { DateRange } from "react-day-picker"
+import { cn } from "~/lib/utils"
+import { Calendar } from "~/modules/shared/components/ui/calendar"
+
+interface ByRangeProps {
+  value: DateRange
+  onChange: (from: Date | undefined, to: Date | undefined) => void
+  defaultValue?: Date
+}
+
+export function ByRange({
+  value,
+  onChange,
+  className,
+  defaultValue = new Date,
+  ...props
+}: ComponentProps<typeof Calendar> & ByRangeProps) {
+  const [selected, setSelected] = useState({
+    from: value.from ?? defaultValue,
+    to: value.to ?? value.from ?? defaultValue
+  })
+  const deferredSelected = useDeferredValue(selected)
+
+  useEffect(() => {
+    if (!value.from || !value.to)
+      setSelected({ from: value.from ?? defaultValue, to: value.to ?? value.from ?? defaultValue })
+    else if (
+      value.from.toDateString() !== selected.from?.toDateString() ||
+      value.to.toDateString() !== selected.to.toDateString()
+    ) {
+      setSelected({ from: value.from ?? defaultValue, to: value.to ?? value.from ?? defaultValue })
+    }
+  }, [value.from?.toDateString(), value.to?.toDateString()])
+
+  useEffect(() => {
+    onChange(deferredSelected.from, deferredSelected.to)
+  }, [deferredSelected.from.toDateString(), deferredSelected.to.toDateString()])
+
+  return (
+    <Calendar
+      {...props}
+      mode="range"
+      selected={selected}
+      defaultMonth={value.to ?? defaultValue}
+      onSelect={(range) => {
+        if (!range || !range.from || !range.to) return
+
+        setSelected({ from: range.from, to: range.to })
+      }}
+      className={cn(
+        "[&_[role=gridcell].bg-accent]:bg-sidebar-primary [&_[role=gridcell].bg-accent]:text-sidebar-primary-foreground [&_[role=gridcell]]:w-[33px]",
+        className
+      )}
+    />
+  )
+}
