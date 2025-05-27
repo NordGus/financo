0e2ad084ebba20e0784d279e99b012e90ba73db6
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-27 20:02:48 +0000

    moving currency to ledger layout

--- webapp/app/modules/shared/layout.tsx
+++ webapp/app/modules/shared/layout.tsx
@@ -1,40 +1,30 @@
 import { Outlet, useLocation, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
 import { Route } from "./+types/layout";
-import { list as listCurrenciesQuery } from "./api/queries/list-currencies";
 import { AppSidebar } from "./components/app-sidebar";
 import { FullScreenThrobber } from "./components/throbber";
 import { SidebarProvider } from "./components/ui/sidebar";
 import { Toaster } from "./components/ui/sonner";
-import { CurrenciesContextProvider } from "./contexts/currencies-context";
 
-export async function clientLoader({ }: Route.ClientLoaderArgs) {
-  const currencies = await listCurrenciesQuery()
+export async function clientLoader({ }: Route.ClientLoaderArgs) { }
 
-  return {
-    currencies
-  }
-}
-
-export default function Layout({ loaderData: { currencies } }: Route.ComponentProps) {
+export default function Layout({ }: Route.ComponentProps) {
   const { pathname } = useLocation() // current location
   const { state: navigationState, location } = useNavigation() // navigation location
 
   return (
-    <CurrenciesContextProvider currencies={currencies}>
-      <SidebarProvider className="min-h-body items-stretch" defaultOpen={true}>
-        <AppSidebar className="h-dvh" collapsible="icon" />
-        <main className="flex flex-col grow h-dvh overflow-hidden relative">
-          <FullScreenThrobber
-            className={cn(
-              "absolute inset-0 z-50",
-              (navigationState === "idle" || location?.pathname === pathname) && "hidden"
-            )}
-          />
-          <Outlet />
-        </main>
-        <Toaster position="top-center" closeButton richColors />
-      </SidebarProvider>
-    </CurrenciesContextProvider>
+    <SidebarProvider className="min-h-body items-stretch" defaultOpen={true}>
+      <AppSidebar className="h-dvh" collapsible="icon" />
+      <main className="flex flex-col grow h-dvh overflow-hidden relative">
+        <FullScreenThrobber
+          className={cn(
+            "absolute inset-0 z-50",
+            (navigationState === "idle" || location?.pathname === pathname) && "hidden"
+          )}
+        />
+        <Outlet />
+      </main>
+      <Toaster position="top-center" closeButton richColors />
+    </SidebarProvider>
   )
 }
