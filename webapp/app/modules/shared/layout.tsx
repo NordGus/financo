eca81dd65196869f3d681218a64fbd6fec6cd8d7
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-27 19:05:21 +0000

    moving currencies context to shared layout

--- webapp/app/modules/shared/layout.tsx
+++ webapp/app/modules/shared/layout.tsx
@@ -6,6 +6,7 @@ import { AppSidebar } from "./components/app-sidebar";
 import { FullScreenThrobber } from "./components/throbber";
 import { SidebarProvider } from "./components/ui/sidebar";
 import { Toaster } from "./components/ui/sonner";
+import { CurrenciesContextProvider } from "./contexts/currencies-context";
 
 export async function clientLoader({ }: Route.ClientLoaderArgs) {
   const currencies = await listCurrenciesQuery()
@@ -15,23 +16,25 @@ export async function clientLoader({ }: Route.ClientLoaderArgs) {
   }
 }
 
-export default function Layout({ }: Route.ComponentProps) {
+export default function Layout({ loaderData: { currencies } }: Route.ComponentProps) {
   const { pathname } = useLocation() // current location
   const { state: navigationState, location } = useNavigation() // navigation location
 
   return (
-    <SidebarProvider className="min-h-body items-stretch" defaultOpen={true}>
-      <AppSidebar className="h-dvh" collapsible="icon" />
-      <main className="flex flex-col grow h-dvh overflow-hidden relative">
-        <FullScreenThrobber
-          className={cn(
-            "absolute inset-0 z-50",
-            (navigationState === "idle" || location?.pathname === pathname) && "hidden"
-          )}
-        />
-        <Outlet />
-      </main>
-      <Toaster position="top-center" closeButton richColors />
-    </SidebarProvider >
+    <CurrenciesContextProvider currencies={currencies}>
+      <SidebarProvider className="min-h-body items-stretch" defaultOpen={true}>
+        <AppSidebar className="h-dvh" collapsible="icon" />
+        <main className="flex flex-col grow h-dvh overflow-hidden relative">
+          <FullScreenThrobber
+            className={cn(
+              "absolute inset-0 z-50",
+              (navigationState === "idle" || location?.pathname === pathname) && "hidden"
+            )}
+          />
+          <Outlet />
+        </main>
+        <Toaster position="top-center" closeButton richColors />
+      </SidebarProvider>
+    </CurrenciesContextProvider>
   )
 }
