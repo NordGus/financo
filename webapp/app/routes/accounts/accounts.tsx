3297a5b132ff8be7b1d6174765a54318d2dd5b24
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-05-31 18:27:14 +0000

    replacing accounts for new stile

--- webapp/app/routes/accounts/accounts.tsx
+++ webapp/app/routes/accounts/accounts.tsx
@@ -1,157 +1,35 @@
-import { useCallback, useEffect } from "react";
-import { Outlet, URLSearchParamsInit, useSearchParams } from "react-router";
-import { toast } from "sonner";
-import { Screen } from "~/modules/accounts/screens";
-import { useAccountsStore } from "~/modules/accounts/stores/accounts";
-import { Create } from "~/modules/accounts/types/create";
-import { Update } from "~/modules/accounts/types/update";
+import { Outlet } from "react-router";
+import { list as listAccountsQuery } from "~/modules/accounts/api/queries/list";
+import { Account, Kind } from "~/modules/accounts/types/accounts";
 import { Route } from "./+types/accounts";
 
+type AccountRecords = Record<Kind, Account[]>
+
 export async function clientLoader({ }: Route.ClientLoaderArgs) {
-  return { breadcrumb: "Accounts" }
+  const accounts = await listAccountsQuery()
+
+  const records = Object.fromEntries(accounts.reduce(
+    (map, account) => {
+      const accounts = map.get(account.kind) ?? []
+
+      return map.set(account.kind, [...accounts, account])
+    },
+    new Map<Kind, Account[]>
+  ).entries()) as AccountRecords
+
+  return {
+    accounts: records
+  }
 }
 
-export default function Index() {
-  const accounts = useAccountsStore((state) => state.accounts)
-
-  const listQuery = useAccountsStore((state) => state.list)
-
-  const createAction = useAccountsStore((state) => state.create)
-  const updateAction = useAccountsStore((state) => state.update)
-  const archiveAction = useAccountsStore((state) => state.archive)
-  const unarchiveAction = useAccountsStore((state) => state.unarchive)
-  const destroyAction = useAccountsStore((state) => state.destroy)
-
-  const [searchParams, setSearchParams] = useSearchParams()
-
-  const onSearchParamsChange = (params: URLSearchParamsInit) => setSearchParams(params)
-
-  const create = useCallback(async (data: Create, success: () => void, failure: () => void) => {
-    try {
-      const res = createAction(data)
-
-      toast.promise(res, {
-        loading: "Creating...",
-        success: (data) => {
-          return `${data.name} created`
-        },
-        error: "Oops!. Something went wrong"
-      })
-
-      await res
-
-      success()
-    } catch (error) {
-      failure()
-
-      throw error
-    }
-  }, [createAction])
-
-  const update = useCallback(async (data: Update, success: () => void, failure: () => void) => {
-    try {
-      const res = updateAction(data)
-
-      toast.promise(res, {
-        loading: "Updating...",
-        success: (data) => {
-          return `${data.name} updated`
-        },
-        error: "Oops!. Something went wrong"
-      })
-
-      await res
-
-      success()
-    } catch (error) {
-      failure()
-
-      throw error
-    }
-  }, [updateAction])
-
-  const destroy = useCallback(async (id: number, success: () => void, failure: () => void) => {
-    try {
-      const res = destroyAction(id)
-
-      toast.promise(res, {
-        loading: "Deleting...",
-        success: (data) => {
-          return `${data.name} deleted`
-        },
-        error: "Oops!. Something went wrong"
-      })
-
-      await res
-
-      success()
-    } catch (error) {
-      failure()
-
-      throw error
-    }
-  }, [destroyAction])
-
-  const archive = useCallback(async (id: number, success: () => void, failure: () => void) => {
-    try {
-      const res = archiveAction(id)
-
-      toast.promise(res, {
-        loading: "Archiving...",
-        success: (data) => {
-          return `${data.name} archived`
-        },
-        error: "Oops!. Something went wrong"
-      })
-
-      await res
-
-      success()
-    } catch (error) {
-      failure()
-
-      throw error
-    }
-  }, [archiveAction])
-
-  const unarchive = useCallback(async (id: number, success: () => void, failure: () => void) => {
-    try {
-      const res = unarchiveAction(id)
-
-      toast.promise(res, {
-        loading: "Unarchiving...",
-        success: (data) => {
-          return `${data.name} unarchived`
-        },
-        error: "Oops!. Something went wrong"
-      })
-
-      await res
-
-      success()
-    } catch (error) {
-      failure()
-
-      throw error
-    }
-  }, [unarchiveAction])
-
-  useEffect(() => { listQuery() }, [])
+export default function Index({ loaderData }: Route.ComponentProps) {
+  const { accounts } = loaderData
 
   return (
     <>
-      <Screen
-        accounts={accounts}
-
-        searchParams={searchParams}
-        onSearchParamsChange={onSearchParamsChange}
-
-        onCreateAccountAction={create}
-        onUpdateAccountAction={update}
-        onArchiveAccountAction={archive}
-        onUnarchiveAccountAction={unarchive}
-        onDeleteAccountAction={destroy}
-      />
+      <section className="flex flex-col gap-2 overflow-y-hidden no-scrollbar my-2">
+        {Object.keys(accounts).join(", ")}
+      </section>
       <Outlet />
     </>
   )
