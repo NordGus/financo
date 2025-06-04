28cb7da1d78728cae3cc3297d305259677114483
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-04 21:23:44 +0000

    updating accounts filter

--- core/scope_accounts/domain/filters/accounts.go
+++ core/scope_accounts/domain/filters/accounts.go
@@ -1,7 +1,37 @@
 package filters
 
-import "financo/models/account"
+import (
+	"financo/lib/currency"
+	"financo/lib/nullable"
+	"financo/models/account"
+)
 
 type Accounts struct {
-	Kinds []account.Kind
+	Kinds      []account.Kind
+	Archived   nullable.Type[bool]
+	Currencies []currency.Type
+}
+
+func FilterAccountKinds(kinds []account.Kind) []account.Kind {
+	var (
+		out      = make([]account.Kind, 0, 7)
+		accepted = map[account.Kind]bool{
+			account.Capital: true,
+			account.Savings: true,
+			account.Debt:    true,
+			account.Credit:  true,
+		}
+	)
+
+	for i := range kinds {
+		if accepted[kinds[i]] {
+			out = append(out, kinds[i])
+		}
+	}
+
+	if len(out) == 0 {
+		out = append(out, account.Capital, account.Savings, account.Credit, account.Debt)
+	}
+
+	return out
 }
