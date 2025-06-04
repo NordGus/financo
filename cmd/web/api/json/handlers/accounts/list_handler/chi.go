c87340ab7bba651f50381966042557035cae724d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-04 21:50:49 +0000

    adding filter parsing for account list query

--- cmd/web/api/json/handlers/accounts/list_handler/chi.go
+++ cmd/web/api/json/handlers/accounts/list_handler/chi.go
@@ -5,9 +5,13 @@ import (
 	"financo/core/scope_accounts/application/queries/list_query"
 	"financo/core/scope_accounts/domain/requests"
 	"financo/core/scope_accounts/infrastructure/repositories/accounts_repository"
+	"financo/lib/currency"
+	"financo/lib/nullable"
+	"financo/models/account"
 	"financo/services/postgresql_database"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +20,41 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 		accounts = accounts_repository.NewPostgreSQL(db)
 
 		req requests.List
+		err error
 	)
+
+	req.Archive, err = parseNullableBool(r, "archived")
+	if err != nil {
+		log.Println("failed to parsed archived", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	req.Kinds, err = parseKinds(r, "kinds")
+	if err != nil {
+		log.Println("failed to parsed kinds", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	req.Currencies, err = parseCurrencies(r, "currencies")
+	if err != nil {
+		log.Println("failed to parsed kinds", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest,
+		)
+		return
+	}
 
 	res, err := list_query.New(req, accounts).Find(r.Context())
 	if err != nil {
@@ -52,4 +90,56 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Add("Content-Type", "application/json")
+}
+
+func parseKinds(r *http.Request, param string) ([]account.Kind, error) {
+	out := make([]account.Kind, 0, 4)
+
+	if !r.URL.Query().Has(param) {
+		return out, nil
+	}
+
+	raw := strings.Split(r.URL.Query().Get(param), ",")
+
+	for i := range raw {
+		if raw[i] == "" {
+			continue
+		}
+
+		out = append(out, account.Kind(raw[i]))
+	}
+
+	return out, nil
+}
+
+func parseCurrencies(r *http.Request, param string) ([]currency.Type, error) {
+	out := make([]currency.Type, 0, 4)
+
+	if !r.URL.Query().Has(param) {
+		return out, nil
+	}
+
+	raw := strings.Split(r.URL.Query().Get(param), ",")
+
+	for i := range raw {
+		if raw[i] == "" {
+			continue
+		}
+
+		out = append(out, currency.Type(raw[i]))
+	}
+
+	return out, nil
+}
+
+func parseNullableBool(r *http.Request, param string) (nullable.Type[bool], error) {
+	var out nullable.Type[bool]
+
+	if !r.URL.Query().Has(param) {
+		return out, nil
+	}
+
+	out = nullable.New(r.URL.Query().Get(param) == "true")
+
+	return out, nil
 }
