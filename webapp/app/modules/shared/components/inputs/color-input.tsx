f52d1b29e72a49ec619765dcde376cca6c7f6708
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-08 23:10:27 +0000

    adding tooltip

--- webapp/app/modules/shared/components/inputs/color-input.tsx
+++ webapp/app/modules/shared/components/inputs/color-input.tsx
@@ -3,6 +3,7 @@ import { HexColorPicker } from "react-colorful";
 import { Button } from "../ui/button";
 import { FormControl } from "../ui/form";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 interface Props {
   value: string
@@ -11,21 +12,28 @@ interface Props {
 
 export function ColorInput({ value, onChange }: Props) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <FormControl>
-          <Button
-            size={"icon"}
-            variant={"outline"}
-            type={"button"}
-          >
-            <Edit2 />
-          </Button>
-        </FormControl>
-      </PopoverTrigger>
-      <PopoverContent className="w-fit">
-        <HexColorPicker color={value} onChange={onChange} />
-      </PopoverContent>
-    </Popover>
+    <Tooltip>
+      <Popover>
+        <TooltipTrigger asChild>
+          <PopoverTrigger asChild>
+            <FormControl>
+              <Button
+                size={"icon"}
+                variant={"outline"}
+                type={"button"}
+              >
+                <Edit2 />
+              </Button>
+            </FormControl>
+          </PopoverTrigger>
+        </TooltipTrigger>
+        <TooltipContent>
+          {"Change color"}
+        </TooltipContent>
+        <PopoverContent className="w-fit">
+          <HexColorPicker color={value} onChange={onChange} />
+        </PopoverContent>
+      </Popover>
+    </Tooltip>
   )
 }
