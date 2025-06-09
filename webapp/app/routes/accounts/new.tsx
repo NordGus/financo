68c82a6eba6564c9d894c801e7671e5b57f1bd17
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-09 19:05:04 +0000

    setting up credit form

--- webapp/app/routes/accounts/new.tsx
+++ webapp/app/routes/accounts/new.tsx
@@ -2,6 +2,7 @@ import { CreditCard, HandCoins, Landmark, PiggyBank } from "lucide-react";
 import { use } from "react";
 import { createSearchParams, Link, useLocation } from "react-router";
 import { CapitalAndSavingsForm } from "~/modules/accounts/components/forms/capital-and-savings";
+import { CreditForm } from "~/modules/accounts/components/forms/credit";
 import { DebtForm } from "~/modules/accounts/components/forms/debt";
 import { ListFiltersContext } from "~/modules/accounts/contexts/list-filters-context";
 import { accountKindsManual } from "~/modules/accounts/manual/account-kinds-manual";
@@ -112,6 +113,24 @@ export default function New({ }: Route.ComponentProps) {
         {
           filters.kind === "debt" && (
             <DebtForm
+              kind={filters.kind}
+              currency={currencies[0].code}
+              color={undefined}
+              icon={undefined}
+              name={undefined}
+              description={undefined}
+              capital={0}
+              main={undefined}
+              hasHistory={undefined}
+              historyAt={undefined}
+              historyBalance={undefined}
+              role={"create"}
+            />
+          )
+        }
+        {
+          filters.kind === "credit" && (
+            <CreditForm
               kind={filters.kind}
               currency={currencies[0].code}
               color={undefined}
