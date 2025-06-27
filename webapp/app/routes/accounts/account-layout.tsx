f480f09be13f7cdc18c5765e4a0f891fbc4b7037
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-27 14:52:26 +0000

    unifying sidebar

--- webapp/app/routes/accounts/account-layout.tsx
+++ webapp/app/routes/accounts/account-layout.tsx
@@ -1,15 +1,7 @@
-import { BookTextIcon, Plus } from "lucide-react";
-import { Link, Outlet, useLocation, useResolvedPath } from "react-router";
+import { Outlet } from "react-router";
+import { AccountsSidebar } from "~/modules/accounts/components/side-bar";
 import { ListFiltersContextProvider } from "~/modules/accounts/contexts/list-filters-context";
 import { getListFilters } from "~/modules/accounts/utils/router-requests";
-import { ToolSidebar } from "~/modules/shared/components/tool-sidebar";
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
-} from "~/modules/shared/components/ui/sidebar";
 import { Route } from "./+types/account-layout";
 
 export async function clientLoader({ request }: Route.ClientLoaderArgs) {
@@ -23,34 +15,11 @@ export async function clientLoader({ request }: Route.ClientLoaderArgs) {
 export default function Layout({ loaderData }: Route.ComponentProps) {
   const { filters } = loaderData
 
-  const { pathname: newPathname } = useResolvedPath("accounts/new", { relative: "path" })
-  const { pathname: summaryPath } = useResolvedPath("accounts", { relative: "path" })
-  const { pathname, search, hash } = useLocation()
-
   return (
     <ListFiltersContextProvider filters={filters}>
-      <ToolSidebar title="Accounts" className="min-w-[250px] max-w-[250px]">
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild isActive={pathname === summaryPath}>
-                  <Link to={{ pathname: summaryPath, search, hash }}>
-                    <BookTextIcon /> My Finances
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild isActive={pathname === newPathname}>
-                  <Link to={{ pathname: newPathname, search, hash }}>
-                    <Plus /> New Account
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </ToolSidebar>
+      <AccountsSidebar>
+        {/* TODO: Implement filters for the account route */}
+      </AccountsSidebar>
       <div className="grow overflow-hidden no-scrollbar grid grid-cols-2 justify-stretch items-stretch gap-4 px-4">
         <Outlet />
       </div>
--- webapp/app/routes/accounts/accounts-layout.tsx
+++ webapp/app/routes/accounts/accounts-layout.tsx
@@ -1,18 +1,10 @@
-import { BookTextIcon, Plus } from "lucide-react";
-import { Link, Outlet, useLocation, useResolvedPath } from "react-router";
+import { Outlet } from "react-router";
 import { CurrenciesFilters } from "~/modules/accounts/components/filters/currencies";
 import { KindsFilters } from "~/modules/accounts/components/filters/kinds";
 import { StatusFilters } from "~/modules/accounts/components/filters/status";
+import { AccountsSidebar } from "~/modules/accounts/components/side-bar";
 import { ListFiltersContextProvider } from "~/modules/accounts/contexts/list-filters-context";
 import { getListFilters } from "~/modules/accounts/utils/router-requests";
-import { ToolSidebar } from "~/modules/shared/components/tool-sidebar";
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
-} from "~/modules/shared/components/ui/sidebar";
 import { Route } from "./+types/accounts-layout";
 
 export async function clientLoader({ request }: Route.ClientLoaderArgs) {
@@ -26,37 +18,13 @@ export async function clientLoader({ request }: Route.ClientLoaderArgs) {
 export default function Layout({ loaderData }: Route.ComponentProps) {
   const { filters } = loaderData
 
-  const { pathname: newPathname } = useResolvedPath("accounts/new", { relative: "path" })
-  const { pathname: summaryPath } = useResolvedPath("accounts", { relative: "path" })
-  const { pathname, search, hash } = useLocation()
-
   return (
     <ListFiltersContextProvider filters={filters}>
-      <ToolSidebar title="Accounts" className="min-w-[250px] max-w-[250px]">
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild isActive={pathname === summaryPath}>
-                  <Link to={{ pathname: summaryPath, search, hash }}>
-                    <BookTextIcon /> My Finances
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild isActive={pathname === newPathname}>
-                  <Link to={{ pathname: newPathname, search, hash }}>
-                    <Plus /> New Account
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+      <AccountsSidebar>
         <StatusFilters />
         <KindsFilters />
         <CurrenciesFilters />
-      </ToolSidebar>
+      </AccountsSidebar>
       <div className="grow overflow-hidden no-scrollbar grid grid-cols-2 justify-stretch items-stretch gap-4 px-4">
         <Outlet />
       </div>
