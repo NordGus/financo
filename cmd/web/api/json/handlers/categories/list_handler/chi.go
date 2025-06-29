7bcf0118e10f66aadac6eac743726fd846955f7b
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-06-29 10:08:03 +0000

    extending list_handler to accept new filters

--- cmd/web/api/json/handlers/categories/list_handler/chi.go
+++ cmd/web/api/json/handlers/categories/list_handler/chi.go
@@ -5,9 +5,12 @@ import (
 	"financo/core/scope_categories/application/queries/list_query"
 	"financo/core/scope_categories/domain/requests"
 	"financo/core/scope_categories/infrastructure/repositories/categories_repository"
+	"financo/lib/nullable"
+	"financo/models/account"
 	"financo/services/postgresql_database"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +19,30 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 		repo = categories_repository.NewPostgreSQL(db)
 
 		req requests.List
+		err error
 	)
+
+	req.Archive, err = parseNullableBool(r, "archived")
+	if err != nil {
+		log.Println("failed to parsed archived", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	req.Kinds, err = parseKinds(r, "kinds")
+	if err != nil {
+		log.Println("failed to parsed kinds", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest,
+		)
+		return
+	}
 
 	res, err := list_query.New(req, repo).Find(r.Context())
 	if err != nil {
@@ -52,4 +78,36 @@ func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Add("Content-Type", "application/json")
+}
+
+func parseKinds(r *http.Request, param string) ([]account.Kind, error) {
+	out := make([]account.Kind, 0, 4)
+
+	if !r.URL.Query().Has(param) {
+		return out, nil
+	}
+
+	raw := strings.Split(r.URL.Query().Get(param), ",")
+
+	for i := range raw {
+		if raw[i] == "" {
+			continue
+		}
+
+		out = append(out, account.Kind(raw[i]))
+	}
+
+	return out, nil
+}
+
+func parseNullableBool(r *http.Request, param string) (nullable.Type[bool], error) {
+	var out nullable.Type[bool]
+
+	if !r.URL.Query().Has(param) {
+		return out, nil
+	}
+
+	out = nullable.New(r.URL.Query().Get(param) == "true")
+
+	return out, nil
 }
