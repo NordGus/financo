a61390bd567142c28ab188701f02090aa73f91f5
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-06 21:10:37 +0000

    implementing new create form

--- webapp/app/modules/categories/schemas/create.ts
+++ webapp/app/modules/categories/schemas/create.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 import { ICONS } from "~/modules/shared/types/icon";
 import { Kind } from "../types/category";
+import { DESCRIPTION_MAX_LENGTH, NAME_MAX_LENGTH, NAME_MIN_LENGTH } from "./constants";
 
 const KINDS: Record<Kind, Kind> = {
   expense: "expense",
   income: "income",
 } as const
 
-export const schema = z.object({
+export const oldSchema = z.object({
   kind: z.nativeEnum(KINDS),
   name: z.string().trim().min(3).max(128),
   description: z.string().trim().max(256).optional(),
@@ -24,3 +25,59 @@ export const schema = z.object({
     icon: z.nativeEnum(ICONS),
   }).array(),
 })
+
+const expenseSchema = z.object({
+  kind: z.literal("expense"),
+  color: z.string({ required_error: "required" })
+    .max(10, { message: "invalid" }),
+  icon: z.nativeEnum(ICONS, { required_error: "required", message: "invalid option" }),
+  name: z.string({ required_error: "required" })
+    .max(NAME_MAX_LENGTH, { message: "too long" })
+    .min(NAME_MIN_LENGTH, { message: "too short" }),
+  description: z.string()
+    .max(DESCRIPTION_MAX_LENGTH, { message: "too long" })
+    .nullish(),
+  subcategories: z.array(
+    z.object({
+      kind: z.literal("expense"),
+      icon: z.nativeEnum(ICONS, { required_error: "required", message: "invalid option" }),
+      name: z.string({ required_error: "required" })
+        .max(NAME_MAX_LENGTH, { message: "too long" })
+        .min(NAME_MIN_LENGTH, { message: "too short" }),
+      description: z.string()
+        .max(DESCRIPTION_MAX_LENGTH, { message: "too long" })
+        .nullish(),
+      intent: z.literal("create")
+    })
+  ),
+  intent: z.literal("create")
+})
+
+const incomeSchema = z.object({
+  kind: z.literal("income"),
+  color: z.string({ required_error: "required" })
+    .max(10, { message: "invalid" }),
+  icon: z.nativeEnum(ICONS, { required_error: "required", message: "invalid option" }),
+  name: z.string({ required_error: "required" })
+    .max(NAME_MAX_LENGTH, { message: "too long" })
+    .min(NAME_MIN_LENGTH, { message: "too short" }),
+  description: z.string()
+    .max(DESCRIPTION_MAX_LENGTH, { message: "too long" })
+    .nullish(),
+  subcategories: z.array(
+    z.object({
+      kind: z.literal("income"),
+      icon: z.nativeEnum(ICONS, { required_error: "required", message: "invalid option" }),
+      name: z.string({ required_error: "required" })
+        .max(NAME_MAX_LENGTH, { message: "too long" })
+        .min(NAME_MIN_LENGTH, { message: "too short" }),
+      description: z.string()
+        .max(DESCRIPTION_MAX_LENGTH, { message: "too long" })
+        .nullish(),
+      intent: z.literal("create")
+    })
+  ),
+  intent: z.literal("create")
+})
+
+export const schema = z.union([expenseSchema, incomeSchema])
