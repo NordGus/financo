b46da69c5f10ab5df1940d58adcfc94cded13fba
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-08 14:25:39 +0000

    fixing get query

--- webapp/app/modules/categories/api/queries/get.ts
+++ webapp/app/modules/categories/api/queries/get.ts
@@ -2,7 +2,7 @@ import { Category } from "../../types/category";
 
 export async function get(id: number): Promise<Category> {
   const response = await fetch(
-    `${import.meta.env.VITE_API_HOST}/api/categories/?${id}`,
+    `${import.meta.env.VITE_API_HOST}/api/categories/${id}`,
     {
       headers: {
         "Content-Type": "application/json; charset=UTF-8"
