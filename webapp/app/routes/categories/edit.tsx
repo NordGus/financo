761cfaf0fcfd821a7b8d52f7d0993a3e28abc616
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-08 07:58:04 +0000

    setting up route

--- webapp/app/routes/categories/edit.tsx
+++ webapp/app/routes/categories/edit.tsx
@@ -1,9 +1,34 @@
+import { get as getCategoryQuery } from "~/modules/categories/api/queries/get";
+import { FormEditTemplate } from "~/modules/categories/components/forms/form-edit-template";
 import { Route } from "./+types/edit";
 
-export default function Edit({ }: Route.ComponentProps) {
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const id = Number(params.id)
+  const category = await getCategoryQuery(id)
+
+  return { category }
+}
+
+export default function Edit({ loaderData }: Route.ComponentProps) {
+  const { category } = loaderData
+
   return (
-    <span>
-      Edit Category Route
-    </span>
+    <section className="flex flex-col gap-2 overflow-y-hidden my-2 relative">
+      <div className="flex flex-col gap-2 overflow-y-auto flex-1">
+        <FormEditTemplate
+          kind={category.kind}
+          color={category.color}
+          icon={category.icon}
+          name={category.name}
+          description={category.description}
+          subcategories={category.children.map(child => ({
+            id: child.id,
+            icon: child.icon,
+            name: child.name,
+            description: child.description
+          }))}
+        />
+      </div>
+    </section>
   )
 }
