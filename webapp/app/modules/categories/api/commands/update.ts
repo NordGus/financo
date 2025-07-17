bfa52473a0977e766f5a917ec975c083075e497c
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-17 06:10:42 +0000

    cleaning categories code

--- webapp/app/modules/categories/api/commands/update.ts
+++ webapp/app/modules/categories/api/commands/update.ts
@@ -1,17 +1,5 @@
 import { Category } from "../../types/category";
-import { OldUpdate, Update } from "../../types/update";
-
-export async function oldUpdate(data: OldUpdate): Promise<Category> {
-  const response = await fetch(`${import.meta.env.VITE_API_HOST}/api/categories/${data.id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json; charset=UTF-8" },
-    body: JSON.stringify(data)
-  })
-
-  if (!response.ok) throw response
-
-  return response.json()
-}
+import { Update } from "../../types/update";
 
 export async function update(data: Update): Promise<Category> {
   const response = await fetch(`${import.meta.env.VITE_API_HOST}/api/categories/${data.id}`, {
