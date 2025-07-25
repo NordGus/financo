a453131fe2c4e3d7370614b6a2590dce03a86392
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-25 12:14:40 +0000

    fixing scrolling

--- webapp/app/routes/trophies/achievements.tsx
+++ webapp/app/routes/trophies/achievements.tsx
@@ -55,40 +55,42 @@ export default function Index({ loaderData }: Route.ComponentProps) {
           <span
             className="absolute top-0 left-0 right-0 contents-[' '] h-2 bg-linear-to-b from-background to-transparent z-50"
           />
-          {
-            milestones.map(({ timestamp, achievements }) => (
-              <Fragment key={timestamp}>
-                <Heading3>{format(new Date(timestamp), "PPP")}</Heading3>
-                {
-                  achievements.map((achievement) => {
-                    switch (achievement.kind) {
-                      case "savings_goal":
-                        return (
-                          <SavingsGoal
-                            id={achievement.id}
-                            name={achievement.name}
-                            description={achievement.description}
-                            saved={achievement.settings.saved}
-                            currency={achievement.settings.currency}
-                          />
-                        )
-                      default:
-                        console.error({
-                          message: `kind: ${achievement.kind}, not implemented`,
-                          achievement
-                        })
+          <div className="flex flex-col flex-1 gap-2 py-2 overflow-y-scroll no-scrollbar">
+            {
+              milestones.map(({ timestamp, achievements }) => (
+                <Fragment key={timestamp}>
+                  <Heading3>{format(new Date(timestamp), "PPP")}</Heading3>
+                  {
+                    achievements.map((achievement) => {
+                      switch (achievement.kind) {
+                        case "savings_goal":
+                          return (
+                            <SavingsGoal
+                              id={achievement.id}
+                              name={achievement.name}
+                              description={achievement.description}
+                              saved={achievement.settings.saved}
+                              currency={achievement.settings.currency}
+                            />
+                          )
+                        default:
+                          console.error({
+                            message: `kind: ${achievement.kind}, not implemented`,
+                            achievement
+                          })
 
-                        return (
-                          <span className="bg-destructive rounded-xl p-6 text-destructive-foreground">
-                            {"Sorry, this kind of achievement is not supported"}
-                          </span>
-                        )
-                    }
-                  })
-                }
-              </Fragment>
-            ))
-          }
+                          return (
+                            <span className="bg-destructive rounded-xl p-6 text-destructive-foreground">
+                              {"Sorry, this kind of achievement is not supported"}
+                            </span>
+                          )
+                      }
+                    })
+                  }
+                </Fragment>
+              ))
+            }
+          </div>
           <span
             className="absolute bottom-0 left-0 right-0 contents-[' '] h-2 bg-linear-to-b from-transparent to-background z-50"
           />
