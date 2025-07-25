988fdaa0b7db68f750e0463abaeabfb06f476a5c
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-25 23:44:41 +0000

    refactoring code for simplification

--- core/scope_trophy_room/infrastructure/repositories/milestones_repository/postgresql.go
+++ core/scope_trophy_room/infrastructure/repositories/milestones_repository/postgresql.go
@@ -4,68 +4,43 @@ package milestones_repository
 
 import (
 	"context"
-	"database/sql"
+	"encoding/json"
+	"errors"
 	"financo/core/domain/databases"
 	"financo/core/scope_trophy_room/domain/repositories"
+	"financo/lib/nullable"
 	"financo/models/achievement"
 	"financo/models/achievement/savings_goal"
-	"slices"
+	"time"
 )
 
 type postgresql struct {
 	db databases.SQLAdapter
 }
 
+type Repository interface {
+	repositories.Milestones
+}
+
 const (
-	minSliceSize = 50
+	minSliceCapacity       = 50
+	settingsBufferCapacity = 1_024 * 1_024 // 1MB
 )
 
-func NewPostgreSQL(db databases.SQLAdapter) repositories.Milestones {
+func NewPostgreSQL(db databases.SQLAdapter) Repository {
 	return &postgresql{
 		db: db,
 	}
 }
 
 func (r *postgresql) Find(ctx context.Context) ([]achievement.Milestone, error) {
-	var (
-		out = make([]achievement.Milestone, 0, minSliceSize)
-	)
+	out := make([]achievement.Milestone, 0, minSliceCapacity)
 
 	conn, err := r.db.Conn(ctx)
 	if err != nil {
 		return out, err
 	}
 	defer conn.Close()
-
-	savings, err := r.findSavingsGoals(ctx, conn)
-	if err != nil {
-		return out, err
-	}
-
-	out = append(out, savings...)
-
-	// sorting all results from newest to oldest
-	slices.SortFunc(out, func(a achievement.Milestone, b achievement.Milestone) int {
-		var (
-			at = a.AchievedAtValue()
-			bt = b.AchievedAtValue()
-		)
-
-		switch {
-		case at.After(bt):
-			return -1 // Newest first
-		case at.Before(bt):
-			return 1
-		default:
-			return 0
-		}
-	})
-
-	return out, nil
-}
-
-func (r *postgresql) findSavingsGoals(ctx context.Context, conn *sql.Conn) ([]achievement.Milestone, error) {
-	out := make([]achievement.Milestone, 0, minSliceSize)
 
 	rows, err := conn.QueryContext(
 		ctx,
@@ -84,11 +59,10 @@ func (r *postgresql) findSavingsGoals(ctx context.Context, conn *sql.Conn) ([]ac
 		WHERE
 			achieved_at IS NOT NULL
 			AND deleted_at IS NULL
-			AND kind = $1
 		ORDER BY
-			achieved_at DESC
+			achieved_at DESC,
+			updated_at DESC
 		`,
-		achievement.SavingsGoal,
 	)
 	if err != nil {
 		return out, err
@@ -96,25 +70,92 @@ func (r *postgresql) findSavingsGoals(ctx context.Context, conn *sql.Conn) ([]ac
 	defer rows.Close()
 
 	for rows.Next() {
-		var r savings_goal.Record
+		var (
+			id          int64
+			kind        achievement.Kind
+			name        string
+			description nullable.Type[string]
+			settings    = make([]uint8, 0, settingsBufferCapacity)
+			achievedAt  nullable.Type[time.Time]
+			deletedAt   nullable.Type[time.Time]
+			createdAt   time.Time
+			updatedAt   time.Time
+		)
 
 		err = rows.Scan(
-			&r.ID,
-			&r.Kind,
-			&r.Name,
-			&r.Description,
-			&r.Settings,
-			&r.AchievedAt,
-			&r.DeletedAt,
-			&r.CreatedAt,
-			&r.UpdatedAt,
+			&id,
+			&kind,
+			&name,
+			&description,
+			&settings,
+			&achievedAt,
+			&deletedAt,
+			&createdAt,
+			&updatedAt,
 		)
 		if err != nil {
 			return out, err
 		}
 
-		out = append(out, r)
+		milestone, err := mapToMilestone(
+			id,
+			kind,
+			name,
+			description,
+			settings,
+			achievedAt,
+			deletedAt,
+			createdAt,
+			updatedAt,
+		)
+		if err != nil {
+			return out, errors.Join(
+				errors.New("milestones_repository: failed to map row to milestone"),
+				err,
+			)
+		}
+
+		out = append(out, milestone)
 	}
 
 	return out, nil
+}
+
+func mapToMilestone(
+	id int64,
+	kind achievement.Kind,
+	name string,
+	description nullable.Type[string],
+	settings []uint8,
+	achievedAt nullable.Type[time.Time],
+	deletedAt nullable.Type[time.Time],
+	createdAt time.Time,
+	updatedAt time.Time,
+) (achievement.Milestone, error) {
+	switch kind {
+	case achievement.SavingsGoal:
+		var s savings_goal.Settings
+
+		err := json.Unmarshal(settings, &s)
+		if err != nil {
+			return nil, errors.Join(
+				errors.New("milestones_repository: failed to unmarshal savings goal settings"),
+				err,
+			)
+		}
+
+		return savings_goal.Record{
+			ID:          id,
+			Kind:        kind,
+			Name:        name,
+			Description: description,
+			Settings:    s,
+			AchievedAt:  achievedAt,
+			DeletedAt:   deletedAt,
+			CreatedAt:   createdAt,
+			UpdatedAt:   updatedAt,
+		}, nil
+	default:
+		return nil, errors.New("milestones_repository: achievement kind not mapped")
+	}
 }
