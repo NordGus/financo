b04fec1cd64255c81c991cecb458318aebe0edb0
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-27 00:49:11 +0000

    adding filters context

--- webapp/app/routes/trophies/_layout.tsx
+++ webapp/app/routes/trophies/_layout.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router";
+import { FiltersContextProvider } from "~/modules/achievements/trophies/contexts/filters-context";
+import { getFilters } from "~/modules/achievements/trophies/utils/router-requests";
 import { list as listCurrenciesQuery } from "~/modules/shared/api/queries/list-currencies";
 import { ToolSidebar } from "~/modules/shared/components/tool-sidebar";
 import { CurrenciesContextProvider } from "~/modules/shared/contexts/currencies-context";
@@ -11,27 +13,31 @@ export function meta({ }: Route.MetaArgs) {
   ]
 }
 
-export async function clientLoader({ }: Route.ClientLoaderArgs) {
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  const filters = getFilters(request)
   const currencies = await listCurrenciesQuery()
 
   return {
-    currencies
+    currencies,
+    filters
   }
 }
 
 export default function Trophies({ loaderData }: Route.ComponentProps) {
-  const { currencies } = loaderData
+  const { currencies, filters } = loaderData
 
   return (
     <CurrenciesContextProvider currencies={currencies}>
-      <div className="flex grow h-dvh overflow-hidden">
-        <ToolSidebar title="Trophy Room" className="min-w-[250px] max-w-[250px]">
+      <FiltersContextProvider filters={filters}>
+        <div className="flex grow h-dvh overflow-hidden">
+          <ToolSidebar title="Trophy Room" className="min-w-[250px] max-w-[250px]">
 
-        </ToolSidebar>
-        <div className="grow overflow-hidden no-scrollbar grid grid-cols-2 justify-stretch items-stretch gap-4 px-4">
-          <Outlet />
+          </ToolSidebar>
+          <div className="grow overflow-hidden no-scrollbar grid grid-cols-2 justify-stretch items-stretch gap-4 px-4">
+            <Outlet />
+          </div>
         </div>
-      </div>
+      </FiltersContextProvider>
     </CurrenciesContextProvider>
   )
 }
