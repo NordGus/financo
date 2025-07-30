6f815625254358593981a3bfe2e802f84cc3c393
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-30 14:29:07 +0000

    changing implementation to return Details

--- core/scope_savings_goals/application/queries/list_query/query.go
+++ core/scope_savings_goals/application/queries/list_query/query.go
@@ -10,7 +10,6 @@ import (
 	"financo/core/scope_savings_goals/infrastructure/lock"
 	"financo/lib/currency"
 	"financo/models/achievement/savings_goal"
-	"log"
 )
 
 type query struct {
@@ -35,8 +34,6 @@ func (q *query) Find(ctx context.Context) ([]responses.Listed, error) {
 	lock.GlobalLock().RLock()
 	defer lock.GlobalLock().RUnlock() // this one can be deferred because is just a read lock
 
-	log.Println("Query")
-
 	// records are assumed to be ordered by position in ascending order
 	records, err := q.goals.Where(ctx, filters.SavingsGoals{
 		Currencies: filters.FilterSavingsGoalCurrency(q.req.Currencies),
@@ -44,8 +41,6 @@ func (q *query) Find(ctx context.Context) ([]responses.Listed, error) {
 	if err != nil {
 		return res, err
 	}
-
-	log.Println("Query")
 
 	for _, record := range records {
 		if _, ok := goals[record.Settings.Currency]; ok {
@@ -58,19 +53,8 @@ func (q *query) Find(ctx context.Context) ([]responses.Listed, error) {
 		goals[record.Settings.Currency] = append(goals[record.Settings.Currency], record)
 	}
 
-	log.Println("Query")
-
 	for curr, records := range goals {
 		res = append(res, responses.SavingsGoalRecordsToListed(curr, records))
-	}
-
-	log.Println("Query END")
-
-	for _, r := range res {
-		log.Println(r.Currency)
-		for _, goal := range r.Goals {
-			log.Println(goal)
-		}
 	}
 
 	return res, nil
--- core/scope_savings_goals/domain/responses/listed.go
+++ core/scope_savings_goals/domain/responses/listed.go
@@ -6,13 +6,19 @@ import (
 )
 
 type Listed struct {
-	Currency currency.Type         `json:"currency"`
-	Goals    []savings_goal.Record `json:"goals"`
+	Currency currency.Type `json:"currency"`
+	Goals    []Detailed    `json:"goals"`
 }
 
 func SavingsGoalRecordsToListed(curr currency.Type, r []savings_goal.Record) Listed {
+	goals := make([]Detailed, 0, len(r))
+
+	for _, record := range r {
+		goals = append(goals, SavingsGoalRecordToDetailed(record))
+	}
+
 	return Listed{
 		Currency: curr,
-		Goals:    r,
+		Goals:    goals,
 	}
 }
