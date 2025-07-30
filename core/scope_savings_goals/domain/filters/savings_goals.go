81cc482cdde2c27422cc786931c537db2c0a60b1
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-07-30 12:07:06 +0000

    refactoring filters for savings goals

--- core/scope_savings_goals/domain/filters/savings_goals.go
+++ core/scope_savings_goals/domain/filters/savings_goals.go
@@ -3,5 +3,30 @@ package filters
 import "financo/lib/currency"
 
 type SavingsGoals struct {
-	Currency currency.Type
+	Currencies []currency.Type
+}
+
+func FilterSavingsGoalCurrency(currencies []currency.Type) []currency.Type {
+	var (
+		out      = make([]currency.Type, 0, 5)
+		accepted = map[currency.Type]bool{
+			currency.CAD: true,
+			currency.USD: true,
+			currency.EUR: true,
+			currency.CHF: true,
+			currency.GBP: true,
+		}
+	)
+
+	for i := range currencies {
+		if accepted[currencies[i]] {
+			out = append(out, currencies[i])
+		}
+	}
+
+	if len(out) == 0 {
+		out = append(out, currency.CAD, currency.USD, currency.EUR, currency.CHF, currency.GBP)
+	}
+
+	return out
 }
