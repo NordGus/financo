08dc3044d6cbb482f39525107ddb6ea24084ade2
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-08-02 19:35:15 +0000

    adding summary ploce holder

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -56,6 +56,7 @@ export default [
 
     layout("routes/savings-goals/_layout.tsx", [
       route("savings-goals", "routes/savings-goals/savings-goals.tsx", [
+        index("routes/savings-goals/index.tsx"),
         route("new", "routes/savings-goals/new.tsx"),
         route(":id", "routes/savings-goals/edit.tsx")
       ])
--- webapp/app/routes/savings-goals/_layout.tsx
+++ webapp/app/routes/savings-goals/_layout.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router";
+import { BookTextIcon, Plus } from "lucide-react";
+import { Link, Outlet, useLocation, useResolvedPath } from "react-router";
 import ClearFiltersButton from "~/modules/achievements/savings-goals/components/filters/clear-button";
 import { CurrenciesFilters } from "~/modules/achievements/savings-goals/components/filters/currencies";
 import { FiltersContextProvider } from "~/modules/achievements/savings-goals/contexts/filters-context";
 import { getFilters } from "~/modules/achievements/savings-goals/utils/router-requests";
 import { list as listCurrenciesQuery } from "~/modules/shared/api/queries/list-currencies";
 import { ToolSidebar } from "~/modules/shared/components/tool-sidebar";
+import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "~/modules/shared/components/ui/sidebar";
 import { CurrenciesContextProvider } from "~/modules/shared/contexts/currencies-context";
 import { Route } from "./+types/_layout";
 
@@ -28,11 +30,35 @@ export async function clientLoader({ request }: Route.ClientLoaderArgs) {
 export default function SavingsGoals({ loaderData }: Route.ComponentProps) {
   const { currencies, filters } = loaderData
 
+  const { pathname: newPathname } = useResolvedPath("savings-goals/new", { relative: "path" })
+  const { pathname: summaryPath } = useResolvedPath("savings-goals", { relative: "path" })
+  const { pathname, search, hash } = useLocation()
+
   return (
     <CurrenciesContextProvider currencies={currencies}>
       <FiltersContextProvider filters={filters}>
         <div className="flex grow h-dvh overflow-hidden">
           <ToolSidebar title="Savings Goals" className="min-w-[250px] max-w-[250px]">
+            <SidebarGroup>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild isActive={pathname === summaryPath}>
+                      <Link to={{ pathname: summaryPath, search, hash }}>
+                        <BookTextIcon /> Summary
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild isActive={pathname === newPathname}>
+                      <Link to={{ pathname: newPathname, search, hash }}>
+                        <Plus /> New Goal
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
             <ClearFiltersButton />
             <CurrenciesFilters />
           </ToolSidebar>
