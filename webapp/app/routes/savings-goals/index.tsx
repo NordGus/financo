ab47f57a57ac7c9d9ba021bf6a569280cae36682
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-08-02 19:39:38 +0000

    mocking the savings goals index path

--- webapp/app/routes/savings-goals/index.tsx
+++ webapp/app/routes/savings-goals/index.tsx
@@ -1,12 +1,16 @@
-import { Heading1 } from "~/modules/shared/components/ui/headings";
-import { Route } from "./+types/new";
+import { Route } from "./+types/index";
 
 export default function New({ }: Route.ComponentProps) {
+  // TODO: implement the summary screen with interesting graphs
+
   return (
     <section className="flex flex-col gap-2 overflow-y-hidden my-2 relative">
-      <div className="flex flex-col gap-2 overflow-y-auto flex-1">
-        <Heading1>Savings Goals Summary Placeholder</Heading1>
-      </div>
+      <span
+        className="absolute top-0 left-0 right-0 contents-[' '] h-2 bg-linear-to-b from-background to-transparent z-50"
+      />
+      <span
+        className="absolute bottom-0 left-0 right-0 contents-[' '] h-2 bg-linear-to-b from-transparent to-background z-50"
+      />
     </section>
   )
 }
